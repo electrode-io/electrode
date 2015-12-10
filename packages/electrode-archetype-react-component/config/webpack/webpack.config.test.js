5825ae2d800a31e62c2a033f2ea70d9c992b99f7
Ben Bayard - bbayard <BBayard@walmartlabs.com>
2015-12-10 11:42:56 -0800

    Add support for enzyme to be usable.

--- packages/electrode-archetype-react-component/config/webpack/webpack.config.test.js
+++ packages/electrode-archetype-react-component/config/webpack/webpack.config.test.js
@@ -6,10 +6,10 @@ var path = require("path");
 var _ = require("lodash");
 var prodCfg = require("./webpack.config");
 
-module.exports = {
+module.exports =  {
   cache: true,
   context: path.join(process.cwd(), "test/client"),
-  entry: require.resolve("@walmart/electrode-archetype-react-component/config/karma/entry"),
+  entry: "../../node_modules/electrode-bolt/config/karma/entry",
   output: {
     path: process.cwd(),
     filename: "bundle.js",
@@ -18,10 +18,16 @@ module.exports = {
   resolve: _.merge({}, prodCfg.resolve, {
     alias: {
       // Allow root import of `src/FOO` from ROOT/src.
-      src: path.join(process.cwd(), "src")
-    }
+      src: path.join(process.cwd(), "src"),
+      'sinon': 'sinon/pkg/sinon'
+   }
   }),
+  externals: {
+    'jsdom': 'window',
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true
+  },
   resolveLoader: prodCfg.resolveLoader,
   module: prodCfg.module,
-  devtool: "source-map"
+  devtool: "#source-map"
 };
