20395563f2670775fe518e38c3cd6428f6f4dacb
Tim Chon <tchon@walmartlabs.com>
2016-01-13 11:59:13 -0800

    Add new watch file.
      * port electrode-archetype-react-component pull request #16

---
+++ packages/electrode-archetype-react-app/config/karma/karma.conf.watch.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*
+ * Karma Configuration: "watch" version.
+ *
+ * This configuration allows live updates of code changes.
+ */
+module.exports = function (config) {
+  require("./karma.conf.dev")(config);
+  config.set({
+    files: [
+      // Test bundle (must be created via `npm run dev|hot|server-test`)
+      "http://127.0.0.1:3001/assets/bundle.js"
+      // Watch these files but do not add them to the bundle.
+    ].concat([
+      "client/**",
+      "test/**"
+    ].map(function (pattern) {
+      return { pattern: pattern, included: false, served: false, watched: true };
+    }))
+  });
+};
