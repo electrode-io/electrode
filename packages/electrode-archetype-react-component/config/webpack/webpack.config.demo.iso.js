3f1168c35873bd42db49e90764be34d207c1dde2
Rob Gerstenberger <rgerstenberger@walmartlabs.com>
2016-02-01 17:23:48 -0800

    remove extract text plugin

--- packages/electrode-archetype-react-component/config/webpack/webpack.config.demo.iso.js
+++ packages/electrode-archetype-react-component/config/webpack/webpack.config.demo.iso.js
@@ -5,15 +5,7 @@
 var path = require("path");
 var webpack = require("webpack");
 
-var babelLoader = require.resolve("babel-loader");
-var styleLoader = require.resolve("style-loader");
-var cssLoader = require.resolve("css-loader");
-var stylusLoader = require.resolve("stylus-loader");
-var urlLoader = require.resolve("url-loader");
-var fileLoader = require.resolve("file-loader");
-
 var base = require("./webpack.config.demo.dev.js");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   cache: true,
@@ -24,27 +16,9 @@ module.exports = {
     filename: "bundle.dev.js",
     publicPath: "http://127.0.0.1:2992/js"
   },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: [/node_modules/],
-        loader: babelLoader
-      },
-      { test: /\.styl$/,
-        loader: ExtractTextPlugin.extract(
-          styleLoader, cssLoader + "!" + stylusLoader) },
-      { test: /\.woff(2)?$/,
-        loader: urlLoader + "?limit=10000&minetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg|png)$/,
-        loader: fileLoader }
-    ]
-  },
+  module: base.module,
   stylus: base.stylus,
   resolve: base.resolve,
   resolveLoader: base.resolveLoader,
-  plugins: [
-    new webpack.SourceMapDevToolPlugin("[file].map"),
-    new ExtractTextPlugin("style.css")
-  ]
+  plugins: base.plugins
 };
