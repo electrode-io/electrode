378707193ca1f8b208fac2ced15d709db8e131ac
Tim Chon <tchon@walmartlabs.com>
2016-02-09 10:13:31 -0800

    update uglify; add DedupePlugin build error ticket

--- packages/electrode-archetype-react-app/config/webpack/partial/optimize.js
+++ packages/electrode-archetype-react-app/config/webpack/partial/optimize.js
@@ -3,8 +3,14 @@ const optimize = require("webpack").optimize;
 
 module.exports = () => (config) => mergeWebpackConfig(config, {
   plugins: [
-    new optimize.DedupePlugin(),
-    new optimize.UglifyJsPlugin()
+    // XXX DedupePlugin causes webpack build errors in Collections App
+    //   * https://jira.walmart.com/browse/GPRDT-196
+    //new optimize.DedupePlugin(),
+    new optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
   ]
 });
 
