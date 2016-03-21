1bdc036cbfb84018bcc750c9c2692a8d6f22d7b3
Joel Chen <xchen@walmartlabs.com>
2016-03-20 20:59:35 -0700

    dev static uses partials

--- packages/electrode-archetype-react-app/config/webpack/webpack.config.dev.static.js
+++ packages/electrode-archetype-react-app/config/webpack/webpack.config.dev.static.js
@@ -1,30 +1,16 @@
 "use strict";
 /**
- * Webpack dev configuration
+ * Webpack dev static configuration
  */
-var webpack = require("webpack");
-var path = require("path");
-
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var StatsWriterPlugin = require("webpack-stats-plugin").StatsWriterPlugin;
+var _ = require("lodash");
 var mergeWebpackConfig = require("webpack-partial").default;
 
-var config = require("./webpack.config");
+var baseConfig = require("./base.js");
+var defineConfig = require("./partial/define.js");
+var devConfig = require("./partial/dev.js");
 
-config = mergeWebpackConfig(config, {
-  output: {
-    filename: "bundle.dev.js",
-    path: path.join(process.cwd(), "dist/js"),
-    publicPath: "http://dev.walmart.com:2992/js"
-  },
-  plugins: [
-    new webpack.SourceMapDevToolPlugin("[file].map"),
-    new ExtractTextPlugin("style.css"),
-    new webpack.NoErrorsPlugin(),
-    new StatsWriterPlugin({
-      filename: "../server/stats.json"
-    })
-  ]
-});
-
-module.exports = config;
+module.exports = _.flow(
+  mergeWebpackConfig.bind(null, {}, baseConfig),
+  defineConfig(),
+  devConfig()
+)();
