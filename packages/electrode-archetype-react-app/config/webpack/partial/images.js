3080247569cd4371a420ffd1e64cfd981f2843cc
Joel Chen <xchen@walmartlabs.com>
2016-03-22 10:22:10 -0700

    fix lint errors

--- packages/electrode-archetype-react-app/config/webpack/partial/images.js
+++ packages/electrode-archetype-react-app/config/webpack/partial/images.js
@@ -1,9 +1,10 @@
 "use strict";
 
-var WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
+var WebpackIsomorphicToolsPlugin = require("webpack-isomorphic-tools/plugin");
 var mergeWebpackConfig = require("webpack-partial").default;
 var cdnLoader = require.resolve("@walmart/cdn-file-loader");
-var isoToolsPlugin = new WebpackIsomorphicToolsPlugin(require("../webpack-isomorphic-tools-config"));
+var isoConfig = require("../webpack-isomorphic-tools-config");
+var isoToolsPlugin = new WebpackIsomorphicToolsPlugin(isoConfig);
 
 if (process.env.NODE_ENV !== "production") {
   isoToolsPlugin = isoToolsPlugin.development();
@@ -15,7 +16,7 @@ module.exports = function () {
       module: {
         loaders: [{
           name: "images",
-          test: isoToolsPlugin.regular_expression('images'),
+          test: isoToolsPlugin.regular_expression("images"),
           loader: cdnLoader + "?limit=10000"
         }]
       },
--- packages/electrode-archetype-react-app/config/webpack/webpack-isomorphic-tools-config.js
+++ packages/electrode-archetype-react-app/config/webpack/webpack-isomorphic-tools-config.js
@@ -1,27 +1,29 @@
-var WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
+"use strict";
+
+var WebpackIsomorphicToolsPlugin = require("webpack-isomorphic-tools/plugin");
 
 module.exports = {
   assets: {
     images: {
       extensions: [
-        'jpeg',
-        'jpg',
-        'png',
-        'gif'
+        "jpeg",
+        "jpg",
+        "png",
+        "gif"
       ],
       parser: WebpackIsomorphicToolsPlugin.url_loader_parser
     },
     fonts: {
       extensions: [
-        'woff',
-        'woff2',
-        'ttf',
-        'eot'
+        "woff",
+        "woff2",
+        "ttf",
+        "eot"
       ],
       parser: WebpackIsomorphicToolsPlugin.url_loader_parser
     },
     svg: {
-      extension: 'svg',
+      extension: "svg",
       parser: WebpackIsomorphicToolsPlugin.url_loader_parser
     }
   }
