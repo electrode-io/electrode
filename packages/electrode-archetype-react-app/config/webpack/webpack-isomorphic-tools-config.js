f7e513d7e0b3d3c2c6d587ea1b8313a27041b9c1
Joel Chen <xchen@walmartlabs.com>
2016-03-24 13:25:53 -0700

    add comments about debug flag

--- packages/electrode-archetype-react-app/config/webpack/webpack-isomorphic-tools-config.js
+++ packages/electrode-archetype-react-app/config/webpack/webpack-isomorphic-tools-config.js
@@ -3,7 +3,9 @@
 var WebpackIsomorphicToolsPlugin = require("webpack-isomorphic-tools/plugin");
 
 module.exports = {
+  // set debug: true if want to see debug logs from the plugin
   "webpack_assets_file_path": "../dist/webpack-assets.json",
+  // the stats file is created if debug is true
   "webpack_stats_file_path": "../dist/webpack-stats.json",
   assets: {
     images: {
