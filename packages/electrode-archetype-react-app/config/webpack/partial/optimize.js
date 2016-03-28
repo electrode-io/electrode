e91445edd4d9d0afbabf4ecb6c002f49a24c37a3
Brian Beck <exogen@gmail.com>
2016-03-28 13:23:11 -0700

    Re-enable webpack DedupePlugin

--- packages/electrode-archetype-react-app/config/webpack/partial/optimize.js
+++ packages/electrode-archetype-react-app/config/webpack/partial/optimize.js
@@ -7,9 +7,7 @@ module.exports = function () {
   return function (config) {
     return mergeWebpackConfig(config, {
       plugins: [
-        // XXX DedupePlugin causes webpack build errors in Collections App
-        //   * https://jira.walmart.com/browse/GPRDT-196
-        //new optimize.DedupePlugin(),
+        new optimize.DedupePlugin(),
         new optimize.UglifyJsPlugin({
           compress: {
             warnings: false
@@ -19,4 +17,3 @@ module.exports = function () {
     });
   };
 };
-
