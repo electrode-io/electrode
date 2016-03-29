aa53cf2cdae5d51bcf597b8a8d38bb8477f67ebf
Dave Stevens <dstevens@walmartlabs.com>
2016-03-28 20:28:40 -0700

    remove post-install script no longer needed

--- packages/electrode-archetype-react-component/scripts/post-install.js
+++
@@ -1,6 +0,0 @@
-console.log("\n\n************************* WARNING ***************************** \n" +
-            "* Version 7.0.0-1 only to be used by team Checkout(CXO), *\n" +
-            "* If you are not team Checkout(CXO),                          *\n" +
-            "* this is not the archetype you are looking for.              *\n" +
-            "* Non-Checkout teams please install version ^6                *\n" +
-            "***************************************************************\n\n");
