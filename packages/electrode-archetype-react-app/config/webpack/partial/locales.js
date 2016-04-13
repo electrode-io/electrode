3efb3345ec7f2943bcf92b66681f90190239034b
Brian Beck <exogen@gmail.com>
2016-04-13 11:15:12 -0700

    Remove TODO comment

--- packages/electrode-archetype-react-app/config/webpack/partial/locales.js
+++ packages/electrode-archetype-react-app/config/webpack/partial/locales.js
@@ -5,11 +5,11 @@ var ContextReplacementPlugin = require("webpack").ContextReplacementPlugin;
 
 // Note that in modern versions of `moment`, there is actually no
 // `locale/en.js`, `en` is simply the default and included in `moment` itself.
-// (There are territory modifiers like `en-gb` and `en-ca` however.) So don't
-// expect to actually see `en.js` in the module list. The locale is listed here
-// so (1) the list of locales makes more sense and (2) just in case a different
-// version of `moment` is used which does have an `en.js`.
-var LOCALES = ["en"]; // TODO: Finalize list of locales.
+// (There are territory modifiers like `en-gb.js` and `en-ca.js` however.)
+// So don't expect to actually see `en.js` in the module list. `en` is listed
+// here anyway so (1) the list of locales makes more sense and (2) just in case
+// a past or future version of `moment` is used which does have `en.js`.
+var LOCALES = ["en"];
 var LOCALES_REGEX = new RegExp("^\./(" + LOCALES.join("|") + ")$");
 
 module.exports = function () {
