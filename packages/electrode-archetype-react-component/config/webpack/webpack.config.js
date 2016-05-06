db1278b2d508acd3aeea0220a5d12d8ab3c4ad70
Dave Stevens <dstevens@walmartlabs.com>
2016-05-05 22:18:42 -0700

    resolve loader too

--- packages/electrode-archetype-react-component/config/webpack/webpack.config.js
+++ packages/electrode-archetype-react-component/config/webpack/webpack.config.js
@@ -31,7 +31,7 @@ const baseConfiguration = {
     extensions: ["", ".js", ".jsx"]
   },
   resolveLoader: {
-    root: [archetypeNodeModules, process.cwd()]
+    root: [archetypeNodeModules, archetypeDevNodeModules, process.cwd()]
   }
 };
 
