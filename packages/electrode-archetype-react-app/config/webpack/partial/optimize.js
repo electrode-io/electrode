9caa65bf3eaedbf650ec59ae4761b1de5af6122e
Mark Brouch <mbrouch@walmartlabs.com>
2016-05-12 10:28:29 -0700

    Add lodash plugins

--- packages/electrode-archetype-react-app/config/webpack/partial/optimize.js
+++ packages/electrode-archetype-react-app/config/webpack/partial/optimize.js
@@ -3,10 +3,13 @@
 var mergeWebpackConfig = require("webpack-partial").default;
 var optimize = require("webpack").optimize;
 
+var LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
+
 module.exports = function () {
   return function (config) {
     return mergeWebpackConfig(config, {
       plugins: [
+        new LodashModuleReplacementPlugin(),
         new optimize.DedupePlugin(),
         new optimize.UglifyJsPlugin({
           compress: {
