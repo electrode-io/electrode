367726244db1edc74f94dd06e5da06ad0debe39e
rray1 <ryan.ray@formidable.com>
2016-06-08 10:57:42 -0700

    Reverting to e59341

--- packages/electrode-archetype-react-component/config/webpack/webpack.config.demo.dev.js
+++ packages/electrode-archetype-react-component/config/webpack/webpack.config.demo.dev.js
@@ -10,17 +10,6 @@ var _ = archDevRequire("lodash");
 
 var base = require("./webpack.config");
 
-try {
-  // If the module is multi-tenant there will be a tenants folder here
-  fs.statSync(process.cwd() + '/demo/tenants/');
-  // Use the correct tenant styles
-  var tenant = process.env.ELECTRODE_TENANT || 'walmart';
-  var stylePath = path.join(process.cwd() + "/demo/tenants/"+tenant+"/demo.styl");
-} catch (e) {
-  // Otherwise just use the default one
-  var stylePath = path.join(process.cwd() + "/demo/demo.styl");
-}
-
 module.exports = {
   devServer: {
     port: process.env.WEBPACK_DEVSERVER_PORT || "4000",
