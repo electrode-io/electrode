88880e872ec55c38b72f713f1fe79917ba3582e0
Brian Beck <exogen@gmail.com>
2016-06-17 14:57:49 -0700

    Use stylus-relative-loader in order to support multi-tenancy

--- packages/electrode-archetype-react-component/config/webpack/partial/stylus.js
+++ packages/electrode-archetype-react-component/config/webpack/partial/stylus.js
@@ -4,7 +4,7 @@ const archDevRequire = require("@walmart/electrode-archetype-react-component-dev
 const mergeWebpackConfig = archDevRequire("webpack-partial").default;
 const styleLoader = archDevRequire.resolve("style-loader");
 const cssLoader = archDevRequire.resolve("css-loader");
-const stylusLoader = archDevRequire.resolve("stylus-loader");
+const stylusLoader = archDevRequire.resolve("stylus-relative-loader");
 
 const cssModuleSupport = process.env.npm_package_config_electrode_archetype_react_component_webpack_css_modules === "true" ? // eslint-disable-line max-len
   "?modules" :
