2c73444e100dc82202620756df1f9ff73d82580b
Sergey Chebykin <sergey.chebykin@gmail.com>
2016-06-23 14:46:38 -0700

    Move entry points config under the /client

--- packages/electrode-archetype-react-app/config/webpack/base.js
+++ packages/electrode-archetype-react-app/config/webpack/base.js
@@ -36,7 +36,7 @@ var context = path.join(process.cwd(), "client");
  * vanilla JS file as entry.
  */
 function appEntry() {
-  var entryPath = path.join(process.cwd(), "entry.config.js");
+  var entryPath = path.join(context, "entry.config.js");
   if (fs.existsSync(entryPath)) {
     return require(entryPath);
   }
