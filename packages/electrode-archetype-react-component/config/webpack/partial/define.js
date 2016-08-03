85e73dedb3475bee8555728067e67177ea553ce2
Jeff McRiffey <jmcriffey@gmail.com>
2016-08-03 16:49:35 -0400

    fix copy-as-flow-declaration

--- packages/electrode-archetype-react-component/config/webpack/partial/define.js
+++ packages/electrode-archetype-react-component/config/webpack/partial/define.js
@@ -11,7 +11,7 @@ module.exports = () => (config) => mergeWebpackConfig(config, {
       // Signal production, so that webpack removes non-production code that
       // is in condtionals like: `if (process.env.NODE_ENV === "production")`
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-      "process.env.ELECTRODE_TENANT":  JSON.stringify(process.env.ELECTRODE_TENANT || "walmart")
+      "process.env.ELECTRODE_TENANT": JSON.stringify(process.env.ELECTRODE_TENANT || "walmart")
     })
   ]
 });
