8a5d6f6efa44177d0c3c878f33d10c577152abfa
Ray de Castro <captray108@gmail.com>
2016-09-06 15:01:27 -0700

    feature. generated project scaffold: yo electrode

---
+++ samples/universal-react-node/client/components/home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class Home extends React.Component {
+  render() {
+    return (
+      <div><h1>Hello <a href="https://github.com/electrode-io">Electrode</a></h1></div>
+    );
+  }
+}
