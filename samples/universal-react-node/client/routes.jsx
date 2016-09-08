966e32b1d63e4a620e491eabe1ba6d67541fccc6
Christopher McMahon <cmcmahon@walmartlabs.com>
2016-09-08 15:26:32 -0700

    Add demo component + route

--- samples/universal-react-node/client/routes.jsx
+++ samples/universal-react-node/client/routes.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Route, IndexRoute} from "react-router";
 import { Home } from "./components/home";
+import { CSRF } from "./components/csrf";
 
 export const routes = (
-  <Route path="/" component={Home} />
+  <Route path="/">
+    <IndexRoute component={Home} />
+    <Route path="csrf" component={CSRF} />
+  </Route>
 );
