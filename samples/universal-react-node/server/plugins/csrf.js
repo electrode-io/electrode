8d86a07c0e79ab458be9bfba9d45adb389c412aa
Christopher McMahon <cmcmahon@walmartlabs.com>
2016-09-08 15:25:47 -0700

    Add csrf plugins + config
    electrode-csrf-jwt configuration
    csrf demo endpoints plugin + configuration

---
+++ samples/universal-react-node/server/plugins/csrf.js
@@ -0,0 +1,34 @@
+"use strict";
+/*eslint-env es6*/
+var plugin = {};
+
+/*
+  Sample endpoints to demonstrate CSRF protection via the electrode-csrf-jwt module.
+  Note the endpoints require no special configuration for protection to be enabled.
+*/
+plugin.register = function (server, options, next) {
+  /* a demo GET endpoint which will return a CSRF cookie + header */
+  server.route({
+    method: "GET",
+    path: "/1",
+    handler: function (req, reply) {
+      reply("valid");
+    }
+  });
+  /* a demo POST endpoint which will require a CSRF cookie + header */
+  server.route({
+    method: "POST",
+    path: "/2",
+    handler: function (req, reply) {
+      reply("valid");
+    }
+  });
+  next();
+};
+
+plugin.register.attributes = {
+  name: "CSRFPlugin",
+  version: "0.0.1"
+};
+
+module.exports = plugin;
