9c8861950c29084d2d405782cda2f9341b80e702
Christopher McMahon <cmcmahon@walmartlabs.com>
2016-09-08 15:26:45 -0700

    Add link to CSRF demo component

--- samples/universal-react-node/client/components/home.jsx
+++ samples/universal-react-node/client/components/home.jsx
@@ -3,7 +3,13 @@ import React from "react";
 export class Home extends React.Component {
   render() {
     return (
-      <div><h1>Hello <a href="https://github.com/electrode-io">Electrode</a></h1></div>
+      <div>
+        <h1>Hello <a href="https://github.com/electrode-io">Electrode</a></h1>
+        <h2>Demonstration Components</h2>
+        <ul>
+          <li><a href="/csrf">CSRF protection using electrode-csrf-jwt</a></li>
+        </ul>
+      </div>
     );
   }
 }
