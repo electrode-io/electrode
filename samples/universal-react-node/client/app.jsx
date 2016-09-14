3741eee6a7c465c2ac2351cb16f4a974ff2959f7
tiffine-koch <tiffine.koch@gmail.com>
2016-09-14 16:21:43 -0700

    Adds above-the-fold component and updates docs in readme.md

--- samples/universal-react-node/client/app.jsx
+++ samples/universal-react-node/client/app.jsx
@@ -3,7 +3,7 @@ import { routes } from "./routes";
 import { Router } from "react-router";
 import { Resolver } from "react-resolver";
 import { createHistory } from "history";
-import "./styles/base.css";
+import "styles/base.css";
 
 window.webappStart = () => {
   Resolver.render(
--- samples/universal-react-node/client/components/home.jsx
+++ samples/universal-react-node/client/components/home.jsx
@@ -1,50 +1,24 @@
 import React from "react";
-import AboveFold from "./above-fold-simple";
-import ContextKey from "./above-fold-context-key";
-import styles from "../../client/styles/base.css";
+import AboveFold from "./above-the-fold";
+import styles from "../styles/base.css";
 
 export class Home extends React.Component {
-  getChildContext() {
-    return {
-      aboveTheFoldOnlyServerRender: {
-        ContextKey: true
-      }
-    };
+  render() {
+    return (
+      <div>
+        <h1>Hello <a href="https://github.com/electrode-io">Electrode</a></h1>
+        <h2>Demonstration Components</h2>
+        <ul>
+          <li><a href="/csrf">CSRF protection using electrode-csrf-jwt</a></li>
+          <li className="aboveFold">
+            <button>
+              <a href="/above-the-fold">
+                Above the Fold Render - increase your App's performance by using a skip prop
+              </a>
+            </button>
+          </li>
+        </ul>
+      </div>
+    );
   }
-
-    render () {
-      return (
-        <div>
-          <h1>Hello <a href="https://github.com/electrode-io">Electrode</a></h1>
-          <h2>Demonstration Components</h2>
-          <ul>
-            <li><a href="/csrf">CSRF protection using electrode-csrf-jwt</a></li>
-
-            <li className="aboveFold">
-              <button>
-                <a href="/above-fold-simple">
-                  Above the Fold Render - increase your App's performance by using a skip prop
-                </a>
-              </button>
-            </li>
-            <li className="aboveFold">
-              <button>
-                <a href="/above-fold-context-key">
-                  Above the Fold Render - increase your App's performance by setting context and passing a contextKey prop
-                </a>
-              </button>
-            </li>
-            <li>
-              <ContextKey />
-            </li>
-          </ul>
-        </div>
-      );
-    }
 }
-
-Home.childContextTypes = {
-  aboveTheFoldOnlyServerRender: React.PropTypes.shape({
-    AboveFold: React.PropTypes.bool
-  })
-};
