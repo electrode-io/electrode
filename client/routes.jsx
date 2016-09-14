c18d867c8139c00910cb3a54f1bc113b7f521dd9
tiffine-koch <tiffine.koch@gmail.com>
2016-09-14 14:00:44 -0700

    First commit

--- client/routes.jsx
+++ client/routes.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { Route, IndexRoute} from "react-router";
 import { Home } from "./components/home";
 import { CSRF } from "./components/csrf";
+import { AboveFold } from "./components/above-fold-simple";
+import { ContextKey } from "./components/above-fold-context-key";
 
 export const routes = (
   <Route path="/">
     <IndexRoute component={Home} />
     <Route path="csrf" component={CSRF} />
+    <Route path="above-fold-simple" component={AboveFold} />
+    <Route path="above-fold-context-key" component={ContextKey} />
   </Route>
 );
