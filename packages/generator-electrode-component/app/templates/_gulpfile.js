02a32617f84c356c2d921d8a7fc3bfa48135ff87
Arpan Nanavati <nanavatiarpan@gmail.com>
2016-09-14 12:00:19 -0700

    update gulpfile comments

--- packages/generator-electrode-component/app/templates/_gulpfile.js
+++ packages/generator-electrode-component/app/templates/_gulpfile.js
@@ -4,10 +4,11 @@ const exec = require("electrode-gulp-helper").exec;
 
 /**
  * There is a full range of gulp tasks defined in the above archetype
- * but here is a concise list of most often used gulp tasks at the
+ * but below is a concise list of most often used gulp tasks at the
  * component level
  *
- * TODO: insert link to the github gulp tasks in the archetype
+ * Full list of gulp tasks:
+ * https://github.com/electrode-io/electrode-archetype-react-component/blob/master/archetype-gulpfile.js#L38-L134
  *
  */
 
