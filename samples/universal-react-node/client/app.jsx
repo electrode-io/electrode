d2ef2047fb4173766d556e20de84acb0104d6e40
Mukund Goel <mukundgoel@users.noreply.github.com>
2016-09-21 17:25:16 -0700

    Add support for Devtools (#24)
    * add redux devtools dependencies
    
    * integrate redux devtools with app
    
    * fix lints and bump dependencies to latest
    
    * adding devtools to readme

--- samples/universal-react-node/client/app.jsx
+++ samples/universal-react-node/client/app.jsx
@@ -1,22 +1,34 @@
 import React from "react";
 import { routes } from "./routes";
 import { Router } from "react-router";
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import { Resolver } from "react-resolver";
 import { createHistory } from "history";
 import { Provider } from "react-redux";
 import "styles/base.css";
 
+import DevTools from "../client/devtools";
+
 const initialState = window.__PRELOADED_STATE__;
 
 const rootReducer = (s, a) => s; // eslint-disable-line no-unused-vars
-const store = createStore(rootReducer, initialState);
+
+const enhancer = compose(
+  // Add middlewares you want to use in development:
+  // applyMiddleware(d1, d2, d3),
+  DevTools.instrument()
+);
+
+const store = createStore(rootReducer, initialState, enhancer);
 
 window.webappStart = () => {
   Resolver.render(
     () =>
       <Provider store={store}>
-        <Router history={createHistory()}>{routes}</Router>
+        <div>
+          <Router history={createHistory()}>{routes}</Router>
+          <DevTools />
+        </div>
       </Provider>,
     document.querySelector(".js-content")
   );
---
+++ samples/universal-react-node/client/devtools/devtools.prod.js
@@ -0,0 +1,8 @@
+import React from "react";
+import identity from "lodash/identity";
+
+const DevTools = () => <div />;
+
+DevTools.instrument = () => identity;
+
+export default DevTools;
