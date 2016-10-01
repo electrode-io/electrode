fdc1135a8e90a5e197aa7512919843706bf4c597
Joel Chen <xchen@walmartlabs.com>
2016-10-01 11:47:45 -0700

    add react webapp with material-ui

---
+++ packages/electrode-webpack-reporter/client/components/home.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import {GridList, GridTile} from 'material-ui/GridList';
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around'
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+    overflowY: 'auto',
+    marginBottom: 24
+  }
+};
+
+const Home = () => (
+  <MuiThemeProvider>
+    <Tabs>
+      <Tab label="Report">
+        <div>
+          <Card>
+            <CardTitle title="Status"></CardTitle>
+          </Card>
+          <Card>
+            <CardTitle title="Logs"></CardTitle>
+          </Card>
+          <Card>
+            <CardTitle title="Modules"></CardTitle>
+          </Card>
+        </div>
+      </Tab>
+      <Tab label="Legacy">
+        <div style={styles.root}>
+          <GridList cols={2}>
+            <GridTile title="Status" titlePosition="top">
+              blah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blah
+              blah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blah
+              blah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blah
+              blah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blah
+              blah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blah
+            </GridTile>
+            <GridTile title="Modules" titlePosition="top">
+              blah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blah
+              blah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blah
+              blah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blah
+              blah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blah
+              blah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blah
+            </GridTile>
+            <GridTile title="Logs" rows={2} cols={2} titlePosition="top">
+              blah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blah
+              blah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blah
+              blah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blah
+              blah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blah
+              blah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blahblah blah
+            </GridTile>
+          </GridList>
+        </div>
+      </Tab>
+    </Tabs>
+  </MuiThemeProvider>
+);
+
+export default Home;
