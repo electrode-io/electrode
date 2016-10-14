9f1bac9fbaa43666e7e597a1de382873a5fc97b5
Joel Chen <xchen@walmartlabs.com>
2016-10-13 23:04:08 -0700

    update with fixes from generator and enable isormophic images

--- client/app.jsx
+++ client/app.jsx
@@ -1,17 +1,13 @@
 import React from "react";
+import {render} from "react-dom";
 import { routes } from "./routes";
 import { Router, browserHistory } from "react-router";
 import { createStore, compose } from "redux";
-import { Resolver } from "react-resolver";
 import { Provider } from "react-redux";
 import "styles/base.css";
-import rootReducer from "./reducers/index";
+import rootReducer from "./reducers";
 
 import DevTools from "../client/devtools";
-
-const initialState = window.__PRELOADED_STATE__;
-
-// const rootReducer = (s, a) => s; // eslint-disable-line no-unused-vars
 
 const enhancer = compose(
   // Add middlewares you want to use in development:
@@ -19,11 +15,11 @@ const enhancer = compose(
   DevTools.instrument()
 );
 
-const store = createStore(rootReducer, initialState, enhancer);
 
 window.webappStart = () => {
-  Resolver.render(
-    () =>
+  const initialState = window.__PRELOADED_STATE__;
+  const store = createStore(rootReducer, initialState, enhancer);
+  render(
       <Provider store={store}>
         <div>
           <Router history={browserHistory}>{routes}</Router>
--- server/index.js
+++ server/index.js
@@ -8,12 +8,18 @@ process.on("SIGINT", () => {
 
 const config = require("electrode-confippet").config;
 const staticPathsDecor = require("electrode-static-paths");
+const supports = require("electrode-archetype-react-app/supports");
 
 require.extensions[".css"] = () => {
   return;
 };
 
-require("babel-register")({
+/**
+ * Use babel register to transpile any JSX code on the fly to run
+ * in server mode, and also transpile react code to apply process.env.NODE_ENV
+ * removal to improve performance in production mode.
+ */
+supports.babelRegister({
   ignore: /node_modules\/(?!react\/)/
 });
 
@@ -33,4 +39,22 @@ const cacheConfig = {
 SSRCaching.enableCaching();
 SSRCaching.setCachingConfig(cacheConfig);
 
-require("electrode-server")(config, [staticPathsDecor()]);
+/**
+ * css-modules-require-hook: handle css-modules on node.js server.
+ * similar to Babel's babel/register it compiles CSS modules in runtime.
+ *
+ * generateScopedName - Short alias for the postcss-modules-scope plugin's option.
+ * Helps you to specify the custom way to build generic names for the class selectors.
+ * You may also use a string pattern similar to the webpack's css-loader.
+ *
+ * https://github.com/css-modules/css-modules-require-hook#generatescopedname-function
+ * https://github.com/webpack/css-loader#local-scope
+ * https://github.com/css-modules/postcss-modules-scope
+ */
+supports.cssModuleHook({
+  generateScopedName: "[name]__[local]___[hash:base64:5]"
+});
+
+supports.isomorphicExtendRequire().then(() => {
+  require("electrode-server")(config, [staticPathsDecor()]); // eslint-disable-line
+});
