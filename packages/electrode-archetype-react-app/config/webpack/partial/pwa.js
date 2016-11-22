1828bed2dec810f63f4c2c491f80e7e68548a5c5
Brandon Dail <brandon.dail@formidable.com>
2016-11-22 14:46:41 -0600

    Create entry points for each import script
    In the sw config file importScripts are defined relative to the root of
    the client folder. These scripts will be requested by the service worker
    using the importScripts API, so we create an entry point for each of
    them. This makes each script isolated from the other, so they can be
    imported in any order and run with their own bootstrap code.

--- packages/electrode-archetype-react-app/config/webpack/partial/pwa.js
+++ packages/electrode-archetype-react-app/config/webpack/partial/pwa.js
@@ -23,6 +23,44 @@ function getSWConfig() {
   return swConfig;
 }
 
+/**
+ * Takes a file path and returns a webpack-compatible
+ * filename descriptor matching the current naming schema
+ * @param  {string} filepath  original file path
+ * @return {string}           parsed file path
+ */
+function getHashedPath(filepath) {
+  var parsed = path.parse(filepath);
+  var name = parsed.name;
+  var ext = parsed.ext;
+  return name + '.[hash]' + ext;
+}
+
+/**
+ * Takes an array of strings representing scripts that
+ * a service worker will import and returns an object
+ * representing the webpack entry config. Each script
+ * will get its own entry point so they can be independantly
+ * imported by the service worker.
+ * @param  {array<string>} importScripts
+ * @param  {object}        current entry config
+ * @return {object}        new entry config
+ */
+function createEntryConfigFromScripts(importScripts, entry) {
+  // Handle the case where there might already be multiple
+  // entry points. If it is we create a new object with all
+  // existing entry points to avoid mutating the config. If its not,
+  // we assume its a string and use it as the bundle entry point.
+  var newEntry = typeof entry === "object"
+    ? Object.assign({}, entry)
+    : { bundle: entry };
+  return importScripts.reduce(function(acc, script) {
+    var name = path.parse(script).name;
+    acc[name] = script;
+    return acc;
+  }, newEntry);
+}
+
 module.exports = function () {
   return function (config) {
     var swConfig = getSWConfig();
@@ -37,11 +75,10 @@ module.exports = function () {
       title: "Electrode",
       statsFilename: "../server/iconstats.json"
     }, swConfig.manifest);
-    var cacheConfig = assign({}, swConfig.cache);
-    var precacheConfig = {
+
+    var cacheConfig = assign({
       staticFileGlobs: [
-        "dist/js/*.{js,css,png,jpg,svg}",
-        "dist/js/icons**/*.png"
+        "dist/js/*.{js,css}"
       ],
       stripPrefix: "dist/js/",
       cacheId: "electrode",
@@ -51,7 +88,7 @@ module.exports = function () {
     }, swConfig.cache);
 
     if (cacheConfig.runtimeCaching) {
-      precacheConfig.runtimeCaching = cacheConfig.runtimeCaching.map(function(runtimeCache) {
+      cacheConfig.runtimeCaching = cacheConfig.runtimeCaching.map(function(runtimeCache) {
         return {
           handler: runtimeCache.handler,
           urlPattern: new RegExp(runtimeCache.urlPattern)
@@ -59,7 +96,25 @@ module.exports = function () {
       });
     }
 
+    /**
+     * If importScripts exists in the cache config we need to overwrite
+     * the entry config and output config so we get an entry point for each
+     * script with unique names.
+     */
+    var entry = config.entry;
+    var output = config.output;
+    if (cacheConfig.importScripts) {
+      var importScripts = cacheConfig.importScripts;
+      cacheConfig.importScripts = importScripts.map(getHashedPath);
+      entry = createEntryConfigFromScripts(importScripts, entry);
+      output = {
+        filename: "[name].[hash].js"
+      };
+    }
+
     return mergeWebpackConfig(config, {
+      entry: entry,
+      output: output,
       module: {
         loaders: [
           {
@@ -87,7 +142,7 @@ module.exports = function () {
           gcm_sender_id: manifestConfig.gcm_sender_id,
           theme_color: manifestConfig.theme_color
         }),
-        new SWPrecacheWebpackPlugin(precacheConfig),
+        new SWPrecacheWebpackPlugin(cacheConfig),
       ]
     });
   };
