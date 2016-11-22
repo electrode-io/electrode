6945c6dc5c9ac2900fa59ddbee1744f62e39a4b1
Brandon Dail <brandon.dail@formidable.com>
2016-11-22 13:37:59 -0600

    Use arrow function with requre.ensure
    Because we can!

--- client/app.jsx
+++ client/app.jsx
@@ -9,9 +9,9 @@ import rootReducer from "./reducers";
 
 import DevTools from "../client/devtools";
 
-require.ensure(["./register-service-worker"], function(require) {
+require.ensure(["./register-service-worker"], (require) => {
   require("./register-service-worker")();
-});
+}, "register-service-worker");
 
 
 const enhancer = compose(
