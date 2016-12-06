d43b83ac37ce8817565ea244e62ff02ba92c8215
Jeff McRiffey <jmcriffey@users.noreply.github.com>
2016-12-05 19:16:42 -0500

    fix css module support (#55)

--- packages/electrode-archetype-react-app/supports.js
+++ packages/electrode-archetype-react-app/supports.js
@@ -1,7 +1,15 @@
 "use strict";
 
+var path = require("path");
+
 module.exports = {
-  cssModuleHook: require("css-modules-require-hook"),
+  cssModuleHook: function(options) {
+    options = options || {};
+    options.generateScopedName = options.generateScopedName || "[hash:base64]";
+    options.rootDir = options.rootDir || path.resolve(process.cwd(), "client");
+
+    require("css-modules-require-hook")(options);
+  },
   babelRegister: require("babel-register"),
   isomorphicExtendRequire: require("isomorphic-loader/lib/extend-require"),
   require: require("./require")
