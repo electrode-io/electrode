5fa8a54b58b8532df61ca9d279de6845dc741b1d
Ameya Koshti <ameyakoshti@gmail.com>
2016-12-09 11:03:28 -0800

    sw toast notifications add to generator (#59)
    * sw toast notifications add to generator
    
    * sw registration

--- packages/generator-electrode/generators/app/templates/client/sw-registration.js
+++ packages/generator-electrode/generators/app/templates/client/sw-registration.js
@@ -1,7 +1,7 @@
 /* eslint-env browser */
 /* eslint-disable no-console */
 
-module.exports = () => {
+module.exports = (notify) => {
   // Exit early if the navigator isn't available
   if (typeof navigator === "undefined") {
     return;
@@ -30,9 +30,9 @@ module.exports = () => {
               // is up to date (?)
               // TODO: Figure out why this is the case
               if (navigator.serviceWorker.controller) {
-                console.log("New or updated content is available, refresh!");
+                notify.show("New or updated content is available, refresh!", "warning");
               } else {
-                console.log("Content is now available offline!");
+                notify.show("Content is now available offline!", "success");
               }
               break;
             case "activating":
