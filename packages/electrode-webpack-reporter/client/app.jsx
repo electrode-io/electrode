9aca6e6b118af3d26e5ac7e5bd211d520fc20fbd
George <georgejweiler@gmail.com>
2016-12-13 15:16:39 -0800

    fix tap event issue on hot reload

--- packages/electrode-webpack-reporter/client/app.jsx
+++ packages/electrode-webpack-reporter/client/app.jsx
@@ -8,16 +8,16 @@ import {createStore} from "redux";
 import {Provider} from "react-redux";
 import rootReducer from "./reducers";
 import Home from "./components/home";
+
 //
 // Add the client app start up code to a function as window.webappStart.
 // The webapp's full HTML will check and call it once the js-content
 // DOM is created.
 //
 
+injectTapEventPlugin();
+
 window.webappStart = () => {
-  if (!injectTapEventPlugin) {
-    injectTapEventPlugin(); // https://github.com/callemall/material-ui/issues/4670
-  }
   const headers = new Headers(); // eslint-disable-line no-undef
   headers.append("Content-Type", "application/json");
   headers.append("Accept", "application/json");
