f509737937858f28d93b5a2b6ac3f78fcdb3a243
Animesh Dutta <a0d00hf@m-C02S32CGG8WM.local>
2016-12-27 16:29:59 -0800

    update koa index

--- packages/electrode-react-webapp/lib/koa/index.js
+++ packages/electrode-react-webapp/lib/koa/index.js
@@ -3,47 +3,52 @@
 const _ = require("lodash");
 const assert = require("assert");
 const ReactWebapp = require("../react-webapp");
-const koaRouter = require('koa-router')();
+const koaRouter = require("koa-router")();
 
-const HTTP_ERROR_500 = 500;
 const HTTP_REDIRECT = 302;
 
 const registerRoutes = (app, options, next) => {
-  console.log("AAPP Options: ", JSON.stringify(options));
+
   ReactWebapp.setupOptions(options)
-    .then((registerOptions) => {
-      console.log("RegisterOptions: ", registerOptions);
-      _.each(registerOptions.paths, (v, path) => {
-        assert(v.content, `You must define content for the webapp plugin path ${path}`);
+  .then((registerOptions) => {
+    _.each(registerOptions.paths, (v, path) => {
+      assert(v.content, `You must define content for the webapp plugin path ${path}`);
 
-        const routeHandler = ReactWebapp.makeRouteHandler(
-          registerOptions, ReactWebapp.resolveContent(v.content));
-          console.log("routeHandler : ", routeHandler);
-        koaRouter.get(path, (request, response) => {
-          console.log("IN Koa Router: request: ", request);
-          const handleStatus = (data) => {
-            console.log("DATA: ", data);
-            const status = data.status;
-            if (status === HTTP_REDIRECT) {
-              response.redirect(data.path);
-            } else {
-              response.send({message: "error"}).code(status);
-            }
-          };
+      const routeHandler = ReactWebapp.makeRouteHandler(
+        registerOptions, ReactWebapp.resolveContent(v.content));
 
-          return routeHandler({mode: request.__mode || "", request})
-            .then((data) => {
-              return data.status ? handleStatus(data) : response.send(data);
-            })
-            .catch((err) => {
-              response.send(err.html).code(err.status || HTTP_ERROR_500);
-            });
+      koaRouter.get(path, function *() {
+        const request = this.request; //eslint-disable-line
+        const respond = (data) => {
+          this.body = data; //eslint-disable-line
+        };
+
+        const handleStatus = (data) => {
+          const status = data.status;
+          if (status === HTTP_REDIRECT) {
+            this.redirect(data.path); //eslint-disable-line
+          } else {
+            respond({message: "error"});
+          }
+        };
+
+        yield routeHandler({mode: request.__mode || "", request})
+        .then((data) => {
+          if (data.status) {
+            handleStatus(data);
+          } else {
+            respond(data);
+          }
+        })
+        .catch((err) => {
+          respond(err.html);//.code(err.status || HTTP_ERROR_500);
         });
-      });
-      app.use(koaRouter.routes());
-    })
-    .then(() => next())
-    .catch(next);
+      }); //end get
+    });
+    app.use(koaRouter.routes());
+  })
+  .then(() => next())
+  .catch(next);
 };
 
 module.exports = registerRoutes;
