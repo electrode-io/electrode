d2501be53478467a20f8ffbe95c461428c7eeae6
Joel Chen <xchen@walmartlabs.com>
2017-01-25 10:34:37 -0800

    generator-electrode: update app to use support.load for src/lib mode

--- packages/generator-electrode/generators/app/templates/src/server/index.js
+++ packages/generator-electrode/generators/app/templates/src/server/index.js
@@ -6,22 +6,13 @@ process.on("SIGINT", () => {
   process.exit(0);
 });
 
-const config = require("electrode-confippet").config;
+const electrodeConfippet = require("electrode-confippet");
 <% if (isHapi) { %>const staticPathsDecor = require("electrode-static-paths");<% } %>
-const supports = require("electrode-archetype-react-app/supports");
+const support = require("electrode-archetype-react-app/support");
 
 require.extensions[".css"] = () => {
   return;
 };
-
-/**
- * Use babel register to transpile any JSX code on the fly to run
- * in server mode, and also transpile react code to apply process.env.NODE_ENV
- * removal to improve performance in production mode.
- */
-supports.babelRegister({
-  ignore: /node_modules\/(?!react\/)/
-});
 
 const cacheConfig = {
   components: {
@@ -36,25 +27,12 @@ const cacheConfig = {
   }
 };
 
-SSRCaching.enableCaching();
-SSRCaching.setCachingConfig(cacheConfig);
+support.load()
+  .then(() => {
+    const config = electrodeConfippet.config;
 
-/**
- * css-modules-require-hook: handle css-modules on node.js server.
- * similar to Babel's babel/register it compiles CSS modules in runtime.
- *
- * generateScopedName - Short alias for the postcss-modules-scope plugin's option.
- * Helps you to specify the custom way to build generic names for the class selectors.
- * You may also use a string pattern similar to the webpack's css-loader.
- *
- * https://github.com/css-modules/css-modules-require-hook#generatescopedname-function
- * https://github.com/webpack/css-loader#local-scope
- * https://github.com/css-modules/postcss-modules-scope
- */
-supports.cssModuleHook({
-  generateScopedName: "[name]__[local]___[hash:base64:5]"
-});
+    SSRCaching.enableCaching();
+    SSRCaching.setCachingConfig(cacheConfig);
 
-supports.isomorphicExtendRequire().then(() => {
-  require<% if (isHapi) { %>("electrode-server")(config, [staticPathsDecor()])<% } else if (isExpress){ %>("./express-server")(config)<% } else { %>("./koa-server")(config)<% } %>;  // eslint-disable-line
-});
+    require<% if (isHapi) { %>("electrode-server")(config, [staticPathsDecor()])<% } else if (isExpress){ %>("./express-server")(config)<% } else { %>("./koa-server")(config)<% } %>;  // eslint-disable-line
+  });
--- packages/generator-electrode/generators/config/templates/default.js
+++ packages/generator-electrode/generators/config/templates/default.js
@@ -19,7 +19,9 @@ module.exports = {
         "pathPrefix": "dist"
       }
     },<%if(pwa){%>
-    "./server/plugins/pwa": {},<%} if(isAutoSsr){%>
+    "server/plugins/pwa": {
+      "module": "./{{env.APP_SRC_DIR}}/server/plugins/pwa"
+    },<%} if(isAutoSsr){%>
     "electrode-auto-ssr": {}, <%}%>
     "webapp": {
       "module": <%if(serverType==="HapiJS"){%>"electrode-react-webapp/lib/hapi"<%}else if (serverType==="ExpressJS"){%>"electrode-react-webapp/lib/express"<%} else {%>"electrode-react-webapp/lib/koa"<%}%>,
