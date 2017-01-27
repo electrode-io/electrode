a4189f7adc8cd157fd4a42c7acc27d093f132ec0
Joel Chen <xchen@walmartlabs.com>
2017-01-26 16:24:31 -0800

    universal-react-node: fix redux store

--- samples/universal-react-node/server/views/index-view.jsx
+++ samples/universal-react-node/server/views/index-view.jsx
@@ -1,63 +1,56 @@
-import ReduxRouterEngine from 'electrode-redux-router-engine';
-import React from 'react';
-import fs from 'fs';
-import path from 'path';
+import ReduxRouterEngine from "electrode-redux-router-engine";
+import React from "react";
+import fs from "fs";
+import path from "path";
 
-import { routes } from "../../client/routes";
-const Promise = require("bluebird");
-import { createStore } from "redux";
+import {routes} from "../../client/routes";
+import Promise from "bluebird";
+import {createStore} from "redux";
 import rootReducer from "../../client/reducers";
 
-function storeInitializer(req, items) {
-    let initialState;
-    if(req.path === "/") {
-      initialState = {
-        data: "This data is obtained from Redux store"
-      };
-    } else if(req.path === "/ssrcachingtemplatetype") {
-      initialState = {
-        count: 100
-      };
-    } else if(req.path === "/ssrcachingsimpletype") {
-      initialState = {
-        count: 100
-      };
-    } else if (req.path === "/above-the-fold") {
-      initialState = {
-        skip: req.query.skip === "true"
-      };
-    } else if (req.path === "/todo-app"){
-      if(items){
-        initialState = items;
-      }
-    } else {
-      initialState = {};
-    }
+const readFileAsync = Promise.promisify(fs.readFile);
 
-    return createStore(rootReducer, initialState);
+function storeInitializer(req, storage) {
+  let initialState;
+  if (req.path === "/") {
+    initialState = {
+      data: "This data is obtained from Redux store"
+    };
+  } else if (req.path === "/ssrcachingtemplatetype") {
+    initialState = {
+      count: 100
+    };
+  } else if (req.path === "/ssrcachingsimpletype") {
+    initialState = {
+      count: 100
+    };
+  } else if (req.path === "/above-the-fold") {
+    initialState = {
+      skip: req.query.skip === "true"
+    };
+  } else if (req.path === "/todo-app") {
+    initialState = {
+      visibilityFilter: storage.visibilityFilter,
+      todos: storage.todos
+    };
+  } else {
+    initialState = {};
+  }
+
+  return createStore(rootReducer, initialState);
 }
 
 function createReduxStore(req, match) {
   return Promise.all([
-      // DO ASYNC THUNK ACTIONS HERE : store.dispatch(boostrapApp())
-      Promise.resolve({}),
-      new Promise((resolve, reject) => {
-        fs.readFile(path.join(process.cwd(),"/server/storage.json") , (err, data)=> {
-          if(err) {
-            reject(err);
-          } else {
-            resolve(data);
-          }
-        })
-      })
+    // DO ASYNC THUNK ACTIONS HERE : store.dispatch(boostrapApp())
+    Promise.resolve({}),
+    readFileAsync(path.resolve("server", "storage.json"))
       .then(JSON.parse)
       .catch(() => {
         return {};
       })
-    ]).then(([resulta, resultb]) => {
-      const store = storeInitializer(req, resultb);
-      return store;
-  });
+  ])
+    .then(([resulta, storage]) => storeInitializer(req, storage));
 }
 
 //
