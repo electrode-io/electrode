daa31dd8a8fc52db5b1880e0d0c8df6b61fd10c6
donsalari <ameyakoshti@gmail.com>
2017-01-31 09:49:04 -0800

    material-ui sample using electrode-react-webapp

--- samples/universal-material-ui/server/views/index-view.jsx
+++ samples/universal-material-ui/server/views/index-view.jsx
@@ -1,8 +1,15 @@
 import RouterResolverEngine from "electrode-router-resolver-engine";
-import { routes } from "../../client/routes";
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import {routes} from "../../client/routes";
 
 module.exports = (req) => {
+  // For Warning: Material UI: userAgent should be supplied in the muiTheme context for server-side rendering
+  global.navigator = global.navigator || {};
+  global.navigator.userAgent = req.headers['user-agent'] || 'all';
+
   if (!req.server.app.routesEngine) {
+    // For Warning: Unknown prop `onTouchTap` on <button> tag.
+    injectTapEventPlugin();
     req.server.app.routesEngine = RouterResolverEngine(routes);
   }
 
