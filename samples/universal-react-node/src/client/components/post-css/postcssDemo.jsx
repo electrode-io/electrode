a59af36d986f2598f84a480efcf9d7650c40dce9
Animesh Dutta <a0d00hf@m-C02S32CGG8WM.local>
2017-02-21 16:50:39 -0800

    update file

--- samples/universal-react-node/src/client/components/post-css/postcssDemo.jsx
+++ samples/universal-react-node/src/client/components/post-css/postcssDemo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import PostCSS from "./post-css";
+import PostCSS from "./demo";
 import styles from "./postcssDemo.css";
-import css from "./postcss.css";
+import css from "./demo.css";
 
 const PostCSSDemo = () => (
   <div>
     <h3>The Output:</h3>
-    <PostCSS/>
+    <PostCSS />
     <h3>The Files: </h3>
     <pre className={styles.outline}>{JSON.stringify(css)}</pre>
   </div>
