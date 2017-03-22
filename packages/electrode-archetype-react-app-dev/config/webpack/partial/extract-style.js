b08d569ab59b80f8e33aeb2adde20f639b9eaa5b
Karl Horky <karl.horky@gmail.com>
2017-03-22 15:14:22 +0100

    Fix CSS modules class name
    Closes electrode-io/electrode#190

--- packages/electrode-archetype-react-app-dev/config/webpack/partial/extract-style.js
+++ packages/electrode-archetype-react-app-dev/config/webpack/partial/extract-style.js
@@ -40,8 +40,8 @@ const cssModuleSupport = stylusExists && !cssNextExists;
 module.exports = function () {
   const cssModuleStylusSupport = archetype.webpack.cssModuleStylusSupport;
   const stylusQuery = cssLoader + "?-autoprefixer!" + stylusLoader;
-  const cssQuery = cssLoader + "?modules&-autoprefixer!" + postcssLoader;
-  const cssStylusQuery = cssLoader + "?modules&-autoprefixer!" + postcssLoader + "!" + stylusLoader;
+  const cssQuery = cssLoader + "?modules&localIdentName=[name]__[local]___[hash:base64:5]&-autoprefixer!" + postcssLoader;
+  const cssStylusQuery = cssLoader + "?modules&localIdentName=[name]__[local]___[hash:base64:5]&-autoprefixer!" + postcssLoader + "!" + stylusLoader;
 
   // By default, this archetype assumes you are using CSS-Modules + CSS-Next
   const rules = [{
@@ -92,6 +92,7 @@ module.exports = function () {
       new CSSSplitPlugin({ size: 4000, imports: true, preserve: true }),
       new webpack.LoaderOptionsPlugin({
         options: {
+          context: Path.resolve(process.cwd(), "client"),
           postcss: () => {
             return cssModuleSupport ? [atImport, cssnext({
               browsers: ["last 2 versions", "ie >= 9", "> 5%"]
