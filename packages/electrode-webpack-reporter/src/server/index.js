51236e9e06cd85eb8067f61081e3bc9e88541cd5
Joel Chen <jchip@users.noreply.github.com>
2017-04-12 13:23:56 -0700

    webpack-reporter: Update reporter data URL (#271)
    * Update reporter data URL
    
    * webpack-reporter: make dev server use the webpack-reporter addon also.
    
    * webpack-reporter: remove unused webapp plugin

--- packages/electrode-webpack-reporter/src/server/index.js
+++ packages/electrode-webpack-reporter/src/server/index.js
@@ -10,38 +10,47 @@ const supports = require("electrode-archetype-react-app/supports");
 const statsUtils = require("../../lib/stats-utils");
 const electrodeServer = require("electrode-server")
 const confippet = require("electrode-confippet");
+const WebpackReporter = require("../../lib/webpack-reporter");
 
 supports.load()
   .then(() => electrodeServer(confippet.config, [staticPathsDecor()]))
   .then((server) => {
-    server.route({
-      method: "GET",
-      path: "/reporter_data",
-      handler: (req, reply) => {
-        const stats = require("./stats_err.json"); // eslint-disable-line
-        const byPkg = statsUtils.getModulesByPkg(stats);
-        const data = {
-          info: statsUtils.getInfo(stats),
-          assets: statsUtils.getAssets(stats),
-          modulesByPkg: byPkg.modulesByPkg,
-          totalSizeByPkg: byPkg.totalSize,
-          warnings: statsUtils.getWarningsHtml(stats),
-          errors: statsUtils.getErrorsHtml(stats),
-          legacy: statsUtils.jsonToHtml(stats, true),
-          modules: stats.chunks[0].modules
-        };
-        reply(data);
-      }
+
+    const app = function () { }
+    app.get = (path, cb) => {
+      server.route({
+        method: "GET",
+        path,
+        handler: (req, reply) => {
+          const res = function () { };
+          res.json = (data) => reply(data), res;
+          res.send = (data) => reply(data), res;
+          return cb(req, res);
+        }
+      });
+    };
+
+    const reporter = new WebpackReporter({
+      skipReportRoutes: process.env.NODE_ENV !== "production",
+      skipSocket: true
     });
 
-    server.route({
-      method: "GET",
-      path: "/reporter_data/legacy",
-      handler: (req, reply) => {
-        const stats = require("../../test/stats_err.json");  // eslint-disable-line
-        reply({
-          legacy: statsUtils.jsonToHtml(stats)
-        });
+    const reporterOptions = {
+      state: true,
+      stats: {
+        hasWarnings: () => true,
+        hasErrors: () => true,
+        toJson: () => require("./stats_err.json") // eslint-disable-line
+      },
+      options: {
+        host: "localhost",
+        port: 3000
       }
-    });
+    }
+
+    const config = {};
+
+    reporter.apply(config);
+    config.setup(app);
+    config.reporter(reporterOptions);
   });
