14ee5f018ddba90b0683672bb8f876bbbc295686
Sheng Di <SDi@walmartlabs.com>
2017-04-24 10:54:59 -0700

    Update component generator models

--- packages/generator-electrode/component/templates/_gulpfile.js
+++ packages/generator-electrode/component/templates/_gulpfile.js
@@ -1,6 +1,8 @@
 "use strict";
 
+<% if (quoteType === "'") { %>
 const exec = require("electrode-gulp-helper").exec;
+<% } %>
 
 /**
  * There is a full range of gulp tasks defined in the above archetype
@@ -13,20 +15,13 @@ const exec = require("electrode-gulp-helper").exec;
  */
 
 const tasks = {
-  "demo": ["generate", "server-dev"],
-  "demo-iso": ["dev-iso"],
-  "generate": ["generate-metadata", "generate-documentation"],
-  "generate-documentation": () => exec(`electrode-docgen --package ./package.json --src ./src --markdown components.md`),
-  "generate-metadata": () => exec(`electrode-docgen --package ./package.json --src ./src --metadata components.json`),
   <% if (quoteType === "'") { %>
-  'archetype:lint-server': () => exec(`eslint --color -c ./.eslintrc config/karma config/webpack demo-server`),
-    "lint-react-demo": () => exec(`eslint --ext .js,.jsx -c ./.eslintrc demo/*.jsx --color`),
-      "lint-react-src": () => exec(`eslint --ext .js,.jsx -c ./.eslintrc src --color`),
-        "lint-scripts": () => exec(`eslint --ext .js -c ./.eslintrc scripts --color`),
-          "lint-react-test": () => exec(`eslint --ext .js,.jsx -c ./test/client/.eslintrc test/client --color`),
- <% } %>
-  "prepublish": ["npm:prepublish"],
-    "preversion": ["check-cov"]
+  'archetype:lint-server': () => exec(`eslint --color -c ./.eslintrc config/karma config/webpack`),
+  "lint-react-src": () => exec(`eslint --ext .js,.jsx -c ./.eslintrc src --color`),
+  "lint-scripts": () => exec(`eslint --ext .js -c ./.eslintrc scripts --color`),
+  "lint-react-test": () => exec(`eslint --ext .js,.jsx -c ./test/client/.eslintrc test/client --color`),
+  <% } %>
+  "prepublish": ["npm:prepublish"]
 };
 
 require("electrode-archetype-react-component")(tasks);
