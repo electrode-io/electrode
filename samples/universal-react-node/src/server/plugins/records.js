dc6ddf3399f4c44c0cf8371a494010c6e9722067
Sheng Di <SDi@walmartlabs.com>
2017-06-02 10:45:19 -0700

    Fix server linter issues

--- samples/universal-react-node/src/server/plugins/records.js
+++ samples/universal-react-node/src/server/plugins/records.js
@@ -1,15 +1,12 @@
 "use strict";
 /*eslint-env es6*/
 const plugin = {};
-const fs = require("fs");
-const path = require("path");
 const HTTP_CREATED = 201;
-const HTTP_ISE = 500;
 const mongojs = require("mongojs");
 const uuidV1 = require("uuid/v1");
 
 plugin.register = function (server, options, next) {
-  let db = mongojs('electrode-mongo', ['records']);
+  const db = mongojs("electrode-mongo", ["records"]);
 
   //Get All Records
   server.route({
@@ -18,7 +15,7 @@ plugin.register = function (server, options, next) {
     handler: (request, reply) => {
       db.records.find((err, docs) => {
         if (err) {
-          return reply('Internal MongoDB error', err);
+          return reply("Internal MongoDB error", err);
         }
         return reply(docs);
       });
@@ -27,20 +24,19 @@ plugin.register = function (server, options, next) {
 
   //Get 1 Record
   server.route({
-    method: 'GET',
-    path: '/records/{id}',
-    handler: function (request, reply) {
-
+    method: "GET",
+    path: "/records/{id}",
+    handler: (request, reply) => {
       db.records.findOne({
         _id: request.params.id
-      }, (err, doc) => {
+      }, (err, doc) => { // eslint-disable-line consistent-return
         if (err) {
-          return reply('Internal MongoDB error');
+          return reply("Internal MongoDB error");
         }
         if (!doc) {
           return reply("No Record Found");
         }
-        let responseString = docs.map((record) => JSON.stringify(record)).toString();
+        const responseString = doc.map((record) => JSON.stringify(record)).toString();
         reply(responseString);
       });
     }
@@ -48,19 +44,19 @@ plugin.register = function (server, options, next) {
 
   //Add a record
   server.route({
-    method: 'POST',
-    path: '/addRecord',
-    handler: function (request, reply) {
+    method: "POST",
+    path: "/addRecord",
+    handler: (request, reply) => {
       const record = request.payload;
       //Create an id
       record._id = uuidV1();
 
-      db.records.save(record, (err, result) => {
+      db.records.save(record, (err) => { // eslint-disable-line consistent-return
         if (err) {
-          console.log(err);
-          return reply('Internal MongoDB error');
+          console.log(err); // eslint-disable-line no-console
+          return reply("Internal MongoDB error");
         }
-        reply(record).code(201);
+        reply(record).code(HTTP_CREATED);
       });
     }
   });
