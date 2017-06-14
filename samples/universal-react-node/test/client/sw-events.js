b3065d776600b2d4c2f57461ccd2330b0388bc15
Sheng Di <SDi@walmartlabs.com>
2017-06-14 11:38:47 -0700

    add sw-events to resolve the test warnings

---
+++ samples/universal-react-node/test/client/sw-events.js
@@ -0,0 +1,11 @@
+/* eslint-env serviceworker */
+self.addEventListener("push", (event) => {
+  const title = "It worked!";
+  const options = {
+    body: "Great job sending that push notification!",
+    tag: "electrode-push-notification-test"
+  };
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
+});
