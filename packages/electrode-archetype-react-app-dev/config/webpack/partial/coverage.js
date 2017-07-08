972907fa50c0e4b61ababbef786145436afa04f5
Joel Chen <jchip@users.noreply.github.com>
2017-07-07 18:42:57 -0700

    [patch] back to isparta-loader due to issue with new loader (#464)

--- packages/electrode-archetype-react-app-dev/config/webpack/partial/coverage.js
+++ packages/electrode-archetype-react-app-dev/config/webpack/partial/coverage.js
@@ -1,19 +1,17 @@
 "use strict";
 
-const iiLoader = require.resolve("../loaders/istanbul-instrument-loader");
-process.traceDeprecation = true;
+const iiLoader = require.resolve("../loaders/isparta-loader");
 
 module.exports = function() {
   return {
     module: {
       rules: [
         {
-          _name: "istanbul-instrument-loader",
+          _name: "isparta-loader",
           loader: iiLoader,
           test: /(test|client)\/.*\.jsx?$/,
           enforce: "pre",
-          exclude: /(node_modules|\bclient\/vendor\b)/,
-          query: "esModules=true"
+          exclude: /(node_modules|\bclient\/vendor\b)/
         }
       ]
     }
