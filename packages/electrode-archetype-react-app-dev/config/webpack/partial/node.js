7ed8eb7321d4e5bcff8669575fff72b817872a74
Joel Chen <jchip@users.noreply.github.com>
2017-08-09 23:08:01 -0700

    [patch] update dep and use node:false webpack option (#534)

--- packages/electrode-archetype-react-app-dev/config/webpack/partial/node.js
+++ packages/electrode-archetype-react-app-dev/config/webpack/partial/node.js
@@ -1,8 +1,6 @@
 "use strict";
 
 const archetype = require("electrode-archetype-react-app/config/archetype");
-const webpack = require("webpack");
-const FunctionModulePlugin = require("webpack/lib/FunctionModulePlugin");
 const logger = require("electrode-archetype-react-app/lib/logger");
 
 module.exports = function(options) {
@@ -14,22 +12,7 @@ module.exports = function(options) {
     archetype.webpack.enableNodeSourcePlugin !== true
   ) {
     logger.info("Disabling NodeSourcePlugin for production");
-    // disable NodeSourcePlugin by setting a custom target, and then apply the
-    // plugins the original web target would've applied.
-    // will be much easier once webapck with node: false option is released
-    const output = config.output;
-    config.target = () => undefined;
-    config.plugins = [
-      new webpack.JsonpTemplatePlugin(output),
-      new FunctionModulePlugin(output),
-      new webpack.LoaderTargetPlugin("web")
-    ].concat(config.plugins);
-    return {
-      resolve: {
-        aliasFields: ["browser"],
-        mainFields: ["browser", "module", "main"]
-      }
-    };
+    return { node: false };
   } else {
     logger.info(`Not disabling NodeSourcePlugin.  NODE_ENV: '${process.env.NODE_ENV}'`);
     return {};
