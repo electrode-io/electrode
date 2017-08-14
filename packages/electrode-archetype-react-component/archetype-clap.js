b579e7d4398a0506915dd88a5d82eef7e86d6b78
Joel Chen <joel123@gmail.com>
2017-08-13 20:32:06 -0700

    [chore] format code

--- packages/electrode-archetype-react-component/archetype-clap.js
+++ packages/electrode-archetype-react-component/archetype-clap.js
@@ -37,8 +37,10 @@ function checkFrontendCov(minimum) {
     minimum = "";
   }
 
-  return exec(`istanbul check-coverage 'coverage/client/*/coverage.json'`, 
-    `--config=${archetype.devPath}/config/istanbul/.istanbul.${minimum}yml`);
+  return exec(
+    `istanbul check-coverage 'coverage/client/*/coverage.json'`,
+    `--config=${archetype.devPath}/config/istanbul/.istanbul.${minimum}yml`
+  );
 }
 
 const tasks = {
@@ -46,7 +48,7 @@ const tasks = {
   "~production-env": () => setProductionEnv(),
 
   // code build/compile tasks
-  "build": ["clean-dist", "build-lib", "build-dist"],
+  build: ["clean-dist", "build-lib", "build-dist"],
   "build-dist": ["build-dist-min", "build-lib:clean-tmp"],
   "build-dist-min": {
     dep: ["~production-env"],
@@ -55,24 +57,35 @@ const tasks = {
   },
   "build-lib": {
     dep: ["~production-env"],
-    task: ["clean-lib", "babel-src-step", "build-lib:flatten-l10n", "build-lib:copy-flow", "build-lib:clean-tmp"]
+    task: [
+      "clean-lib",
+      "babel-src-step",
+      "build-lib:flatten-l10n",
+      "build-lib:copy-flow",
+      "build-lib:clean-tmp"
+    ]
   },
   "babel-src-step": `babel src -d lib`,
   "build-lib:clean-tmp": () => $$.rm("-rf", "./tmp"),
   "build-lib:copy-flow": `node ${archetype.devPath}/scripts/copy-as-flow-declaration.js`,
   "build-lib:flatten-l10n": `node ${archetype.devPath}/scripts/l10n/flatten-messages.js`,
 
-  "archetype:check": ["archetype:test-dev-pkg", "clean-test-init", "archetype:test-init-pkg", "clean-test-init"],
+  "archetype:check": [
+    "archetype:test-dev-pkg",
+    "clean-test-init",
+    "archetype:test-init-pkg",
+    "clean-test-init"
+  ],
   "archetype:test-dev-pkg": `pjv -f dev/package.json`,
   "archetype:test-init-pkg": "pjv -f test-init/package.json",
 
-  "check": ["check-dep", "lint", "test-cov", "build-lib:clean-tmp"],
+  check: ["check-dep", "lint", "test-cov", "build-lib:clean-tmp"],
   "check-ci": ["check-dep", "lint", "test-ci", "build-lib:clean-tmp"],
   "check-cov": ["lint", "test-cov"],
   "check-dep": `ecd -f package.json --cf ${archetype.devPath}/config/dependencies/check.json -w`,
   "check-dev": ["lint", "test-dev"],
 
-  "clean": ["clean-lib", "clean-dist"],
+  clean: ["clean-lib", "clean-dist"],
   "clean-dist": () => $$.rm("-rf", "dist"),
   "clean-lib": () => $$.rm("-rf", "lib"),
   "clean-test-init": () => $$.rm("-rf", "test-init"),
@@ -83,17 +96,23 @@ const tasks = {
   "cov-frontend-85": () => checkFrontendCov("85"),
   "cov-frontend-95": () => checkFrontendCov("95"),
 
-  "lint": ["lint-react-src", "lint-react-test", "lint-scripts"],
-  "lint-react-src": mkCmd(`eslint --ext .js,.jsx`,
-    `-c ${archetype.devPath}/config/eslint/.eslintrc-react src --color`),
-  "lint-react-test": mkCmd(`eslint --ext .js,.jsx`, 
-    `-c ${archetype.devPath}/config/eslint/.eslintrc-react-test test/client --color`),
+  lint: ["lint-react-src", "lint-react-test", "lint-scripts"],
+  "lint-react-src": mkCmd(
+    `eslint --ext .js,.jsx`,
+    `-c ${archetype.devPath}/config/eslint/.eslintrc-react src --color`
+  ),
+  "lint-react-test": mkCmd(
+    `eslint --ext .js,.jsx`,
+    `-c ${archetype.devPath}/config/eslint/.eslintrc-react-test test/client --color`
+  ),
   "lint-scripts": `eslint --ext .js -c ${archetype.devPath}/config/eslint/.eslintrc-base scripts --color`,
 
   "npm:prepublish": ["build-lib", "build-dist-min"],
 
-  "server-test": mkCmd(`webpack-dev-server --port 3001`,
-    `--config ${archetype.devPath}/config/webpack/webpack.config.test.js --colors`),
+  "server-test": mkCmd(
+    `webpack-dev-server --port 3001`,
+    `--config ${archetype.devPath}/config/webpack/webpack.config.test.js --colors`
+  ),
 
   "test-ci": ["test-frontend-ci"],
   "test-cov": ["test-frontend-cov"],
@@ -104,11 +123,13 @@ const tasks = {
   "test-frontend-ci": `karma start --browsers PhantomJS,Firefox ${archetype.devPath}/config/karma/karma.conf.coverage.js --colors`,
   "test-frontend-cov": `karma start ${archetype.devPath}/config/karma/karma.conf.coverage.js --colors`,
   "test-frontend-dev": `karma start ${archetype.devPath}/config/karma/karma.conf.dev.js --colors`,
-  "test-frontend-dev-watch": mkCmd(`karma start ${archetype.devPath}/config/karma/karma.conf.watch.js`, 
-    ` --colors --browsers Chrome --no-single-run --auto-watch`)
+  "test-frontend-dev-watch": mkCmd(
+    `karma start ${archetype.devPath}/config/karma/karma.conf.watch.js`,
+    ` --colors --browsers Chrome --no-single-run --auto-watch`
+  )
 };
 
-module.exports = function (xclap) {
+module.exports = function(xclap) {
   setupPath();
   xclap = xclap || requireAt(process.cwd())("xclap") || devRequire("xclap");
   xclap.load("electrode", tasks);
