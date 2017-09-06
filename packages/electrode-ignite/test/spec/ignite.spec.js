7cc406920b9948ae282d597519233fc620492d54
Sheng Di <SDi@walmartlabs.com>
2017-09-05 21:28:18 -0700

    Electrode Ignite add unit test

--- packages/electrode-ignite/test/spec/ignite.spec.js
+++ packages/electrode-ignite/test/spec/ignite.spec.js
@@ -8,23 +8,40 @@ const logger = require("ignite-core/lib/logger");
 const chalk = require("chalk");
 const pkg = require("../../package.json");
 
-describe.skip("electrode-ignite", function() {
+describe("electrode-ignite", function() {
   let loggerStub = "";
+  let processStub = "";
 
   beforeEach(function() {
     loggerStub = sinon.stub(logger, "log");
+    processStub = sinon.stub(process, "exit");
   });
 
   afterEach(function() {
     loggerStub.restore();
+    processStub.restore();
   });
 
-  it("Print welcome message", function() {
+  it("Print welcome message for non-check-ignite tasks", function() {
+    electrodeIgnite();
+    sinon.assert.callCount(loggerStub, 1);
+    assert.equal(
+      loggerStub.getCalls()[0].args.toString(),
+      chalk.green(`Welcome to electrode-ignite version ${pkg.version}`)
+    );
+  });
+
+  it("Print welcome message for check-ignite tasks", function() {
+    process.argv[2] = "check-ignite";
     electrodeIgnite();
     sinon.assert.callCount(loggerStub, 2);
     assert.equal(
       loggerStub.getCalls()[0].args.toString(),
       chalk.green(`Welcome to electrode-ignite version ${pkg.version}`)
+    );
+    assert.equal(
+      loggerStub.getCalls()[1].args.toString(),
+      chalk.green("Checking latest version available on npm ...")
     );
   });
 });
