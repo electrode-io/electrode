307766cd27acfe54da44ab0d320ba337b5a96fee
Sheng Di <SDi@walmartlabs.com>
2017-10-12 16:44:18 -0700

    Update ignite-core unit test

--- packages/ignite-core/test/spec/menu-items/check-node-npm.spec.js
+++ packages/ignite-core/test/spec/menu-items/check-node-npm.spec.js
@@ -1,9 +1,10 @@
 "use strict";
 
-const checkNodeNpmItem = require("../../../lib/menu-items/check-node-npm");
 const expect = require("chai").expect;
-
-// TODO: add more tests
+const sinon = require("sinon");
+const checkEnv = require("../../../lib/util/check-env");
+const checkNodeNpmItem = require("../../../lib/menu-items/check-node-npm");
+const logger = require("../../../lib/util/logger");
 
 describe("menu-item check-node-npm", function() {
   it("should create menu item", () => {
@@ -11,5 +12,24 @@ describe("menu-item check-node-npm", function() {
     expect(mi.emit).to.exist;
     expect(mi.icon).to.exist;
     expect(mi.execute).to.exist;
+  });
+
+  it("should check node and npm environment", done => {
+    const nodeStub = sinon.stub(checkEnv, "node");
+    const npmStub = sinon.stub(checkEnv, "npm");
+    const loggerStub = sinon.stub(logger, "log");
+
+    const mi = checkNodeNpmItem();
+    mi.emit("post_execute");
+
+    setTimeout(() => {
+      nodeStub.restore();
+      npmStub.restore();
+      loggerStub.restore();
+      expect(nodeStub).to.have.been.calledOnce;
+      expect(npmStub).to.have.been.calledOnce;
+      expect(loggerStub).to.have.been.calledOnce;
+      done();
+    }, 30);
   });
 });
--- packages/ignite-core/test/spec/util/check-env.spec.js
+++ packages/ignite-core/test/spec/util/check-env.spec.js
@@ -2,13 +2,59 @@
 
 const checkEnv = require("../../../lib/util/check-env");
 const expect = require("chai").expect;
-
-// TODO: add more tests
+const logger = require("../../../lib/util/logger");
+const sinon = require("sinon");
 
 describe("check-env", function() {
   it("getNpmVersion should get NPM version", () => {
     return checkEnv.getNpmVersion().then(version => {
       expect(version).to.be.not.empty;
+    });
+  });
+
+  describe("node version requirements for electrode", () => {
+    let logs = [];
+    let logStub;
+    beforeEach(() => {
+      logStub = sinon.stub(logger, "log").callsFake(msg => logs.push(msg));
+    });
+    afterEach(() => {
+      logStub.restore();
+    });
+
+    it("electrode should work for node >= 6.0.0.", () => {
+      logs = [];
+      checkEnv.node("6.0.0");
+      expect(logs[0]).includes("You are using Node version 6.0.0. Electrode should work for you.");
+    });
+
+    it("electrode should not work for node < 6.0.0.", () => {
+      logs = [];
+      checkEnv.node("5.0.0");
+      expect(logs[0]).includes("You are using Node version 5.0.0. We recommend use Node LTS version 6.");
+    });
+  });
+
+  describe("npm version requirements for electrode", () => {
+    let logs = [];
+    let logStub;
+    beforeEach(() => {
+      logStub = sinon.stub(logger, "log").callsFake(msg => logs.push(msg));
+    });
+    afterEach(() => {
+      logStub.restore();
+    });
+
+    it("electrode should work for npm >= 3.0.0.", () => {
+      logs = [];
+      checkEnv.npm("3.0.0");
+      expect(logs[0]).includes("You are using npm version 3.0.0. Electrode should work for you.");
+    });
+
+    it("electrode should not work for node < 3.0.0.", () => {
+      logs = [];
+      checkEnv.npm("2.0.0");
+      expect(logs[0]).includes("You are using npm version 2.0.0. Electrode requires npm version 3 and up.");
     });
   });
 });
