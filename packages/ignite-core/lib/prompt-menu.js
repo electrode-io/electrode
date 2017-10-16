1e3cac18dd20d2c2ac4ff8912f001ddeb7e453d1
Joel Chen <joel123@gmail.com>
2017-10-16 10:53:06 -0700

    [patch] fix argument parsing
    - strict mode
    - only log title with a valid command

--- packages/ignite-core/lib/prompt-menu.js
+++ packages/ignite-core/lib/prompt-menu.js
@@ -135,6 +135,7 @@ class PromptMenu extends EventEmitter {
           desc: menuText,
           aliases: mi.cliAliases,
           handler: () => {
+            logger.log(this._title);
             logger.log(`Running ${menuText}`);
             return this.runMenuItem(mi);
           }
@@ -142,6 +143,7 @@ class PromptMenu extends EventEmitter {
       },
       Yargs
     )
+      .strict()
       .help()
       .usage(`Usage: ${this._progName} <command>`)
       .parse(argv);
