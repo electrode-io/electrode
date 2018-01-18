e2373947a8bb8c1f8a272168071a3f3d87cc9650
Joel Chen <xchen@walmartlabs.com>
2018-01-17 16:02:56 -0800

    remove files no longer needed

--- tools/logger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const VisualLogger = require("visual-logger");
-
-if (process.env.CI) {
-  console.log("CI env detected");
-}
-
-module.exports = new VisualLogger().setItemType(process.env.CI ? "none" : "normal");
