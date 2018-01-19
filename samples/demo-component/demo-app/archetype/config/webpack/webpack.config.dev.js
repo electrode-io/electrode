9bdfefdc5b4b5876f67e246080e34ed6eb8a9ad9
Sheng Di <SDi@walmartlabs.com>
2018-01-19 11:34:54 -0800

    update sample demo component

--- samples/demo-component/demo-app/archetype/config/webpack/webpack.config.dev.js
+++ samples/demo-component/demo-app/archetype/config/webpack/webpack.config.dev.js
@@ -5,7 +5,8 @@ const repoPackagesDir = Path.join(__dirname, "../../../../packages");
 module.exports = {
   resolve: {
     alias: {
-      "demo-component": Path.join(repoPackagesDir, "demo-component/src")
+      "demo-component": Path.join(repoPackagesDir, "demo-component/src"),
+      "react": Path.resolve(__dirname, "../../../node_modules", "react")
     },
     modules: [
       Path.join(repoPackagesDir, "demo-component"),
--- samples/demo-component/demo-app/archetype/config/webpack/webpack.config.js
+++ samples/demo-component/demo-app/archetype/config/webpack/webpack.config.js
@@ -6,7 +6,8 @@ const repoPackagesDir = Path.join(__dirname, "../../../../packages");
 module.exports = {
   resolve: {
     alias: {
-      "demo-component": Path.join(repoPackagesDir, "demo-component/src")
+      "demo-component": Path.join(repoPackagesDir, "demo-component/src"),
+      "react": Path.resolve(__dirname, "../../../node_modules", "react")
     },
     modules: [
       Path.join(repoPackagesDir, "demo-component"),
