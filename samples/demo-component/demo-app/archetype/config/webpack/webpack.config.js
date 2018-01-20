e8047caa29de1a7b213dfe29ab9889260d0ee6f8
Sheng Di <SDi@walmartlabs.com>
2018-01-19 20:26:37 -0800

    add react-dom to webpack-alias

--- samples/demo-component/demo-app/archetype/config/webpack/webpack.config.js
+++ samples/demo-component/demo-app/archetype/config/webpack/webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
   resolve: {
     alias: {
       "demo-component": Path.join(repoPackagesDir, "demo-component/src"),
-      "react": Path.resolve(__dirname, "../../../node_modules", "react")
+      "react": Path.resolve(__dirname, "../../../node_modules", "react"),
+      "react-dom": Path.resolve(__dirname, "../../../node_modules", "react-dom")
     },
     modules: [
       Path.join(repoPackagesDir, "demo-component"),
