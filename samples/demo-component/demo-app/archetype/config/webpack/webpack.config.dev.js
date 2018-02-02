ff7822f6797d045a743807cc5761c564e93c777a
Sheng Di <SDi@walmartlabs.com>
2018-02-02 11:13:46 -0800

    Consumes component from demo folder

--- samples/demo-component/demo-app/archetype/config/webpack/webpack.config.dev.js
+++ samples/demo-component/demo-app/archetype/config/webpack/webpack.config.dev.js
@@ -5,7 +5,8 @@ const repoPackagesDir = Path.join(__dirname, "../../../../packages");
 module.exports = {
   resolve: {
     alias: {
-      "demo-component": Path.join(repoPackagesDir, "demo-component/src"),
+      "demo-component": Path.join(repoPackagesDir, "demo-component/demo/demo.jsx"),
+      "demo-component-style": Path.join(repoPackagesDir, "demo-component/demo/demo.css"),
       react: Path.dirname(require.resolve("react/package.json")),
       "react-dom": Path.dirname(require.resolve("react-dom/package.json"))
     },
