d13afeddb19d2ba0a1a0182ca88d92faefbf0013
Roman <romitch@gmail.com>
2018-05-15 15:23:14 -0400

    component demo-app webpack config allow importing from `packages/.../lib` (#784)
    * components repo demo-app webconfig alias changes to allow importing internal modules using `lib/...` path
    
    * freeze on yeoman-test@1.7.0 since 1.7.1 contains breaking changes
    
    * removed unnecessary .js extension in webpack config

--- packages/generator-electrode/component/templates/demo-app/archetype/config/webpack/component-alias.config.js
+++ packages/generator-electrode/component/templates/demo-app/archetype/config/webpack/component-alias.config.js
@@ -17,7 +17,8 @@ const config = {
 
 components.forEach(folderName => {
   const packageName = require(Path.join(repoPackagesDir, folderName, "package.json")).name;
-  config.resolve.alias[packageName] = Path.join(repoPackagesDir, folderName, "src");
+  config.resolve.alias[`${packageName}/lib`] = Path.join(repoPackagesDir, folderName, "src");
+  config.resolve.alias[`${packageName}$`] = Path.join(repoPackagesDir, folderName, "src/index");
   config.resolve.modules.push(Path.join(repoPackagesDir, folderName));
   config.resolve.modules.push(Path.join(repoPackagesDir, folderName, "node_modules"));
 });
