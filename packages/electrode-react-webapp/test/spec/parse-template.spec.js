65db005eb529a56d3582173e9a21fc70ccada649
Joel Chen <joel123@gmail.com>
2018-05-31 12:28:25 -0700

    [minor] support properties for tokens

--- packages/electrode-react-webapp/test/spec/parse-template.spec.js
+++ packages/electrode-react-webapp/test/spec/parse-template.spec.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const AsyncTemplate = require("../../lib/async-template");
-const Fs = require("fs");
 const Path = require("path");
 const expect = require("chai").expect;
 
@@ -14,36 +13,72 @@ describe("AsyncTemplate._parseTemplate", function() {
     });
     const expected = [
       { str: "<html>\n\n<head>" },
-      { id: "ssr content", isModule: false, pos: 17, custom: undefined, wantsNext: undefined },
       {
-        id: "webapp header bundles",
+        id: "ssr-content",
+        isModule: false,
+        pos: 17,
+        custom: undefined,
+        wantsNext: undefined,
+        props: {}
+      },
+      {
+        id: "webapp-header-bundles",
         isModule: false,
         pos: 41,
         custom: undefined,
-        wantsNext: undefined
+        wantsNext: undefined,
+        props: {}
       },
       {
-        id: "webapp body bundles",
+        id: "webapp-body-bundles",
         isModule: false,
         pos: 75,
         custom: undefined,
-        wantsNext: undefined
+        wantsNext: undefined,
+        props: {}
       },
-      { id: "page title", isModule: false, pos: 107, custom: undefined, wantsNext: undefined },
       {
-        id: "prefetch bundles",
+        id: "page-title",
+        isModule: false,
+        pos: 107,
+        custom: undefined,
+        wantsNext: undefined,
+        props: {}
+      },
+      {
+        id: "prefetch-bundles",
         isModule: false,
         pos: 130,
         custom: undefined,
-        wantsNext: undefined
+        wantsNext: undefined,
+        props: {}
       },
       { str: `<script>\n    console.log("test")\n  </script>` },
-      { id: "meta tags", isModule: false, pos: 206, custom: undefined, wantsNext: undefined },
-      { id: "#critical css", isModule: true, pos: 228, custom: undefined, wantsNext: undefined },
+      {
+        id: "meta-tags",
+        isModule: false,
+        pos: 206,
+        custom: undefined,
+        wantsNext: undefined,
+        props: {}
+      },
+      {
+        id: "#critical-css",
+        isModule: true,
+        pos: 228,
+        custom: undefined,
+        wantsNext: false,
+        props: {}
+      },
       { str: "</head>\n\n</html>" }
     ];
-    const template = Fs.readFileSync(htmlFile).toString();
-    expect(asyncTemplate._parseTemplate(template)).to.deep.equal(expected);
+    const criticalCssToken = asyncTemplate.tokens[8];
+    expect(criticalCssToken.custom.process()).to.equal(
+      `\ntoken process module critical-css not found\n`
+    );
+    criticalCssToken.custom = undefined;
+
+    expect(asyncTemplate.tokens).to.deep.equal(expected);
   });
 
   it("should parse template with token at end", () => {
@@ -55,35 +90,140 @@ describe("AsyncTemplate._parseTemplate", function() {
 
     const expected = [
       { str: "<html>\n\n<head>" },
-      { id: "ssr content", isModule: false, pos: 17, custom: undefined, wantsNext: undefined },
       {
-        id: "webapp header bundles",
+        id: "ssr-content",
+        isModule: false,
+        pos: 17,
+        custom: undefined,
+        wantsNext: undefined,
+        props: {}
+      },
+      {
+        id: "webapp-header-bundles",
         isModule: false,
         pos: 41,
         custom: undefined,
-        wantsNext: undefined
+        wantsNext: undefined,
+        props: {}
       },
       {
-        id: "webapp body bundles",
+        id: "webapp-body-bundles",
         isModule: false,
         pos: 75,
         custom: undefined,
-        wantsNext: undefined
+        wantsNext: undefined,
+        props: {}
       },
       {
-        id: "prefetch bundles",
+        id: "prefetch-bundles",
         isModule: false,
         pos: 107,
         custom: undefined,
-        wantsNext: undefined
+        wantsNext: undefined,
+        props: {}
       },
       { str: `<script>\n    console.log("test")\n  </script>` },
-      { id: "meta tags", isModule: false, pos: 183, custom: undefined, wantsNext: undefined },
-      { id: "#critical css", isModule: true, pos: 205, custom: undefined, wantsNext: undefined },
+      {
+        id: "meta-tags",
+        isModule: false,
+        pos: 183,
+        custom: undefined,
+        wantsNext: undefined,
+        props: {}
+      },
       { str: "</head>\n\n</html>" },
-      { id: "page title", isModule: false, pos: 246, custom: undefined, wantsNext: undefined }
+      {
+        id: "page-title",
+        isModule: false,
+        pos: 220,
+        custom: undefined,
+        wantsNext: undefined,
+        props: {}
+      }
     ];
-    const template = Fs.readFileSync(htmlFile).toString();
-    expect(asyncTemplate._parseTemplate(template)).to.deep.equal(expected);
+    expect(asyncTemplate.tokens).to.deep.equal(expected);
+  });
+
+  it("should parse template multi line tokens with props", () => {
+    const htmlFile = Path.join(__dirname, "../data/template3.html");
+    const asyncTemplate = new AsyncTemplate({
+      htmlFile,
+      tokenHandlers: "./test/fixtures/token-handler"
+    });
+
+    const expected = [
+      {
+        str: "<html>\n\n<head>"
+      },
+      {
+        id: "ssr-content",
+        isModule: false,
+        pos: 17,
+        props: {
+          attr: ["1", "2", "3"],
+          args: ["a", "b", "c"]
+        },
+        custom: undefined,
+        wantsNext: undefined
+      },
+      {
+        id: "prefetch-bundles",
+        isModule: false,
+        pos: 83,
+        props: {},
+        custom: undefined,
+        wantsNext: undefined
+      },
+      {
+        str: `<script>\n    console.log("test")\n  </script>`
+      },
+      {
+        id: "meta-tags",
+        isModule: false,
+        pos: 167,
+        props: {},
+        custom: undefined,
+        wantsNext: undefined
+      },
+
+      {
+        str: "</head>\n\n</html>"
+      },
+      {
+        id: "page-title",
+        isModule: false,
+        pos: 204,
+        props: {},
+        custom: undefined,
+        wantsNext: undefined
+      }
+    ];
+    expect(asyncTemplate.tokens).to.deep.equal(expected);
+  });
+
+  it("should throw for token with invalid props", () => {
+    const htmlFile = Path.join(__dirname, "../data/template4.html");
+    expect(
+      () =>
+        new AsyncTemplate({
+          htmlFile,
+          tokenHandlers: "./test/fixtures/token-handler"
+        })
+    ).to.throw(
+      "token prefetch-bundles at position 83 has malformed prop 'bad-prop': array missing [."
+    );
+  });
+
+  it("should throw for token with invalid prop name", () => {
+    const htmlFile = Path.join(__dirname, "../data/template5.html");
+    expect(
+      () =>
+        new AsyncTemplate({
+          htmlFile,
+          tokenHandlers: "./test/fixtures/token-handler"
+        })
+    ).to.throw(
+      "token ssr-content at position 17 has malformed prop 'attr[1,2,3]': name must be 'name='."
+    );
   });
 });
