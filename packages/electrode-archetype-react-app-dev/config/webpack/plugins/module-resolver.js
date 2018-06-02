36611f1a585ead0831e546356a73f8d8930fc4f2
Joel Chen <joel123@gmail.com>
2018-06-01 22:28:11 -0700

    extract node resolver to common module in lib

--- packages/electrode-archetype-react-app-dev/config/webpack/plugins/module-resolver.js
+++ packages/electrode-archetype-react-app-dev/config/webpack/plugins/module-resolver.js
@@ -1,8 +1,7 @@
 "use strict";
 
 const Path = require("path");
-const requireAt = require("require-at");
-const optionalRequire = require("optional-require");
+const nodeResolver = require("../../../lib/node-resolver");
 
 /*
  * This is a webpack module resolver plugin to ensure that modules are resolved from
@@ -16,37 +15,10 @@ module.exports = class ModuleResolver {
     this.target = target;
   }
 
-  isModuleRequest(request) {
-    //
-    // If request is not an absolute/relative path then it's requiring a
-    // nodejs module.
-    //
-    if (Path.isAbsolute(request)) return false;
-
-    if (request === "." || request === "..") {
-      return false;
-    }
-
-    if (request.startsWith("../") || request.startsWith("./")) {
-      return false;
-    }
-
-    if (Path.sep !== "/") {
-      return !request.startsWith("." + Path.sep) || !request.startsWith(".." + Path.sep);
-    }
-
-    return true;
-  }
-
   apply(resolver) {
     const archetypeDir = Path.join(__dirname, "..", "..", "..");
 
     resolver.plugin(this.source, (req, callback) => {
-      //
-      // Let webpack's internal resolver deal with requiring non-modules
-      //
-      if (!this.isModuleRequest(req.request)) return callback();
-
       //
       // If require is NOT originated from a dir within node_modules, then
       // call require.resolve from within the archetype's directory
@@ -54,32 +26,14 @@ module.exports = class ModuleResolver {
       //
       const atPath = req.path.indexOf(this.path) < 0 ? archetypeDir : req.path;
 
-      //
-      // If request is requiring for a module, then first try to resolve the
-      // module's real path by looking for its package.json, and then use that
-      // with request converted to a relative path without the module name part.
-      //
-      const splits = req.request.split("/");
-      const nameX = splits[0].startsWith("@") ? 2 : 1; // check for scoped module
-      const name = splits.slice(0, nameX).join("/");
-      //
-      // All modules must have package.json
-      //
-      const resolved = optionalRequire.resolve(
-        requireAt(atPath),
-        // ensure require request paths are POSIX
-        Path.posix.join(name, "package.json"),
-        false
-      );
+      const result = nodeResolver.resolve(req.request, atPath);
 
-      if (!resolved) return callback();
+      //
+      // Let webpack's internal resolver deal with requiring non-modules
+      //
+      if (result === undefined) return callback();
 
-      splits.splice(0, nameX, ".");
-
-      const obj = Object.assign({}, req, {
-        path: Path.dirname(resolved),
-        request: splits.join("/")
-      });
+      const obj = Object.assign({}, req, result);
 
       return resolver.doResolve(
         this.target,
