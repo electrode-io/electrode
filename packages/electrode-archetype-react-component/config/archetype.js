56fd3254f67ffd0a9e2d58e8d538f22dfc88e2af
Joel Chen <xchen@walmartlabs.com>
2018-06-04 11:12:03 -0700

    allow karma dependencies to be turned off

--- packages/electrode-archetype-react-component/config/archetype.js
+++ packages/electrode-archetype-react-component/config/archetype.js
@@ -5,9 +5,44 @@ const optionalRequire = require("optional-require")(require);
 const pkg = require("../package.json");
 const dir = Path.join(__dirname, "..");
 
+//
+// Find the host component's dir that's depending on this archetype,
+// by checking for the occurence of the first node_modules in the path,
+// since process.cwd() may not necessary be accurate potentially.
+// Generally CWD is required to be the same, but this will let archetype
+// to be certain.
+//
+function getHostDir() {
+  const dir = __dirname;
+  const firstNmIdx = dir.indexOf("node_modules");
+  if (firstNmIdx > 0) {
+    return dir.substr(0, firstNmIdx - 1); // -1 to strip the trailing \ or /
+  }
+  return process.cwd();
+}
+
+const hostDir = getHostDir();
+
+if (process.cwd() !== hostDir) {
+  // TODO: add logger from app archetype to component archetype
+  console.log(
+    "electrode-archetype-react-component: determined your component's dir to be",
+    hostDir
+  );
+}
+
+const userConfig = Object.assign(
+  {
+    options: { karma: true }
+  },
+  optionalRequire(Path.join(hostDir, "archetype/config"))
+);
+
 module.exports = {
   dir,
   pkg,
+  hostDir,
+  options: userConfig.options,
   webpack: {
     devHostname: "localhost",
     devPort: 2992,
