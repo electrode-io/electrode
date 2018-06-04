1810528ccb354d5be1197ce313837a78378f6f29
Joel Chen <jchip@users.noreply.github.com>
2018-06-04 13:55:41 -0700

    allow karma dependencies to be turned off (#800)

--- packages/electrode-archetype-react-component/archetype-clap.js
+++ packages/electrode-archetype-react-component/archetype-clap.js
@@ -91,145 +91,173 @@ function lint(options) {
   return Promise.resolve(commands.filter(x => x));
 }
 
-const tasks = {
-  // env setup common tasks
-  "~production-env": () => setProductionEnv(),
+/*
+ *
+ * For information on how to specify a task, see:
+ *
+ * https://www.npmjs.com/package/xclap
+ *
+ */
 
-  // code build/compile tasks
-  build: ["clean-dist", "build-lib", "build-dist"],
-  "build-dist": ["build-dist-min", "build-lib:clean-tmp"],
-  "build-dist-min": {
-    dep: ["~production-env"],
-    desc: "Build minified dist files for production",
-    task: `webpack --config ${archetype.devPath}/config/webpack/webpack.config.js --colors`
-  },
-  "build-lib": {
-    dep: ["~production-env"],
-    task: [
-      ".prep-tmp-lib",
-      "babel-src-step",
-      "clean-lib",
-      ".tmp-to-lib",
-      "build-lib:flatten-l10n",
-      // TODO: fix the badly written and messy copy-as-flow-declaration.js so it can
-      // deterministically await async complete (and exit if isMain).
-      // "build-lib:copy-flow",
-      "build-lib:clean-tmp"
-    ]
-  },
-  "babel-src-step": `babel -D src -d .tmplib`,
-  "build-lib:clean-tmp": () => $$.rm("-rf", "./tmp"),
-  "build-lib:copy-flow": `node -r ${archetype.devPath}/scripts/copy-as-flow-declaration.js`,
-  "build-lib:flatten-l10n": flattenMessagesL10n,
+// quick tips name naming and invoking tasks:
+// - task name without . or - are primary tasks
+// - task name starts with . are hidden in help output
+// - when invoking tasks in [], starting name with ? means optional (ie: won't fail if task not found)
 
-  "archetype:check": [
-    "archetype:test-dev-pkg",
-    "clean-test-init",
-    "archetype:test-init-pkg",
-    "clean-test-init"
-  ],
-  "archetype:test-dev-pkg": `pjv -f dev/package.json`,
-  "archetype:test-init-pkg": "pjv -f test-init/package.json",
+function makeTasks(hostDir) {
+  const tasks = {
+    // env setup common tasks
+    ".production-env": () => setProductionEnv(),
 
-  check: ["check-dep", "lint", "test-cov", "build-lib:clean-tmp"],
-  "check-ci": ["check-dep", "lint", "test-ci", "build-lib:clean-tmp"],
-  "check-cov": ["lint", "test-cov"],
-  "check-dep": `ecd -f package.json --cf ${archetype.devPath}/config/dependencies/check.json -w`,
-  "check-dev": ["lint", "test-dev"],
+    // code build/compile tasks
+    build: ["clean-dist", "build-lib", "build-dist"],
+    "build-dist": ["build-dist-min", "build-lib:clean-tmp"],
+    "build-dist-min": {
+      dep: [".production-env"],
+      desc: "Build minified dist files for production",
+      task: `webpack --config ${archetype.devPath}/config/webpack/webpack.config.js --colors`
+    },
+    "build-lib": {
+      dep: [".production-env"],
+      task: [
+        ".prep-tmp-lib",
+        "babel-src-step",
+        "clean-lib",
+        ".tmp-to-lib",
+        "build-lib:flatten-l10n",
+        // TODO: fix the badly written and messy copy-as-flow-declaration.js so it can
+        // deterministically await async complete (and exit if isMain).
+        // "build-lib:copy-flow",
+        "build-lib:clean-tmp"
+      ]
+    },
+    "babel-src-step": `babel -D src -d .tmplib`,
+    "build-lib:clean-tmp": () => $$.rm("-rf", "./tmp"),
+    "build-lib:copy-flow": `node -r ${archetype.devPath}/scripts/copy-as-flow-declaration.js`,
+    "build-lib:flatten-l10n": flattenMessagesL10n,
 
-  ".prep-tmp-lib": () => $$.rm("-rf", ".tmplib"),
-  ".tmp-to-lib": () => $$.mv(".tmplib", "lib"),
-  clean: ["clean-lib", "clean-dist"],
-  "clean-dist": () => $$.rm("-rf", "dist"),
-  "clean-lib": () => $$.rm("-rf", "lib"),
-  "clean-test-init": () => $$.rm("-rf", "test-init"),
+    "archetype:check": [
+      "archetype:test-dev-pkg",
+      "clean-test-init",
+      "archetype:test-init-pkg",
+      "clean-test-init"
+    ],
+    "archetype:test-dev-pkg": `pjv -f dev/package.json`,
+    "archetype:test-init-pkg": "pjv -f test-init/package.json",
 
-  "cov-frontend": () => checkFrontendCov(),
-  "cov-frontend-50": () => checkFrontendCov("50"),
-  "cov-frontend-70": () => checkFrontendCov("70"),
-  "cov-frontend-85": () => checkFrontendCov("85"),
-  "cov-frontend-95": () => checkFrontendCov("95"),
+    check: ["check-dep", "lint", "test-cov", "build-lib:clean-tmp"],
+    "check-ci": ["check-dep", "lint", "test-ci", "build-lib:clean-tmp"],
+    "check-cov": ["lint", "test-cov"],
+    "check-dep": `ecd -f package.json --cf ${archetype.devPath}/config/dependencies/check.json -w`,
+    "check-dev": ["lint", "test-dev"],
 
-  lint: ["lint-react-src", "lint-react-test", "lint-scripts"],
+    ".prep-tmp-lib": () => $$.rm("-rf", ".tmplib"),
+    ".tmp-to-lib": () => $$.mv(".tmplib", "lib"),
+    clean: ["clean-lib", "clean-dist"],
+    "clean-dist": () => $$.rm("-rf", "dist"),
+    "clean-lib": () => $$.rm("-rf", "lib"),
+    "clean-test-init": () => $$.rm("-rf", "test-init"),
 
-  "lint-react-src": {
-    desc: "Run eslint on client code in directories client and templates",
-    task: () =>
-      lint({
-        ext: ".js,.jsx",
-        config: `${archetype.devPath}/config/eslint/.eslintrc-react`,
-        targets: ["src"]
-      })
-  },
+    "cov-frontend": () => checkFrontendCov(),
+    "cov-frontend-50": () => checkFrontendCov("50"),
+    "cov-frontend-70": () => checkFrontendCov("70"),
+    "cov-frontend-85": () => checkFrontendCov("85"),
+    "cov-frontend-95": () => checkFrontendCov("95"),
 
-  "lint-react-test": {
-    desc: "Run eslint in directories test/client and templates",
-    task: () =>
-      lint({
-        ext: ".js,.jsx",
-        config: `${archetype.devPath}/config/eslint/.eslintrc-react-test`,
-        targets: ["test/client"]
-      })
-  },
+    lint: ["lint-react-src", "lint-react-test", "lint-scripts"],
 
-  "lint-scripts": {
-    desc: "Run eslint in directories scripts",
-    task: () =>
-      lint({
-        ext: ".js",
-        config: `${archetype.devPath}/config/eslint/.eslintrc-base`,
-        targets: ["scripts"]
-      })
-  },
+    "lint-react-src": {
+      desc: "Run eslint on client code in directories client and templates",
+      task: () =>
+        lint({
+          ext: ".js,.jsx",
+          config: `${archetype.devPath}/config/eslint/.eslintrc-react`,
+          targets: ["src"]
+        })
+    },
 
-  "npm:prepublish": ["build-lib", "build-dist-min"],
+    "lint-react-test": {
+      desc: "Run eslint in directories test/client and templates",
+      task: () =>
+        lint({
+          ext: ".js,.jsx",
+          config: `${archetype.devPath}/config/eslint/.eslintrc-react-test`,
+          targets: ["test/client"]
+        })
+    },
 
-  "server-test": mkCmd(
-    `webpack-dev-server --port 3001`,
-    `--config ${archetype.devPath}/config/webpack/webpack.config.test.js --colors`
-  ),
+    "lint-scripts": {
+      desc: "Run eslint in directories scripts",
+      task: () =>
+        lint({
+          ext: ".js",
+          config: `${archetype.devPath}/config/eslint/.eslintrc-base`,
+          targets: ["scripts"]
+        })
+    },
 
-  "test-ci": ["test-frontend-ci"],
-  "test-cov": ["karma-test-frontend-cov", "jest-test-frontend-cov"],
-  "test-dev": ["test-frontend-dev"],
-  "test-watch": ["test-frontend-dev-watch"],
-  "concurrent-test-watch": ["hot", "test-frontend-dev-watch"],
-  "test-frontend": `karma start ${archetype.devPath}/config/karma/karma.conf.js --colors`,
-  "test-frontend-ci": mkCmd(
-    `karma start --browsers PhantomJS,Firefox`,
-    `${archetype.devPath}/config/karma/karma.conf.coverage.js --colors`
-  ),
-  "karma-test-frontend-cov": () => {
-    if ($$.test("-d", "test")) {
-      console.log("\nRunning Karma unit tests:\n");
-      return mkCmd(
-        `~$karma start`,
+    "npm:prepublish": ["build-lib", "build-dist-min"],
+
+    "server-test": mkCmd(
+      `webpack-dev-server --port 3001`,
+      `--config ${archetype.devPath}/config/webpack/webpack.config.test.js --colors`
+    ),
+
+    "test-ci": ["?.karma.test-frontend-ci"],
+    "test-cov": ["?.karma.test-frontend-cov", ".jest.test-frontend-cov"],
+    "test-dev": ["?.karma.test-frontend-dev"],
+    "test-watch": ["?.karma.test-frontend-dev-watch"],
+    "concurrent-test-watch": ["hot", "?.karma.test-frontend-dev-watch"],
+    "test-frontend": ["?.karma.test-frontend"],
+    // TODO: need more jest equivalent tasks compare to karma
+    ".jest.test-frontend-cov": () => {
+      const srcJestFiles = glob.sync(`${hostDir}/src/**/\*.{test,spec}.{js,jsx}`);
+
+      if ($$.test("-d", "_test_") || srcJestFiles.length > 0) {
+        console.info("\nRunning jest unit tests:\n");
+        return mkCmd(`~$jest`, `--config ${archetype.devPath}/config/jest/jest.config.js`);
+      }
+      return undefined;
+    }
+  };
+
+  if (archetype.options.karma !== false) {
+    Object.assign(tasks, {
+      ".karma.test-frontend-ci": mkCmd(
+        `karma start --browsers PhantomJS,Firefox`,
         `${archetype.devPath}/config/karma/karma.conf.coverage.js --colors`
-      );
-    }
-    return undefined;
-  },
-  "jest-test-frontend-cov": () => {
-    const srcJestFiles = glob.sync(`${process.cwd()}/src/**/\*.{test,spec}.{js,jsx}`);
+      ),
+      ".karma.test-frontend": `karma start ${
+        archetype.devPath
+      }/config/karma/karma.conf.js --colors`,
+      ".karma.test-frontend-cov": () => {
+        if ($$.test("-d", "test")) {
+          console.log("\nRunning Karma unit tests:\n");
+          return mkCmd(
+            `~$karma start`,
+            `${archetype.devPath}/config/karma/karma.conf.coverage.js --colors`
+          );
+        }
+        return undefined;
+      },
+      ".karma.test-frontend-dev": mkCmd(
+        `karma start`,
+        `${archetype.devPath}/config/karma/karma.conf.dev.js --colors`
+      ),
+      ".karma.test-frontend-dev-watch": mkCmd(
+        `karma start ${archetype.devPath}/config/karma/karma.conf.watch.js`,
+        ` --colors --browsers Chrome --no-single-run --auto-watch`
+      )
+    });
+  } else {
+    console.log("Disabling karma test tasks since archetype config options.karma === false");
+  }
 
-    if ($$.test("-d", "_test_") || srcJestFiles.length > 0) {
-      console.info("\nRunning jest unit tests:\n");
-      return mkCmd(`~$jest`, `--config ${archetype.devPath}/config/jest/jest.config.js`);
-    }
-    return undefined;
-  },
-  "test-frontend-dev": mkCmd(
-    `karma start`,
-    `${archetype.devPath}/config/karma/karma.conf.dev.js --colors`
-  ),
-  "test-frontend-dev-watch": mkCmd(
-    `karma start ${archetype.devPath}/config/karma/karma.conf.watch.js`,
-    ` --colors --browsers Chrome --no-single-run --auto-watch`
-  )
-};
+  return tasks;
+}
 
 module.exports = function(xclap) {
   setupPath();
-  xclap = xclap || requireAt(process.cwd())("xclap") || devRequire("xclap");
-  xclap.load("electrode", tasks);
+  xclap = xclap || requireAt(archetype.hostDir)("xclap") || devRequire("xclap");
+  xclap.load("electrode", makeTasks(archetype.hostDir));
 };
