f313773c7198a3eb776205e13bc73b8b1ea98ea0
Joel Chen <joel123@gmail.com>
2018-07-31 13:11:38 -0700

    format code

--- packages/generator-electrode/generators/config/index.js
+++ packages/generator-electrode/generators/config/index.js
@@ -1,56 +1,53 @@
-'use strict';
-var _ = require('lodash');
-var Generator = require('yeoman-generator');
+"use strict";
+var _ = require("lodash");
+var Generator = require("yeoman-generator");
 
 module.exports = class extends Generator {
   constructor(args, options) {
     super(args, options);
 
-    this.option('name', {
+    this.option("name", {
       type: String,
       required: true,
-      desc: 'Project name'
+      desc: "Project name"
     });
 
-    this.option('pwa', {
-      type: String,
+    this.option("pwa", {
+      type: Boolean,
       required: true,
-      desc: 'Progressive Web App'
+      desc: "Progressive Web App"
     });
 
-    this.option('serverType', {
+    this.option("serverType", {
       type: String,
       required: true,
-      desc: 'Server Type can be hapijs or express'
+      desc: "Server Type can be HapiJS or express"
     });
-    this.option('autoSsr', {
-      type: String,
+
+    this.option("autoSsr", {
+      type: Boolean,
       required: true,
-      desc: 'Automatically disable server side rendering'
+      desc: "Automatically disable server side rendering"
     });
   }
 
   writing() {
-    let routeMatch = (this.options.serverType === 'HapiJS') ? "/{args*}" : "*";
+    let routeMatch = this.options.serverType === "HapiJS" ? "/{args*}" : "*";
     //do not overwrite if file already exists
-    if (!this.fs.exists(this.destinationPath('config/default.js'))) {
-      this.fs.copyTpl(
-        this.templatePath('default.js'),
-        this.destinationPath('config/default.js'),
-        {
-          projectName: this.options.name,
-          routeValue: routeMatch,
-          pwa: this.options.pwa,
-          serverType: this.options.serverType,
-          isAutoSsr: this.options.autoSsr
-        }
-      );
+    if (!this.fs.exists(this.destinationPath("config/default.js"))) {
+      this.fs.copyTpl(this.templatePath("default.js"), this.destinationPath("config/default.js"), {
+        projectName: this.options.name,
+        routeValue: routeMatch,
+        pwa: this.options.pwa,
+        serverType: this.options.serverType,
+        isAutoSSR: this.options.autoSsr
+      });
     }
 
     if (this.options.pwa) {
       this.fs.copyTpl(
-        this.templatePath('sw-config.js'),
-        this.destinationPath('config/sw-config.js'),
+        this.templatePath("sw-config.js"),
+        this.destinationPath("config/sw-config.js"),
         { projectName: this.options.name }
       );
     }
