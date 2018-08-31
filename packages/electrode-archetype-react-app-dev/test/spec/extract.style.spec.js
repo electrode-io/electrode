808eed6bc0feac40aebcbfcbea474f7fd9e03f59
Sheng Di <sdi@usc.edu>
2018-08-30 20:16:40 -0700

    add unit tests for extract-styles.js (#921)

---
+++ packages/electrode-archetype-react-app-dev/test/spec/extract.style.spec.js
@@ -0,0 +1,73 @@
+const expect = require("chai").expect;
+const archetype = require("electrode-archetype-react-app/config/archetype");
+const moduleName = "../../config/webpack/partial/extract-style";
+
+describe("electrode-archetype-react-app-dev extract-styles", () => {
+  afterEach(() => {
+    delete require.cache[require.resolve(moduleName)];
+  });
+
+  describe("webpack config rules name", () => {
+    it("Should enable css modules when cssModuleSupport is true", () => {
+      archetype.webpack.cssModuleSupport = true;
+      const moduleConfig = require(moduleName)().module;
+      expect(moduleConfig.rules[0]._name).to.equal("extract-css-modules");
+      expect(moduleConfig.rules[1]._name).to.equal("extract-css-scss");
+      expect(moduleConfig.rules[2]._name).to.equal("extract-css-stylus");
+    });
+
+    it("Should disable css modules when cssModuleSupport is false", () => {
+      archetype.webpack.cssModuleSupport = false;
+      const moduleConfig = require(moduleName)().module;
+      expect(moduleConfig.rules[0]._name).to.equal("extract-css");
+      expect(moduleConfig.rules[1]._name).to.equal("extract-scss");
+      expect(moduleConfig.rules[2]._name).to.equal("extract-stylus");
+    });
+
+    it("Should enable both stylus & css modules when cssModuleStylusSupport is true", () => {
+      archetype.webpack.cssModuleSupport = false;
+      archetype.webpack.cssModuleStylusSupport = true;
+      const moduleConfig = require(moduleName)().module;
+      expect(moduleConfig.rules[0]._name).to.equal("extract-css");
+      expect(moduleConfig.rules[1]._name).to.equal("extract-scss");
+      expect(moduleConfig.rules[2]._name).to.equal("extract-stylus");
+      expect(moduleConfig.rules[3]._name).to.equal("extract-css-stylus");
+    });
+  });
+
+  describe("enableShortenCSSNames", () => {
+    it("Should shorten css names if both cssModuleSupport & enableShortenCSSNames are true", () => {
+      process.env.NODE_ENV = "production";
+      archetype.webpack.cssModuleSupport = true;
+      archetype.webpack.enableShortenCSSNames = true;
+      const moduleConfig = require(moduleName)().module;
+      expect(moduleConfig.rules[0].loader[2].loader).to.include("[hash:base64:5]");
+      delete process.env.NODE_ENV;
+    });
+
+    it("Should not shorten css names if cssModuleSupport is false", () => {
+      archetype.webpack.cssModuleSupport = false;
+      archetype.webpack.enableShortenCSSNames = true;
+      const moduleConfig = require(moduleName)().module;
+      expect(moduleConfig.rules[0].loader[2].loader).to.not.include("[hash:base64:5]");
+    });
+  });
+
+  describe("webpack config rules loader", () => {
+    it("Should enable cssnext if cssModuleSupport is true", () => {
+      archetype.webpack.cssModuleSupport = true;
+      const moduleConfig = require(moduleName)().plugins;
+      const webpackLoaderOptions = moduleConfig[2].options.options;
+      expect(webpackLoaderOptions.postcss()).to.be.an("array").that.is.not.empty;
+      expect(webpackLoaderOptions.stylus.use).to.be.an("array").that.is.empty;
+    });
+
+    it("Should enable stylus autoprefixer if cssModuleSupport is false", () => {
+      archetype.webpack.cssModuleSupport = false;
+      const moduleConfig = require(moduleName)().plugins;
+      const webpackLoaderOptions = moduleConfig[2].options.options;
+      expect(webpackLoaderOptions.postcss()).to.be.an("array").that.is.empty;
+      expect(webpackLoaderOptions.stylus.use).to.be.an("array").that.is.not.empty;
+    });
+  });
+});
