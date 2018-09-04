5ed3209e75ac2674dd5338da7bc88755884ed796
Joel Chen <xchen@walmartlabs.com>
2018-09-04 00:57:25 -0700

    undo careless mistakes

--- packages/electrode-archetype-react-app-dev/lib/webpack-dev-hapi.js
+++ packages/electrode-archetype-react-app-dev/lib/webpack-dev-hapi.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /* eslint-disable no-console, no-magic-numbers, max-statements */
-/* eslint-disable max-params, prefer-template, complexity */
+/* eslint-disable max-params, prefer-template, complexity, global-require */
 const Path = require("path");
 const Fs = require("fs");
 const Url = require("url");
@@ -19,8 +19,6 @@ const archetype = require("electrode-archetype-react-app/config/archetype");
 const _ = require("lodash");
 const statsUtils = require("./stats-utils");
 const statsMapper = require("./stats-mapper");
-const archetypeWebpackConfig = Path.join(archetype.config.webpack, "webpack.config.dev.js");
-const config = require(archetypeWebpackConfig);
 
 function register(server, options, next) {
   if (!archetype.webpack.devMiddleware) {
@@ -29,6 +27,9 @@ function register(server, options, next) {
     );
     return next();
   }
+
+  const archetypeWebpackConfig = Path.join(archetype.config.webpack, "webpack.config.dev.js");
+  const config = require(archetypeWebpackConfig);
 
   const webpackHotOptions = _.merge(
     {
@@ -297,11 +298,11 @@ ${jumpToError}</body></html>
         return serveStatic(cwdContextBaseUrl, devMiddleware.fileSystem, cwdMemIndex, err => {
           return sendHtml(
             `<html><body>
-            <div style="margin-top:50px;padding:20px;border-radius:10px;border:2px solid red;">
-            <h2>Error reading webpack mem fs</h2>
-            <div style="color: red;">${err.message}</div>
-            <h3>check <a href="${reporterUrl}">reporter</a> to see if there're any errors.</h3>
-            </div></body></html>`
+<div style="margin-top:50px;padding:20px;border-radius:10px;border:2px solid red;">
+<h2>Error reading webpack mem fs</h2>
+<div style="color: red;">${err.message}</div>
+<h3>check <a href="${reporterUrl}">reporter</a> to see if there're any errors.</h3>
+</div></body></html>`
           );
         });
       } else if (req.url.startsWith(reporterUrl) || returnReporter) {
