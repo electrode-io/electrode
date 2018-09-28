44b465d0b3f91c32d5876d59504d7a2984dc919e
Sheng Di <sdi@usc.edu>
2018-09-28 11:39:30 -0700

    [major] webpack4: update universal react app sample to work with webpack4 (#965)
    * update sample to work with webpack4
    
    * remove unnecessary comments

--- samples/universal-react-node/xclap.js
+++ samples/universal-react-node/xclap.js
@@ -1,1 +1,22 @@
+/*
+ * Tell Electrode app archetype that you want to use ES6 syntax in your server code
+ */
+
+process.env.SERVER_ES6 = true;
+
+/*
+ * Tell Electrode app archetype that you want to use webpack dev as a middleware
+ * This will run webpack dev server as part of your app server.
+ */
+
+process.env.WEBPACK_DEV_MIDDLEWARE = true;
+
+/*
+ * Tell Electrode app archetype that you want to shorten css names under production env
+ */
+
+process.env.ENABLE_SHORTEN_CSS_NAMES = true;
+
 require("electrode-archetype-react-app")();
+
+//
