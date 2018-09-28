563dac7e4b77950419bd083235b19994a1aaff69
Sheng Di <sdi@walmartlabs.com>
2018-09-27 17:09:04 -0700

    update sample to work with webpack4

--- samples/universal-react-node/config/default.js
+++ samples/universal-react-node/config/default.js
@@ -1,5 +1,21 @@
 module.exports = {
   plugins: {
+    "webpack-dev": {
+      module: "electrode-archetype-react-app-dev/lib/webpack-dev-hapi",
+      enable: process.env.WEBPACK_DEV_MIDDLEWARE === "true" && process.env.WEBPACK_DEV === "true",
+      options: {
+        // webpack dev middleware options
+        dev: {
+          // user reporter that's called by the archetype's reporter
+          reporter: reporterOptions => {
+            // For example, you can start app server with `clap devbrk` and attach
+            // to it with chrome://inspect, and then enable this debugger statement
+            // so chrome stop here every time webpack middleware finish compiling
+            // debugger
+          }
+        }
+      }
+    },
     "electrode-csrf-jwt": {
       options: {
         secret: "shhhhhh",
