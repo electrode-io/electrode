5463dbd23ea9afb21d22a150bf4594c2ec5d76af
Joel Chen <xchen@walmartlabs.com>
2018-10-02 15:37:06 -0700

    verify installed modules against what's packed in DLL

--- packages/electrode-archetype-react-app-dev/config/webpack/partial/dll-load.js
+++ packages/electrode-archetype-react-app-dev/config/webpack/partial/dll-load.js
@@ -30,14 +30,64 @@ const findDllManifests = name => {
     const statsFile = Path.join(moduleDir, "dist", `stats${dev}.json`);
     const stats = JSON.parse(Fs.readFileSync(statsFile));
     const dllNames = Object.keys(stats.assetsByChunkName);
-    const manifests = dllNames.map(n => {
-      return Path.join(moduleDir, "dist", `dll_${n}-manifest${dev}.json`);
+    const dllInfo = dllNames.map(n => {
+      return {
+        name: n,
+        moduleName: name,
+        manifest: Path.join(moduleDir, "dist", `dll_${n}-manifest${dev}.json`),
+        versions: Path.join(moduleDir, "dist", `dll_${n}-versions${dev}.json`)
+      };
     });
-    return manifests;
+    return dllInfo;
   } catch (err) {
     logger.error("Unable to resolve Electrode DLL module", name, err.message);
     throw err;
   }
+};
+
+const verifyVersions = info => {
+  const versions = JSON.parse(Fs.readFileSync(info.versions));
+  Object.keys(versions).forEach(modName => {
+    let pkg;
+    try {
+      pkg = require(Path.join(modName, "package.json"));
+    } catch (err) {
+      logger.info(
+        "Electrode DLL module",
+        info.moduleName,
+        "- You don't have module",
+        modName,
+        "installed.  Skipping it."
+      );
+      return;
+    }
+    if (pkg.version !== versions[modName]) {
+      const aParts = pkg.version.split(".");
+      const dParts = versions[modName].split(".");
+      const major = aParts[0] !== dParts[0] ? "major " : "";
+      logger.error(
+        "Electrode DLL module",
+        info.moduleName,
+        "DLL",
+        info.name,
+        `has different ${major}versions for`,
+        modName,
+        versions[modName],
+        "yours: ",
+        pkg.version
+      );
+    } else {
+      logger.verbose(
+        "Electrode DLL module",
+        info.moduleName,
+        "DLL",
+        info.name,
+        `has matched versions for`,
+        modName,
+        pkg.version
+      );
+    }
+  });
 };
 
 const loadDllManifest = name => {
@@ -47,26 +97,28 @@ const loadDllManifest = name => {
 module.exports = function() {
   const loadDlls = archetype.webpack.loadDlls;
 
-  const dlls = Object.keys(loadDlls).filter(x => loadDlls[x] && loadDlls[x].enable !== false);
+  const dllMods = Object.keys(loadDlls).filter(x => loadDlls[x] && loadDlls[x].enable !== false);
 
-  if (_.isEmpty(dlls)) return {};
+  if (_.isEmpty(dllMods)) return {};
 
-  logger.info("Electrode DLL modules to be loaded", dlls.join(" "));
+  logger.info("Electrode DLL modules to be loaded", dllMods.join(" "));
 
-  const manifests = dlls.reduce((mani, dllName) => mani.concat(findDllManifests(dllName)), []);
+  const dllInfo = dllMods.reduce((mani, modName) => mani.concat(findDllManifests(modName)), []);
 
-  if (_.isEmpty(manifests)) {
+  if (_.isEmpty(dllInfo)) {
     logger.warn("Electrode DLL found no manifests to load");
     return {};
   }
 
-  logger.verbose("Electrode DLL manifests", manifests.join(", "));
+  logger.verbose("Electrode DLL manifests", dllInfo.map(x => x.manifest).join(", "));
+
+  dllInfo.forEach(verifyVersions);
 
   return {
-    plugins: manifests.map(filename => {
+    plugins: dllInfo.map(info => {
       return new webpack.DllReferencePlugin({
         context: process.cwd(),
-        manifest: loadDllManifest(filename)
+        manifest: loadDllManifest(info.manifest)
       });
     })
   };
