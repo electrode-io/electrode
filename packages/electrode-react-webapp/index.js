e493852e5dfea619042b9790e66b812bd24fa29b
Dat Vong <Dat.Vong@walmartlabs.com>
2018-10-13 14:34:15 -0700

    Ad support for hapi 17

--- packages/electrode-react-webapp/index.js
+++ packages/electrode-react-webapp/index.js
@@ -1,12 +1,15 @@
 "use strict";
 
 const hapiPlugin = require("./lib/hapi");
+const hapiPlugin17 = require("./lib/hapi/index17");
 const middleware = require("./lib/express");
 const ReactWebapp = require("./lib/react-webapp");
 const AsyncTemplate = require("./lib/async-template");
+const { isHapi17 } = require("electrode-hapi-compat");
 
 module.exports = {
-  register: hapiPlugin.register, // for hapi
+  register: isHapi17() ? hapiPlugin17.register : hapiPlugin.register, // for hapi
+  pkg: isHapi17() ? hapiPlugin17.pkg : undefined,
   middleware, // for express
   hapiRegisterRoutes: hapiPlugin.registerRoutes,
   ReactWebapp,
