b7f3b5b791ec24515dd19fc79a35c4635a879356
Joel Chen <xchen@walmartlabs.com>
2018-10-19 13:06:51 -0700

    fix DLL loading for karma

--- packages/electrode-archetype-react-app-dev/config/karma/util/load-electrode-dll.js
+++ packages/electrode-archetype-react-app-dev/config/karma/util/load-electrode-dll.js
@@ -1,22 +1,20 @@
 "use strict";
 
-const Fs = require("fs");
-const Path = require("path");
+const dllUtil = require("../../webpack/util/dll-util");
 
 module.exports = () => {
   const tag = process.env.NODE_ENV === "production" ? "" : ".dev";
 
-  const dllAssetsFile = Path.resolve(`dist/electrode-dll-assets${tag}.json`);
-
   const dllPaths = [];
 
   try {
-    const dllAssets = JSON.parse(Fs.readFileSync(dllAssetsFile));
+    const dll = dllUtil.loadAssets();
+    const dllAssets = dll.assets;
     Object.keys(dllAssets).forEach(m => {
       const dllMod = dllAssets[m];
-      Object.keys(dllMod).forEach(dll => {
-        if (dll === "cdnMapping") return;
-        const assets = dllMod[dll].assets;
+      Object.keys(dllMod).forEach(modName => {
+        if (modName === "cdnMapping") return;
+        const assets = dllMod[modName].assets;
         assets.forEach(a => {
           if (a.endsWith(`${tag}.js`)) {
             dllPaths.push(`${m}/dist/${a}`);
