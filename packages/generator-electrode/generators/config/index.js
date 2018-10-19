5a4332c163f8e1679e487dbd2ad726cdd14d3437
Joel Chen <xchen@walmartlabs.com>
2018-10-18 23:16:15 -0700

    update express server for app generator

--- packages/generator-electrode/generators/config/index.js
+++ packages/generator-electrode/generators/config/index.js
@@ -39,11 +39,16 @@ module.exports = class extends Generator {
 
   writing() {
     const extended = this.options._extended;
+
+    const isHapi = this.options.serverType === "HapiJS";
+    const dir = this.options.serverType.toLowerCase();
+
     // if another top level generator extended parent, then don't copy config/default.js
     // do not overwrite if file already exists
-    if (!extended && !this.fs.exists(this.destinationPath("config/default.js"))) {
-      const routeMatch = this.options.serverType === "HapiJS" ? "/{args*}" : "*";
-      this.fs.copyTpl(this.templatePath("default.js"), this.destinationPath("config/default.js"), {
+    const destPath = this.destinationPath(`config/default.js`);
+    if (!extended && !this.fs.exists(destPath)) {
+      const routeMatch = isHapi ? "/{args*}" : "*";
+      this.fs.copyTpl(this.templatePath(`${dir}/default.js`), destPath, {
         projectName: this.options.name,
         routeValue: routeMatch,
         pwa: this.options.pwa,
@@ -53,7 +58,12 @@ module.exports = class extends Generator {
     }
 
     // copying pwa config/sw-config even if parent has been extended
-    if (this.options.pwa && !this.fs.exists(this.destinationPath("config/sw-config.js"))) {
+    // only got PWA plugin for Hapi for now
+    if (
+      isHapi &&
+      this.options.pwa &&
+      !this.fs.exists(this.destinationPath("config/sw-config.js"))
+    ) {
       this.fs.copyTpl(
         this.templatePath("sw-config.js"),
         this.destinationPath("config/sw-config.js"),
