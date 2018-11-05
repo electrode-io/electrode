fd203513d0426928de1b2c9c6085fad16690cc0a
Sheng Di <sdi@walmartlabs.com>
2018-11-04 16:56:13 -0800

    Extract styles update
     Please enter the commit message for your changes. Lines starting

--- packages/electrode-archetype-react-component-dev/config/webpack/partial/extract-style.js
+++ packages/electrode-archetype-react-component-dev/config/webpack/partial/extract-style.js
@@ -1,13 +1,12 @@
 "use strict";
 
 const Path = require("path");
-const glob = require("glob");
 const optionalRequire = require("optional-require")(require);
 
 const atImport = require("postcss-import");
-const cssnext = require("postcss-cssnext");
 const webpack = require("webpack");
 const autoprefixer = require("autoprefixer-stylus");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const styleLoader = require.resolve("style-loader");
 const cssLoader = require.resolve("css-loader");
@@ -18,12 +17,12 @@ const demoAppPath = Path.resolve(process.cwd(), "..", "..", "demo-app");
 const archetypeAppPath = Path.resolve(demoAppPath, "archetype", "config");
 
 const archetypeApp = optionalRequire(archetypeAppPath) || { webpack: {}, options: {} };
-const archetypeAppWebpack = archetypeApp.webpack;
-let cssModuleSupport = archetypeAppWebpack.cssModuleSupport;
-const cssModuleStylusSupport = archetypeAppWebpack.cssModuleStylusSupport;
 
-const sassSupport = archetypeApp.options && archetypeApp.options.sass;
-const sassLoader = sassSupport === false ? "" : require.resolve("sass-loader");
+console.log("xxxxxxxxx", optionalRequire(archetypeAppPath));
+
+const archetypeAppWebpack = archetypeApp.webpack;
+const cssModuleSupport = archetypeAppWebpack.cssModuleSupport;
+const cssModuleStylusSupport = archetypeAppWebpack.cssModuleStylusSupport;
 
 /*
  * cssModuleSupport: false
@@ -37,87 +36,144 @@ const sassLoader = sassSupport === false ? "" : require.resolve("sass-loader");
  * case 3: *only* *.scss => normal CSS => CSS-Modules + CSS-Next
  */
 
-const cssLoaderOptions = "?modules&localIdentName=[name]__[local]___[hash:base64:5]&-autoprefixer";
-const cssQuery = `${styleLoader}!${cssLoader}!${postcssLoader}`;
-const stylusQuery = `${styleLoader}!${cssLoader}?-autoprefixer!${stylusLoader}`;
-const scssQuery = `${cssQuery}!${sassLoader}`;
-const cssModuleQuery = `${styleLoader}!${cssLoader}${cssLoaderOptions}!${postcssLoader}`;
-const cssStylusQuery = `${cssModuleQuery}!${stylusLoader}`;
-const cssScssQuery = `${cssModuleQuery}!${sassLoader}`;
-
-const cssExists = glob.sync(Path.resolve(process.cwd(), "src/styles", "*.css")).length > 0;
-const stylusExists = glob.sync(Path.resolve(process.cwd(), "src/styles", "*.styl")).length > 0;
-const scssExists = glob.sync(Path.resolve(process.cwd(), "src/styles", "*.scss")).length > 0;
-
 const rules = [];
 
 /*
- * cssModuleSupport default to undefined
- *
- * when cssModuleSupport not specified:
- * *only* *.css, cssModuleSupport sets to true
- * *only* *.styl, cssModuleSupport sets to false
- * *only* *.scss, cssModuleSupport sets to false
+ * css Loader
  */
-if (cssModuleSupport === undefined) {
-  cssModuleSupport = cssExists && !stylusExists && !scssExists;
-}
+const cssQuery = {
+  loader: cssLoader,
+  options: {
+    minimize: true
+  }
+};
+
+/*
+ * css-modules Loader
+ */
+const getCSSModuleOptions = () => {
+  const enableShortenCSSNames = archetypeAppWebpack.enableShortenCSSNames;
+  const enableShortHash = process.env.NODE_ENV === "production" && enableShortenCSSNames;
+  const localIdentName = `${enableShortHash ? "" : "[name]__[local]___"}[hash:base64:5]`;
+
+  return {
+    context: Path.resolve("src"),
+    modules: true,
+    localIdentName
+  };
+};
+
+const cssModuleQuery = {
+  loader: cssLoader,
+  options: getCSSModuleOptions()
+};
+
+/*
+ * postcss Loader
+ *
+ * Note:
+ * - webpack requires an identifier (ident) in options
+ * when {Function}/require is used (Complex Options).
+ */
+const browserslist = ["last 2 versions", "ie >= 9", "> 5%"];
+const postcssQuery = {
+  loader: postcssLoader,
+  options: {
+    ident: "postcss",
+    plugins: loader => [
+      autoprefixer({
+        browsers: browserslist
+      }),
+      atImport({ root: loader.resourcePath }),
+      postcssPresetEnv({ browsers: browserslist })
+    ]
+  }
+};
+
+/*
+ * sass Loader
+ */
+const getSassLoader = () => {
+  console.log("xxxxxxxxxxxxxx", archetypeApp);
+  if (archetypeApp.options.sass) {
+    const sassLoader = require.resolve("sass-loader");
+    return sassLoader;
+  }
+  return "";
+};
+
+const sassQuery = {
+  loader: getSassLoader()
+};
+
+/*
+ * stylus Loader
+ */
+const stylusQuery = {
+  loader: stylusLoader
+};
 
 module.exports = function() {
-  rules.push(
-    {
-      test: /\.css$/,
-      loader: cssModuleSupport ? cssModuleQuery : cssQuery
-    },
-    {
-      test: /\.scss$/,
-      loader: cssModuleSupport ? cssScssQuery : scssQuery
-    },
-    {
-      test: /\.styl$/,
-      loader: cssModuleSupport ? cssStylusQuery : stylusQuery
-    }
-  );
+  rules.push({
+    _name: `extract-css${cssModuleSupport ? "-modules" : ""}`,
+    test: /\.css$/,
+    use: ExtractTextPlugin.extract({
+      fallback: styleLoader,
+      use: cssModuleSupport ? [cssModuleQuery, postcssQuery] : [cssQuery, postcssQuery],
+      publicPath: ""
+    })
+  });
+
+  if (archetypeApp.options.sass) {
+    rules.push({
+      _name: `extract${cssModuleSupport ? "-css" : ""}-scss`,
+      test: /\.(scss|sass)$/,
+      use: ExtractTextPlugin.extract({
+        fallback: styleLoader,
+        use: cssModuleSupport
+          ? [cssModuleQuery, postcssQuery, sassQuery]
+          : [cssQuery, postcssQuery, sassQuery],
+        publicPath: ""
+      })
+    });
+  }
+
+  rules.push({
+    _name: `extract${cssModuleSupport ? "-css" : ""}-stylus`,
+    test: /\.styl$/,
+    use: ExtractTextPlugin.extract({
+      fallback: styleLoader,
+      use: cssModuleSupport
+        ? [cssModuleQuery, postcssQuery, stylusQuery]
+        : [cssQuery, postcssQuery, stylusQuery],
+      publicPath: ""
+    })
+  });
 
   /*
   *** cssModuleStylusSupport flag is about to deprecate. ***
   * If you want to enable stylus with CSS-Modules + CSS-Next,
   * Please use stylus as your style and enable cssModuleSupport flag instead.
   */
-  if (cssModuleStylusSupport) {
-    rules.push({
-      test: /\.styl$/,
-      loader: cssStylusQuery
-    });
-  }
+ if (cssModuleStylusSupport) {
+  rules.push({
+    _name: "extract-css-stylus",
+    test: /\.styl$/,
+    use: ExtractTextPlugin.extract({
+      fallback: styleLoader,
+      use: [cssModuleQuery, postcssQuery, stylusQuery],
+      publicPath: ""
+    })
+  });
+}
 
   return {
     module: { rules },
     plugins: [
       new webpack.LoaderOptionsPlugin({
+        minimize: true,
         options: {
-          postcss: () => {
-            return cssModuleSupport
-              ? [
-                  atImport,
-                  cssnext({
-                    browsers: ["last 2 versions", "ie >= 9", "> 5%"]
-                  })
-                ]
-              : [];
-          },
-          stylus: {
-            use: !cssModuleSupport
-              ? [
-                  autoprefixer({
-                    browsers: ["last 2 versions", "ie >= 9", "> 5%"]
-                  })
-                ]
-              : [],
-            define: {
-              $tenant: process.env.ELECTRODE_TENANT || "walmart"
-            }
-          }
+          context: Path.resolve("src")
         }
       })
     ].filter(x => !!x)
