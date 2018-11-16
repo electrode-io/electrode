7c4b75550f7d1ea6e7f728c8f1fdcf326edcaa6a
Joel Chen <jchip@users.noreply.github.com>
2018-11-16 14:23:02 -0800

    get module true path by using webpack4 module identifier (#1045)

--- packages/electrode-archetype-react-app-dev/lib/stats-mapper.js
+++ packages/electrode-archetype-react-app-dev/lib/stats-mapper.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const _ = require("lodash");
+
 function extractBundles(stats) {
   // Stats has modules, single bundle
   if (stats.modules) return [stats];
@@ -12,16 +14,28 @@ function extractBundles(stats) {
 }
 
 function getBundles(statsResult) {
+  const mapModules = mod => {
+    if (mod.modules) {
+      return mod.modules.map(mapModules);
+    }
+    const identifier = mod.identifier;
+    const mark = identifier.lastIndexOf("!/");
+    if (mark > 0) {
+      return identifier.substr(mark + 1);
+    }
+    return identifier;
+  };
   // For multi-compiler, stats will be an object with a 'children' array of stats
   const bundles = extractBundles(statsResult.toJson({ errorDetails: false }));
   return bundles.map(stats => {
+    const modules = _.flatMap(stats.modules.map(mapModules));
     return {
       name: stats.name,
       time: stats.time,
       hash: stats.hash,
       warnings: stats.warnings || [],
       errors: stats.errors || [],
-      modules: stats.modules.map(x => x.name)
+      modules
     };
   });
 }
