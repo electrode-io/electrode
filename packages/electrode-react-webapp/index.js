523f61542b69f7c3775fedccfea4a483ce92d0f9
Joel Chen <jchip@users.noreply.github.com>
2018-11-20 13:46:05 -0800

    fix hapi16/17 exports (#1061)

--- packages/electrode-react-webapp/index.js
+++ packages/electrode-react-webapp/index.js
@@ -1,15 +1,13 @@
 "use strict";
 
-const hapiPlugin = require("./lib/hapi");
-const hapiPlugin17 = require("./lib/hapi/index17");
+const hapiPlugin = require("./lib/hapi/");
 const middleware = require("./lib/express");
 const ReactWebapp = require("./lib/react-webapp");
 const AsyncTemplate = require("./lib/async-template");
-const { isHapi17 } = require("electrode-hapi-compat");
 
 module.exports = {
-  register: isHapi17() ? hapiPlugin17.register : hapiPlugin.register, // for hapi
-  pkg: isHapi17() ? hapiPlugin17.pkg : undefined,
+  register: hapiPlugin.register || hapiPlugin, // for hapi
+  pkg: hapiPlugin.pkg, // hapi 17
   middleware, // for express
   hapiRegisterRoutes: hapiPlugin.registerRoutes,
   ReactWebapp,
