7ddbb3b6b6a1ba3d37f22fc7cc39beadc205658f
Joel Chen <xchen@walmartlabs.com>
2019-01-07 22:43:20 -0800

    fix express-server linting errors

--- packages/generator-electrode/generators/app/templates/src/server/express-server.js
+++ packages/generator-electrode/generators/app/templates/src/server/express-server.js
@@ -4,19 +4,29 @@ const Promise = require("bluebird");
 const express = require("express");
 const app = express();
 const path = require("path");
-const _ = require("lodash");
 
 const setStaticPaths = function() {
   app.use(express.static(path.join(__dirname, "../../dist")));
 };
 
+const xrequire = require;
+
+const logger = {
+  log() {
+    console.log.apply(console, arguments); // eslint-disable-line
+  },
+  error() {
+    console.error.apply(console, arguments); // eslint-disable-line
+  }
+};
+
 const setDevMiddleware = config => {
   try {
-    const devSetup = require("electrode-archetype-react-app-dev/lib/webpack-dev-express");
+    const devSetup = xrequire("electrode-archetype-react-app-dev/lib/webpack-dev-express");
     devSetup(app, "http", config.port);
   } catch (err) {
     if (process.env.NODE_ENV !== "production") {
-      console.log("setup dev middleware failed", err);
+      logger.error("setup dev middleware failed", err);
     }
   }
 };
@@ -24,11 +34,11 @@ const setDevMiddleware = config => {
 const setRouteHandler = config =>
   new Promise((resolve, reject) => {
     const webapp = p => (p.startsWith(".") ? path.resolve(p) : p);
-    const registerRoutes = require(webapp(config.webapp.module)); //eslint-disable-line
+    const registerRoutes = xrequire(webapp(config.webapp.module));
 
     return registerRoutes(app, config.webapp.options, err => {
       if (err) {
-        console.error(err); //eslint-disable-line
+        logger.error(err);
         reject(err);
       } else {
         resolve();
@@ -42,8 +52,7 @@ const startServer = config =>
       if (err) {
         reject(err);
       } else {
-        //eslint-disable-next-line
-        console.log(`\nApp listening on port: ${config.port}`);
+        logger.log(`\nApp listening on port: ${config.port}`);
         resolve();
       }
     });
