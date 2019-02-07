8eac8c814e0109e6e04ffe26d2baf6c6e8889a95
Joel Chen <jchip@users.noreply.github.com>
2019-02-07 13:35:55 -0800

    [patch] always save stats to disk in webpack dev mode (#1148)

--- packages/electrode-archetype-react-app-dev/config/webpack/partial/stats.js
+++ packages/electrode-archetype-react-app-dev/config/webpack/partial/stats.js
@@ -1,5 +1,11 @@
 "use strict";
 
+const Fs = require("fs");
+const Path = require("path");
+const mkdirp = require("mkdirp");
+const INDENT = 2;
+const archetype = require("electrode-archetype-react-app/config/archetype");
+
 module.exports = function(opts) {
   const StatsWriterPlugin = require("webpack-stats-plugin").StatsWriterPlugin;
 
@@ -13,6 +19,20 @@ module.exports = function(opts) {
     statsOptions.fields = null;
   }
 
+  if (process.env.WEBPACK_DEV === "true") {
+    //
+    // save a physical version of stats to .etmp/stats.json
+    // in webpack dev server mode.
+    //
+    statsOptions.transform = data => {
+      const dir = archetype.webpack.devArtifactsPath || archetype.eTmpDir;
+      mkdirp.sync(dir);
+      const str = JSON.stringify(data, null, INDENT);
+      Fs.writeFileSync(Path.resolve(dir, "stats.json"), str);
+      return str;
+    };
+  }
+
   if (process.env.OPTIMIZE_STATS === "true") {
     statsOptions.fields = null;
     statsOptions.transform = data => {
@@ -23,6 +43,7 @@ module.exports = function(opts) {
       return JSON.stringify(data, null, 2);
     };
   }
+
   return {
     plugins: [new StatsWriterPlugin(statsOptions)]
   };
