5601eef5ac46cd2d8e01cfa7bfa0c89c4391d254
1846689910 <zuhe75@gmail.com>
2019-03-06 14:06:15 -0800

    add target to babel config

--- packages/electrode-archetype-react-app-dev/config/webpack/partial/output.js
+++ packages/electrode-archetype-react-app-dev/config/webpack/partial/output.js
@@ -1,15 +1,13 @@
 "use strict";
 
 const Path = require("path");
-const { AppMode } = require("electrode-archetype-react-app/config/archetype");
+const { AppMode, babel } = require("electrode-archetype-react-app/config/archetype");
 const inspectpack = process.env.INSPECTPACK_DEBUG === "true";
-let { ENV_TARGET } = process.env;
-
-ENV_TARGET = ENV_TARGET || "default";
+let { target } = babel;
 
 module.exports = {
   output: {
-    path: Path.resolve(ENV_TARGET !== "default" ? `dist-${ENV_TARGET}` : "dist", "js"),
+    path: Path.resolve(target !== "default" ? `dist-${target}` : "dist", "js"),
     pathinfo: inspectpack, // Enable path information for inspectpack
     publicPath: "/js/",
     chunkFilename: "[hash].[name].js",
