7e1bdc0a185eb3a7c03b97a9392da72bb52c92bf
1846689910 <zuhe75@gmail.com>
2019-04-02 16:03:56 -0700

    extract-style & output

--- packages/electrode-archetype-react-app-dev/config/webpack/partial/output.js
+++ packages/electrode-archetype-react-app-dev/config/webpack/partial/output.js
@@ -1,15 +1,18 @@
 "use strict";
 
 const Path = require("path");
-
+const { babel } = require("electrode-archetype-react-app/config/archetype");
 const inspectpack = process.env.INSPECTPACK_DEBUG === "true";
+const { target, envTargets } = babel;
+const hasOtherTargets =
+  Object.keys(envTargets).sort().join(",") !== "default,node";
 
 module.exports = {
   output: {
-    path: Path.resolve("dist", "js"),
+    path: Path.resolve(target !== "default" ? `dist-${target}` : "dist", "js"),
     pathinfo: inspectpack, // Enable path information for inspectpack
     publicPath: "/js/",
-    chunkFilename: "[hash].[name].js",
-    filename: "[name].bundle.[hash].js"
+    chunkFilename: hasOtherTargets ? `${target}.[hash].[name].js` : "[hash].[name].js",
+    filename: hasOtherTargets ? `${target}.[name].bundle.js` : "[name].bundle.[hash].js"
   }
 };
