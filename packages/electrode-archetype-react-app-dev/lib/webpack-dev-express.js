79f50ec52697a1b74a25375fca2c3371e021571c
1846689910 <zuhe75@gmail.com>
2019-04-04 13:25:24 -0700

    modify webpack-dev for express & koa

--- packages/electrode-archetype-react-app-dev/lib/webpack-dev-express.js
+++ packages/electrode-archetype-react-app-dev/lib/webpack-dev-express.js
@@ -2,13 +2,17 @@
 const AppDevMiddleware = require("./app-dev-middleware");
 
 function setup(app) {
-  const middleware = new AppDevMiddleware({});
-  middleware.setup();
-  app.use((req, res, next) => {
-    if (!req.app) req.app = {};
-    req.app.webpackDev = middleware.webpackDev;
-    next();
-  });
+  const { BABEL_ENV, NODE_ENV } = process.env;
+  const isProduction = (BABEL_ENV || NODE_ENV) === "production";
+  if (!isProduction) {
+    const middleware = new AppDevMiddleware({});
+    middleware.setup();
+    app.use((req, res, next) => {
+      if (!req.app) req.app = {};
+      req.app.webpackDev = middleware.webpackDev;
+      next();
+    });
+  }
 }
 
 module.exports = setup;
