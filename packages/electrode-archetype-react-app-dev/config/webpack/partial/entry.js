3f5e6628b9c8191836f43440203907785f74f41a
1846689910 <zuhe75@gmail.com>
2019-04-05 09:30:56 -0700

    fix entry format with enableBabelPolyfill as true

--- packages/electrode-archetype-react-app-dev/config/webpack/partial/entry.js
+++ packages/electrode-archetype-react-app-dev/config/webpack/partial/entry.js
@@ -3,10 +3,10 @@ const Fs = require("fs");
 const optionalRequire = require("optional-require")(require);
 const Path = require("path");
 const archetype = require("electrode-archetype-react-app/config/archetype");
+const _ = require("lodash");
 const AppMode = archetype.AppMode;
 
 const context = Path.resolve(AppMode.src.client);
-const polyfill = archetype.webpack.enableBabelPolyfill;
 
 const logger = require("electrode-archetype-react-app/lib/logger");
 
@@ -32,7 +32,42 @@ function appEntry() {
   return entry || (Fs.existsSync(Path.join(context, "app.js")) ? "./app.js" : "./app.jsx");
 }
 
+function shouldPolyfill() {
+  if (archetype.webpack.enableBabelPolyfill) {
+    const hasMultipleTarget =
+      Object.keys(archetype.babel.envTargets)
+        .sort()
+        .join(",") !== "default,node";
+    if (hasMultipleTarget) {
+      return archetype.babel.target === "default";
+      // for all other targets, disable polyfill
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
+
+function makeEntry() {
+  let entry = appEntry();
+  const polyfill = shouldPolyfill();
+  if (polyfill) {
+    const babelPolyfill = "babel-polyfill";
+    if (_.isArray(entry)) {
+      entry = { main: [babelPolyfill, ...entry] };
+    } else if (_.isObject(entry)) {
+      entry = Object.entries(entry).reduce((prev, [k, v]) => {
+        prev[k] = [babelPolyfill].concat(v);
+        return prev;
+      }, {});
+    } else {
+      entry = { main: [babelPolyfill, entry] };
+    }
+  }
+  return entry;
+}
+
 module.exports = {
   context,
-  entry: polyfill ? { main: ["babel-polyfill", appEntry()] } : appEntry()
+  entry: makeEntry()
 };
