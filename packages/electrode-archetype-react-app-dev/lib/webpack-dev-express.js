191f1f1ecb9532bceeadb2e05f658f2eb2e12176
1846689910 <38680030+1846689910@users.noreply.github.com>
2019-04-04 23:30:40 -0700

    skip dev middleware for production with express and koa (#1199)

--- packages/electrode-archetype-react-app-dev/lib/webpack-dev-express.js
+++ packages/electrode-archetype-react-app-dev/lib/webpack-dev-express.js
@@ -2,13 +2,16 @@
 const AppDevMiddleware = require("./app-dev-middleware");
 
 function setup(app) {
-  const middleware = new AppDevMiddleware({});
-  middleware.setup();
-  app.use((req, res, next) => {
-    if (!req.app) req.app = {};
-    req.app.webpackDev = middleware.webpackDev;
-    next();
-  });
+  const isProduction = process.env.NODE_ENV === "production";
+  if (!isProduction) {
+    const middleware = new AppDevMiddleware({});
+    middleware.setup();
+    app.use((req, res, next) => {
+      if (!req.app) req.app = {};
+      req.app.webpackDev = middleware.webpackDev;
+      next();
+    });
+  }
 }
 
 module.exports = setup;
--- packages/electrode-archetype-react-app-dev/lib/webpack-dev-koa.js
+++ packages/electrode-archetype-react-app-dev/lib/webpack-dev-koa.js
@@ -2,12 +2,15 @@
 const AppDevMiddleware = require("./app-dev-middleware");
 
 function setup(app) {
-  const middleware = new AppDevMiddleware({});
-  middleware.setup();
-  app.use(async (ctx, next) => {
-    ctx.webpackDev = middleware.webpackDev;
-    return next();
-  });
+  const isProduction = process.env.NODE_ENV === "production";
+  if (!isProduction) {
+    const middleware = new AppDevMiddleware({});
+    middleware.setup();
+    app.use(async (ctx, next) => {
+      ctx.webpackDev = middleware.webpackDev;
+      return next();
+    });
+  }
 }
 
 module.exports = setup;
