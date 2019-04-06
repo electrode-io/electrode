3727de917a916e59a822b905f1cb386e1a79ea90
Joel Chen <xchen@walmartlabs.com>
2019-04-06 14:40:59 -0700

    [minor] support using render to stream APIs for SSR

--- packages/electrode-redux-router-engine/test/spec/electrode-server.integration.spec.js
+++ packages/electrode-redux-router-engine/test/spec/electrode-server.integration.spec.js
@@ -3,52 +3,123 @@
 const ReduxRouterEngine = require("../..");
 const expect = require("chai").expect;
 const electrodeServer = require("electrode-server");
+const { runFinally, asyncVerify } = require("run-verify");
+const streamToArray = require("stream-to-array");
 
 require("babel-register");
 
 const routes = require("../routes.jsx").default;
 
 describe("electrode server (Hapi) integration", function() {
-  let server;
-  let engine;
+  const setupServer = async (streaming, withIds) => {
+    let engine;
 
-  before(async () => {
-    server = await electrodeServer({
+    const server = await electrodeServer({
       electrode: { logLevel: "none" },
       connections: { default: { port: 0 } }
     });
+
     server.route({
       method: "get",
       path: "/test",
       handler: async (request, reply) => {
-        if (!engine) engine = new ReduxRouterEngine({ routes });
+        if (!engine) engine = new ReduxRouterEngine({ routes, streaming, withIds });
         const result = await engine.render(request);
-        reply(result);
+        if (streaming) {
+          result.resultType = result.html.constructor.name;
+          streamToArray(result.html, (err, arr) => {
+            result.html = arr.join("");
+            reply(result);
+          });
+        } else {
+          reply(result);
+        }
       }
     });
-  });
 
-  after(done => {
-    server.stop(done);
-  });
+    return server;
+  };
+
+  const closeServer = server => {
+    return (
+      server &&
+      new Promise(resolve => {
+        server.stop(resolve);
+      })
+    );
+  };
 
   it("should render basic test route", () => {
-    return server.inject("/test").then(resp => {
-      expect(resp.result).to.deep.equal({
-        status: 200,
-        html: "<div>Page<div>Home</div></div>",
-        prefetch: "window.__PRELOADED_STATE__ = {};"
-      });
-    });
+    let server;
+    return asyncVerify(
+      () => setupServer(),
+      s => {
+        server = s;
+        return server.inject("/test").then(resp => {
+          expect(resp.result).to.deep.equal({
+            status: 200,
+            html: "<div>Page<div>Home</div></div>",
+            prefetch: "window.__PRELOADED_STATE__ = {};"
+          });
+        });
+      },
+      runFinally(() => closeServer(server))
+    );
   });
 
   it("should render with URL query", () => {
-    return server.inject("/test?foo=bar").then(resp => {
-      expect(resp.result).to.deep.equal({
-        status: 200,
-        html: "<div>Page<div>Home - Query: ?foo=bar</div></div>",
-        prefetch: "window.__PRELOADED_STATE__ = {};"
-      });
-    });
+    let server;
+    return asyncVerify(
+      () => setupServer(),
+      s => {
+        server = s;
+        return server.inject("/test?foo=bar").then(resp => {
+          expect(resp.result).to.deep.equal({
+            status: 200,
+            html: "<div>Page<div>Home - Query: ?foo=bar</div></div>",
+            prefetch: "window.__PRELOADED_STATE__ = {};"
+          });
+        });
+      },
+      runFinally(() => closeServer(server))
+    );
+  });
+
+  it("should render static with streaming", () => {
+    let server;
+    return asyncVerify(
+      () => setupServer(true),
+      s => {
+        server = s;
+        return server.inject("/test").then(resp => {
+          expect(resp.result).to.deep.equal({
+            status: 200,
+            html: "<div>Page<div>Home</div></div>",
+            prefetch: "window.__PRELOADED_STATE__ = {};",
+            resultType: "ReactMarkupReadableStream"
+          });
+        });
+      },
+      runFinally(() => closeServer(server))
+    );
+  });
+
+  it("should render with ids and streaming", () => {
+    let server;
+    return asyncVerify(
+      () => setupServer(true, true),
+      s => {
+        server = s;
+        return server.inject("/test").then(resp => {
+          expect(resp.result).to.deep.equal({
+            status: 200,
+            html: `<div data-reactroot="">Page<div>Home</div></div>`,
+            prefetch: "window.__PRELOADED_STATE__ = {};",
+            resultType: "ReactMarkupReadableStream"
+          });
+        });
+      },
+      runFinally(() => closeServer(server))
+    );
   });
 });
