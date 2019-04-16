dc4e1dac89e57cd56a510b8cc36d3a9c1375bd56
Joel Chen <jchip@users.noreply.github.com>
2019-04-16 09:56:49 -0700

    allow sourcemap to work in local prod mode (#1216)

--- packages/electrode-archetype-react-app-dev/config/webpack/partial/sourcemaps-remote.js
+++ packages/electrode-archetype-react-app-dev/config/webpack/partial/sourcemaps-remote.js
@@ -1,15 +1,13 @@
 "use strict";
 
-const archetype = require("electrode-archetype-react-app/config/archetype");
 const SourceMapDevToolPlugin = require("webpack").SourceMapDevToolPlugin;
 
 module.exports = function() {
-  const host = `http://${archetype.webpack.devHostname}:${archetype.webpack.devPort}`;
   return {
     plugins: [
       new SourceMapDevToolPlugin({
         filename: "../map/[file].map",
-        append: `\n//# sourceMappingURL=${host}/dist/map/[url]`
+        append: `\n//# sourceMappingURL=/map/[url]`
       })
     ]
   };
