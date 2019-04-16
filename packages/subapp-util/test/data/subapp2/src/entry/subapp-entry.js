985eab33a0fde2461b9a00fc9349ffc0f3d09013
1846689910 <zuhe75@gmail.com>
2019-04-16 16:54:31 -0700

    wip

---
+++ packages/subapp-util/test/data/subapp2/src/entry/subapp-entry.js
@@ -0,0 +1,3 @@
+module.exports = {
+  name: "subapp2"
+};
--- packages/subapp-util/test/spec/index.spec.js
+++ packages/subapp-util/test/spec/index.spec.js
@@ -1,5 +1,45 @@
 "use strict";
+const {
+  es6Require,
+  scanSubAppsFromDir
+  // scanSingleSubAppFromDir
+  // getAllSubAppManifest,
+  // registerSubApp,
+  // getSubAppContainer,
+  // loadSubAppByName,
+  // loadSubAppServerByName,
+  // refreshSubAppByName,
+  // refreshAllSubApps
+} = require("../../lib");
+const Path = require("path");
+const Fs = require("fs");
 
 describe("subapp-util", function() {
-  it("test", () => {});
+
+  describe("es6Require", () => {
+    it("should import module", () => {
+      const fs = es6Require("fs");
+      expect(fs).to.equal(Fs);
+    });
+  });
+
+  describe("scanSubAppsFromDir", () => {
+    it("should get subapps in src dir if subapp-manifest.js exists", () => {
+      const path = Path.resolve("test/data/subapp1/src");
+      const subapp = scanSubAppsFromDir(path);
+      expect(subapp.subapp1).to.exist;
+    });
+    it("should get subapps in src dir if subapp-manifest.js does not exist", () => {
+      const path = Path.resolve("test/data/subapp2/src");
+      const subapp = scanSubAppsFromDir(path);
+      expect(Fs.existsSync(Path.resolve("test/data/subapp2/src/entry"))).be.true;
+      expect(subapp.Entry).to.exist;
+    });
+  });
+
+  describe("scanSingleSubAppFromDir", () => {
+    it("should get single sub app", () => {
+
+    });
+  });
 });
