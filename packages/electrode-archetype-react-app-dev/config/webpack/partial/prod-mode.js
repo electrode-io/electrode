b6eaf8bdac8ecc5ef0cdd7e90b4f7f68ea2cc509
1846689910 <zuhe75@gmail.com>
2019-04-26 13:41:27 -0700

    move flag

--- packages/electrode-archetype-react-app-dev/config/webpack/partial/prod-mode.js
+++ packages/electrode-archetype-react-app-dev/config/webpack/partial/prod-mode.js
@@ -1,7 +1,8 @@
 "use strict";
+const archetype = require("electrode-archetype-react-app/config/archetype");
 
 module.exports = function() {
   return {
-    mode: "production"
+    mode: archetype.webpack.minify ? "production" : "development"
   };
 };
