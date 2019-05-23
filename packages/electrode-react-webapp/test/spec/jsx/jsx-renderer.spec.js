fd6f84b9d6de4ca96831895da4682b05bc91fb3f
Joel Chen <xchen@walmartlabs.com>
2019-05-23 12:53:55 -0700

    [patch] track Component rendering depth for JSX template

--- packages/electrode-react-webapp/test/spec/jsx/jsx-renderer.spec.js
+++ packages/electrode-react-webapp/test/spec/jsx/jsx-renderer.spec.js
@@ -1,8 +1,12 @@
+/* @jsx createElement */
+
+/* eslint-disable no-unused-vars */
+
 "use strict";
 
 const Path = require("path");
 const Fs = require("fs");
-const { JsxRenderer, Component, IndexPage } = require("../../..").jsx;
+const { JsxRenderer, Component, IndexPage, createElement } = require("../../..").jsx;
 const Template = require("../../jsx-templates/test1").default;
 const Template2 = require("../../jsx-templates/test2").default;
 const Template3 = require("../../jsx-templates/test3").default;
@@ -43,6 +47,100 @@ describe("Jsx Renderer", function() {
     renderer.initializeRenderer();
     renderer.initializeRenderer(true);
     renderer.initializeRenderer();
+  });
+
+  it("should give top level Components depth 0", () => {
+    const TestDepth = (props, context, scope) => {
+      return `TestDepth: ${scope.depth}`;
+    };
+    const renderer = new JsxRenderer({
+      insertTokenIds: true,
+      templateFullPath: Path.dirname(require.resolve("../../jsx-templates/test1")),
+      // IndexPage doesn't nest its children so depth doesn't increase from it
+      template: (
+        <IndexPage>
+          <html>
+            <head />
+            <body>
+              <TestDepth />
+            </body>
+          </html>
+        </IndexPage>
+      ),
+      tokenHandlers: "./test/fixtures/token-handler"
+    });
+    renderer.initializeRenderer();
+    return renderer.render({}).then(context => {
+      expect(context.output._result).contains("TestDepth: 0");
+    });
+  });
+
+  it("should recreate nest Components and give them depth > 0", () => {
+    const TestDepth2 = (props, context, scope) => {
+      return `TestDepth2: ${scope.depth} elementId: ${scope.element.id}`;
+    };
+
+    const TestDepth1 = (props, context, scope) => {
+      return (
+        <div>
+          {`TestDepth1: ${scope.depth} elementId: ${scope.element.id}`}
+          <TestDepth2
+          // A directly nested Component is created again in every rendering pass
+          />
+          {props.children}
+        </div>
+      );
+    };
+
+    const TestDepth0 = (props, context, scope) => {
+      return (
+        <div>
+          {`TestDepth0: ${scope.depth} elementId: ${scope.element.id}`}
+          {props.children}
+        </div>
+      );
+    };
+
+    const renderer = new JsxRenderer({
+      insertTokenIds: true,
+      templateFullPath: Path.dirname(require.resolve("../../jsx-templates/test1")),
+      // IndexPage doesn't nest its children so depth doesn't increase from it
+      template: (
+        <IndexPage>
+          <html>
+            <head />
+            <body>
+              <TestDepth0>
+                <TestDepth1 />
+              </TestDepth0>
+            </body>
+          </html>
+        </IndexPage>
+      ),
+      tokenHandlers: "./test/fixtures/token-handler"
+    });
+
+    renderer.initializeRenderer();
+    const testRender = () => renderer.render({}).then(context => context.output._result);
+
+    const results = [];
+    return testRender()
+      .then(r => results.push(r))
+      .then(testRender)
+      .then(r => results.push(r))
+      .then(() => {
+        results.forEach(r => {
+          expect(r).contains("TestDepth0: 0");
+          expect(r).contains("TestDepth1: 1");
+          expect(r).contains("TestDepth2: 2");
+        });
+        const regex = /TestDepth2: 2 elementId: ([0-9]+)\n/;
+        const a = parseInt(results[0].match(regex)[1]);
+        const b = parseInt(results[1].match(regex)[1]);
+        expect(a).to.be.above(0);
+        expect(b).to.be.above(0);
+        expect(b).to.be.above(a);
+      });
   });
 
   const test1ExpectedOutput = Fs.readFileSync(
