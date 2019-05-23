2e2c062a3f12128b08fff39b1bc1ac50a85e9613
Joel Chen <xchen@walmartlabs.com>
2019-05-23 13:22:17 -0700

    string directly in JSX

--- packages/electrode-react-webapp/test/jsx-templates/test4.jsx
+++ packages/electrode-react-webapp/test/jsx-templates/test4.jsx
@@ -4,8 +4,8 @@ import { IndexPage, createElement, Require } from "../../lib/jsx";
 
 export default (
   <IndexPage>
-    <Require _id={"./test-token"} name="require1" />
-    <Require _id={"./test-token"} name="require2" />
-    <Require _id={"./test-token"} name="require3" />
+    <Require _id="./test-token" name="require1" />
+    <Require _id="./test-token" name="require2" />
+    <Require _id="./test-token" name="require3" />
   </IndexPage>
 );
