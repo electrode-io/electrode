866802b9b75723b16c01b0dd7dc090bf522284e1
Joel Chen <xchen@walmartlabs.com>
2019-05-23 13:21:20 -0700

    relative path for require

--- packages/electrode-react-webapp/test/jsx-templates/test4.jsx
+++ packages/electrode-react-webapp/test/jsx-templates/test4.jsx
@@ -1,12 +1,11 @@
 /* @jsx createElement */
 
 import { IndexPage, createElement, Require } from "../../lib/jsx";
-import Path from "path";
 
 export default (
   <IndexPage>
-    <Require _id={Path.join(__dirname, "test-token")} name="require1" />
-    <Require _id={Path.join(__dirname, "test-token")} name="require2" />
-    <Require _id={Path.join(__dirname, "test-token")} name="require3" />
+    <Require _id={"./test-token"} name="require1" />
+    <Require _id={"./test-token"} name="require2" />
+    <Require _id={"./test-token"} name="require3" />
   </IndexPage>
 );
