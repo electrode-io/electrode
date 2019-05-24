8bceec16099c4642cf196d86b4d9c3b08182bb84
Joel Chen <jchip@users.noreply.github.com>
2019-05-24 14:23:21 -0700

    [minor] support JSX template for subapp routes (#1254)

---
+++ samples/poc-subapp/src/server-routes/default/index.jsx
@@ -0,0 +1,101 @@
+/* @jsx createElement */
+
+const {
+  IndexPage,
+  createElement,
+  Token,
+  Require,
+  Literal
+} = require("electrode-react-webapp/lib/jsx");
+
+const Template = (
+  <IndexPage DOCTYPE="html">
+    <Token _id="INITIALIZE" />
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        />
+        <Token _id="META_TAGS" />
+        <Token _id="PAGE_TITLE" />
+        <Require _id="subapp-web/lib/init" />
+
+        <Token _id="CRITICAL_CSS" />
+
+        <style>
+          <Literal file={`${__dirname}/style.css`} />
+        </style>
+      </head>
+      <Token _id="HEAD_CLOSED" />
+      <body>
+        <noscript>
+          <h4>JavaScript is Disabled</h4>
+          <p>Sorry, this webpage requires JavaScript to function correctly.</p>
+          <p>Please enable JavaScript in your browser and reload the page.</p>
+        </noscript>
+
+        <div style="background: cyan">
+          <Require
+            _id="subapp-web/lib/load"
+            name="Header"
+            _concurrent
+            timestamp
+            startOnLoad
+            elementId="subapp-header01"
+            serverSideRendering
+            hydrateServerData={false}
+            clientSideRendering={false}
+            inlineScript
+          />
+        </div>
+
+        <div style="background: green">
+          <Require
+            _id="subapp-web/lib/load"
+            _concurrent
+            name="MainBody"
+            timestamp
+            elementId="subapp-mainbody"
+            streaming
+            async
+            hydrateServerData
+            serverSideRendering
+          />
+        </div>
+
+        <Require
+          _id="subapp-web/lib/load"
+          _concurrent
+          timestamp
+          elementId="subapp-bottom"
+          name="Bottom"
+          streaming
+          hydrateServerData
+          async
+          serverSideRendering
+        />
+
+        <Require
+          _id="subapp-web/lib/load"
+          _concurrent
+          timestamp
+          elementId="subapp-footer"
+          name="Footer"
+          async
+          serverSideRendering
+        />
+
+        <Require _id="subapp-web/lib/start" />
+
+        <Require _id="subapp-web/lib/more-data" _concurrent name="MainBody" />
+      </body>
+      <Token _id="BODY_CLOSED" />
+    </html>
+    <Token _id="HTML_CLOSED" />
+  </IndexPage>
+);
+
+export default Template;
--- samples/poc-subapp/src/server-routes/default/route.js
+++ samples/poc-subapp/src/server-routes/default/route.js
@@ -14,5 +14,10 @@ module.exports = {
     "/cart"
   ],
   pageTitle: "Online Store Home",
-  htmlFile: "index.html"
+  // Specify the HTML index generation template
+  // since using JSX template, don't include the extension
+  // because babel transpiled it to .js in prod mode
+  // but in dev mode we still need to be able to load the
+  // .jsx version directly through @babel/register.
+  templateFile: "index"
 };
