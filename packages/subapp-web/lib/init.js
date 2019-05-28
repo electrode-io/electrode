aebd9afc4e86a77876a07ba35673af0ea23d5dc1
Joel Chen <jchip@users.noreply.github.com>
2019-05-28 16:34:18 -0700

    subapp - handle load JS bundles from CDN mapped URLs (#1258)

--- packages/subapp-web/lib/init.js
+++ packages/subapp-web/lib/init.js
@@ -17,30 +17,35 @@ module.exports = function setup(setupContext) {
 
   const routeData = setupContext.routeOptions.__internals;
 
-  const vendorAssets = util.getVendorBundles(routeData.assets);
-  const bundleBase = util.getBundleBase(setupContext.routeOptions);
-  const bundleNames = vendorAssets.map(a => a.chunkNames[0]);
+  const cdnJsBundles = util.getCdnJsBundles(
+    routeData.assets.byChunkName,
+    setupContext.routeOptions
+  );
+
   let vendorBundleLoadJs = "";
   let commonScript = "";
+
+  const vendorAssets = util.getVendorBundles(routeData.assets);
+  const bundleNames = vendorAssets.map(a => a.chunkNames[0]);
+
   if (vendorAssets.length > 0) {
     vendorBundleLoadJs = `markBundlesLoaded(${JSON.stringify(bundleNames)});`;
     commonScript = vendorAssets
-      .map(a => `<script src="${bundleBase}${a.name}" async></script>`)
+      .map(a => {
+        // map all chunk names that has a URL mapped and load them with script tags
+        return a.chunkNames
+          .map(x => cdnJsBundles[x] && `<script src="${cdnJsBundles[x]}" async></script>`)
+          .filter(x => x)
+          .join("");
+      })
       .join("\n");
   }
 
-  const jsBundleByChunkName = Object.entries(routeData.assets.byChunkName).reduce(
-    (a, [name, bundle]) => {
-      a[name] = Array.isArray(bundle) ? bundle.find(x => x.endsWith(".js")) : bundle;
-      return a;
-    },
-    {}
-  );
-
   const bundleAssets = {
-    byChunkName: jsBundleByChunkName,
-    basePath: bundleBase
+    byChunkName: cdnJsBundles,
+    basePath: ""
   };
+
   const webSubAppJs = `\n<script id="bundleAssets" type="application/json">
 ${JSON.stringify(bundleAssets)}
 </script>
