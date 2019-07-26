f0a92f72f01a7f24e34dfabfda7b01753e70e093
1846689910 <zuhe75@gmail.com>
2019-07-26 08:29:47 -0700

    mod

--- packages/electrode-archetype-react-app-dev/config/webpack/partial/loadable.js
+++ packages/electrode-archetype-react-app-dev/config/webpack/partial/loadable.js
@@ -2,6 +2,8 @@ const LoadablePlugin = require("@loadable/webpack-plugin");
 const archetype = require("electrode-archetype-react-app/config/archetype");
 module.exports = function() {
   return {
-    plugins: archetype.babel.enableDynamicImport ? [new LoadablePlugin({ filename: "../server/loadable-stats.json" })] : []
+    plugins: archetype.babel.enableDynamicImport
+      ? [new LoadablePlugin({ filename: "../server/loadable-stats.json" })]
+      : []
   };
 };
