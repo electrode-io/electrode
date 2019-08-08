deb9797881b6a5f7e30cda67d2eeaffea175a25b
Charles Selvarajan <charles.selvarajan@walmartlabs.com>
2019-08-08 03:30:06 -0700

    Set webpack profile only when executing specific tasks

--- packages/electrode-archetype-react-app/arch-clap.js
+++ packages/electrode-archetype-react-app/arch-clap.js
@@ -142,16 +142,16 @@ function removeLogFiles() {
   } catch (e) {} // eslint-disable-line
 }
 
-function checkForCustomWebpackConfig(defaultFile) {
-  const customFilePath = Path.join(process.cwd(), "webpack.config.js");
-  return Fs.existsSync(customFilePath) ? customFilePath : webpackConfig(defaultFile);
+function setWebpackProfile(profile) {
+  process.env.ELECTRODE_WEBPACK_PROFILE = profile || "production";
+  logger.info(`Electrode Webpack Profile set to ${process.env.ELECTRODE_WEBPACK_PROFILE}`);
 }
 
-function setWebpackDevProfile() {
-  const devProfileTasks = ["dev", "server-admin", "wds.dev"];
-  const taskReq = devProfileTasks.includes(process.argv[2] || "");  
-  process.env.ELECTRODE_WEBPACK_PROFILE = taskReq ? "development" : "production";
-  logger.info(`Electrode Webpack Profile set to ${process.env.ELECTRODE_WEBPACK_PROFILE}`);
+function checkForCustomWebpackConfig(defaultFile) {
+  const customFilePath = Path.join(process.cwd(), "webpack.config.js");
+  const webpackConfigFile = Fs.existsSync(customFilePath) ? customFilePath : webpackConfig(defaultFile);
+  logger.info(`Using Webpack Profile @ ${webpackConfigFile}`);
+  return webpackConfigFile;
 }
 
 /*
@@ -428,10 +428,11 @@ function makeTasks(xclap) {
     //
     ".build-browser-coverage-1": () => {
       setProductionEnv();
+      setWebpackProfile("browsercoverage");
       return exec(
         `webpack`,
         `--config`,
-        quote(webpackConfig("webpack.config.browsercoverage.js")),
+        quote(checkForCustomWebpackConfig("webpack.config.browsercoverage.js")),
         `--colors`
       );
     },
@@ -471,11 +472,17 @@ function makeTasks(xclap) {
     "mv-to-dist": ["mv-to-dist:clean", "mv-to-dist:mv-dirs", "mv-to-dist:keep-targets"],
     "build-dist-dev-static": {
       desc: false,
-      task: mkCmd(
-        `webpack --config`,
-        quote(webpackConfig("webpack.config.dev.static.js")),
-        `--colors`,
-        `--display-error-details`
+      task: xclap.exec(
+        [
+          `webpack --config`,
+          quote(checkForCustomWebpackConfig("webpack.config.dev.static.js")),
+          `--colors`,
+          `--display-error-details`
+        ],
+        {
+          xclap: { delayRunMs: 2000 },
+          execOptions: { env: { ELECTRODE_WEBPACK_PROFILE: "static" } }
+        }
       )
     },
 
@@ -512,7 +519,7 @@ function makeTasks(xclap) {
             ],
             {
               xclap: { delayRunMs: index * 2000 },
-              execOptions: { env: { ENV_TARGET: name } }
+              execOptions: { env: { ELECTRODE_WEBPACK_PROFILE: "production", ENV_TARGET: name } }
             }
           )
         )
@@ -912,6 +919,7 @@ Individual .babelrc files were generated for you in src/client and src/server
     "server-admin": {
       desc: "Start development with admin server",
       task: function() {
+        setWebpackProfile("development");
         AppMode.setEnv(AppMode.src.dir);
         // eslint-disable-next-line no-shadow
         const exec = AppMode.isSrc
@@ -930,27 +938,39 @@ Individual .babelrc files were generated for you in src/client and src/server
 
     "wds.dev": {
       desc: "Start webpack-dev-server in dev mode",
-      task: mkCmd(
-        "webpack-dev-server",
-        `--watch --watch-aggregate-timeout 2000`,
-        archetype.webpack.enableHotModuleReload ? `--hot` : ``,
-        `--config`,
-        quote(checkForCustomWebpackConfig("webpack.config.dev.js")),
-        `--progress --colors`,
-        `--port ${archetype.webpack.devPort}`,
-        `--host ${archetype.webpack.devHostname}`
+      task: xclap.exec(
+        [
+          "webpack-dev-server",
+          `--watch --watch-aggregate-timeout 2000`,
+          archetype.webpack.enableHotModuleReload ? `--hot` : ``,
+          `--config`,
+          quote(checkForCustomWebpackConfig("webpack.config.dev.js")),
+          `--progress --colors`,
+          `--port ${archetype.webpack.devPort}`,
+          `--host ${archetype.webpack.devHostname}`
+        ],
+        {
+          xclap: { delayRunMs: 2000 },
+          execOptions: { env: { ELECTRODE_WEBPACK_PROFILE: "development" } }
+        }
       )
     },
 
     "wds.test": {
       desc: "Start webpack-dev-server in test mode",
-      task: mkCmd(
-        "webpack-dev-server",
-        `--config`,
-        quote(webpackConfig("webpack.config.test.js")),
-        `--progress --colors`,
-        `--port ${archetype.webpack.testPort}`,
-        `--host ${archetype.webpack.devHostname}`
+      task: xclap.exec(
+        [
+          "webpack-dev-server",
+          `--config`,
+          quote(checkForCustomWebpackConfig("webpack.config.test.js")),
+          `--progress --colors`,
+          `--port ${archetype.webpack.testPort}`,
+          `--host ${archetype.webpack.devHostname}`
+        ],
+        {
+          xclap: { delayRunMs: 2000 },
+          execOptions: { env: { ELECTRODE_WEBPACK_PROFILE: "test" } }
+        }
       )
     },
 
@@ -1100,13 +1120,15 @@ Individual .babelrc files were generated for you in src/client and src/server
     Object.assign(tasks, {
       "build-dist-dll": {
         dep: [".mk-dll-dir", ".mk-dist-dir", ".production-env"],
-        task: () =>
-          exec(
+        task: () => {
+          setWebpackProfile("dll");
+          return exec(
             `webpack --config`,
-            quote(webpackConfig("webpack.config.dll.js")),
+            quote(checkForCustomWebpackConfig("webpack.config.dll.js")),
             `--colors`,
             `--display-error-details`
-          )
+          );
+        }
       },
       "copy-dll": () => shell.cp("-r", "dll/*", "dist")
     });
@@ -1151,7 +1173,6 @@ Individual .babelrc files were generated for you in src/client and src/server
 }
 
 module.exports = function(xclap) {
-  setWebpackDevProfile();
   setupPath();
   createElectrodeTmpDir();
   xclap = xclap || requireAt(process.cwd())("xclap") || devRequire("xclap");
