b1300a3e32af76aa2ced383fc6ccd4bd20a42e24
Charles Selvarajan <charles.selvarajan@walmartlabs.com>
2019-08-12 10:32:45 -0700

    PR feedback, checkCustomConfig returns absolute path

--- packages/electrode-archetype-react-app-dev/config/webpack/util/check-custom-config.js
+++ packages/electrode-archetype-react-app-dev/config/webpack/util/check-custom-config.js
@@ -2,10 +2,11 @@
 
 const Fs = require("fs");
 const Path = require("path");
+const archetype = require("electrode-archetype-react-app/config/archetype");
 
 function checkForCustomWebpackConfig(reqConfigFile) {
   const customFilePath = Path.join(process.cwd(), "webpack.config.js");
-  const defaultFile = reqConfigFile || Path.join("../", "webpack.config.js");
+  const defaultFile = reqConfigFile || Path.join(archetype.config.webpack, "webpack.config.js");
 
   return Fs.existsSync(customFilePath) ? customFilePath : defaultFile;
 }
