5345684f2f226580363d9d4d941df7997612c2f6
Joel Chen <jchip@users.noreply.github.com>
2019-08-19 16:49:18 -0700

    optional check skip if user dep on a diff major version (#1330)

---
+++ packages/opt-archetype-check/test/test1.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const assert = require("assert");
+
+const { isSameMajorVersion } = require("../optional-check");
+
+assert(isSameMajorVersion("1.0.0", "1.2.1") === true);
+
+assert(isSameMajorVersion("~1.0.0", "1.2.1") === true);
+
+assert(isSameMajorVersion("^1.1.0", "1.2.1") === true);
+
+assert(isSameMajorVersion("0.1.0", "0.1.2") === true);
+assert(isSameMajorVersion("~0.1.1", "0.1.3") === true);
+assert(isSameMajorVersion("^0.1.1", "0.1.3") === true);
+
+assert(isSameMajorVersion("1.0.0", "2.2.1") === false);
+assert(isSameMajorVersion("~1.0.0", "2.2.1") === false);
+assert(isSameMajorVersion("^1.0.0", "2.2.1") === false);
