00c95d9528c51a4f248c98c7c072656868b472cb
Joel Chen <jchip@users.noreply.github.com>
2019-09-16 19:58:19 -0700

    update hapi plugin options and drop v16 support (#1348)

--- packages/subapp-server/lib/hapi-plugin.js
+++ packages/subapp-server/lib/hapi-plugin.js
@@ -3,16 +3,10 @@
 /* eslint-disable max-statements, complexity, global-require, no-magic-numbers */
 /* eslint-disable no-console */
 
-const { universalHapiPlugin } = require("electrode-hapi-compat");
-const { setupSubAppHapiRoutes, legacyPlugin } = require("./utils");
+const { setupSubAppHapiRoutes } = require("./utils");
 
-module.exports = universalHapiPlugin(
-  {
-    hapi16: legacyPlugin,
-    hapi17: setupSubAppHapiRoutes
-  },
-  {
-    name: "subapp-server",
-    version: "1.0.0"
-  }
-);
+module.exports = {
+  register: setupSubAppHapiRoutes,
+  name: "subapp-server",
+  version: "1.0.0"
+};
