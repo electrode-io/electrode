919527c746922eb91636113fa91e812db138f20a
Joel Chen <xchen@walmartlabs.com>
2019-09-26 08:47:37 -0700

    enable proxy for subapp sample

--- samples/poc-subapp/xclap.js
+++ samples/poc-subapp/xclap.js
@@ -6,6 +6,8 @@ process.env.SERVER_ES6 = true;
 
 process.env.WEBPACK_DEV_MIDDLEWARE = true;
 
+process.env.APP_SERVER_PORT = 3100;
+
 /*
  * Enable webpack's NodeSourcePlugin to simulate NodeJS libs in browser
  */
@@ -40,26 +42,3 @@ process.env.WEBPACK_DEV_MIDDLEWARE = true;
 // process.env.WEBPACK_DEV_HTTPS = true;
 
 require("electrode-archetype-react-app")();
-
-//
-// When single quote option is selected, this will use eslint to fix
-// generated files from double to single quote after npm install.
-// This is more reliable in case you stop npm install after the generator
-// generated the app.
-// You may remove this if you want.
-//
-const xclap = require("xclap");
-const Fs = require("fs");
-const Path = require("path");
-xclap.load("user", {
-  "fix-generator-eslint": {
-    task: () => {
-      const pkg = require("./package.json");
-      if (pkg.scripts.install === "clap fix-generator-eslint") {
-        pkg.scripts.install = "echo OK";
-        Fs.writeFileSync(Path.resolve("package.json"), JSON.stringify(pkg, null, 2));
-        return "~$eslint --fix config src test --ext .js,.jsx";
-      }
-    }
-  }
-});
