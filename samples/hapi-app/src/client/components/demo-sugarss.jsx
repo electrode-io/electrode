edfffa0262f2a7cbe2ec3dfa8da2805925e07a4d
Barbara Choy <barbchoy@gmail.com>
2019-09-26 15:30:55 -0700

    started a sugarss demo

---
+++ samples/hapi-app/src/client/components/demo-sugarss.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import sugar from "../styles/custom-sugarss.styl";
+
+class DemoSugarss extends React.Component {
+  render() {
+    return (
+      <div>
+        <h6 styleName={"sugar.docs-header"}>Demo SugarSS</h6>
+      </div>
+    );
+  }
+}
+
+export default DemoSugarss;
