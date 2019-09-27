b102a0ae11fb33162954c5b72d73ba3b0a4e5089
Barbara Choy <barbchoy@gmail.com>
2019-09-27 14:56:54 -0700

    buttons working using sss

--- samples/hapi-app/src/client/components/demo-sugarss.jsx
+++ samples/hapi-app/src/client/components/demo-sugarss.jsx
@@ -5,8 +5,16 @@ class DemoSugarss extends React.Component {
   render() {
     return (
       <div>
-        <h6 styleName="sugar.docs-header">Demo SugarSS</h6>
-        <div styleName="sugar.docs-section"></div>
+        <h6 styleName="sugar.header">Demo SugarSS</h6>
+        <div styleName="sugar.section">
+          <div styleName="sugar.main">
+            <button styleName="sugar.button-one">Tap Me</button>
+            <button styleName="sugar.button-two">
+              <span>Hover Me</span>
+            </button>
+            <button styleName="sugar.button-three">Click Me</button>
+          </div>
+        </div>
       </div>
     );
   }
