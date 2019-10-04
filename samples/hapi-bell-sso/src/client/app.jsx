0ad0456857a31bcf2434cf71752cf6ba903bd0e2
Barbara Choy <barbchoy@gmail.com>
2019-10-04 12:45:36 -0700

    first commit

---
+++ samples/hapi-bell-sso/src/client/app.jsx
@@ -0,0 +1,61 @@
+//
+// This is the client side entry point for the React app.
+//
+
+import React from "react";
+import { render, hydrate } from "react-dom";
+import { routes } from "./routes";
+import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
+import { renderRoutes } from "react-router-config";
+import { loadableReady } from "@loadable/component";
+
+//
+
+//
+// Redux configure store with Hot Module Reload
+//
+const configureStore = initialState => {
+  const store = createStore(rootReducer, initialState);
+
+  if (module.hot) {
+    module.hot.accept("./reducers", () => {
+      const nextRootReducer = require("./reducers").default;
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+
+  return store;
+};
+
+const store = configureStore(window.__PRELOADED_STATE__);
+
+const start = App => {
+  const jsContent = document.querySelector(".js-content");
+  const reactStart = window.__PRELOADED_STATE__ && jsContent.innerHTML ? hydrate : render;
+
+  loadableReady(() =>
+    reactStart(
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>,
+      jsContent
+    )
+  );
+};
+
+window.webappStart = () => start(() => renderRoutes(routes));
+
+//
+// Hot Module Reload setup
+//
+if (module.hot) {
+  module.hot.accept("./routes", () => {
+    const r = require("./routes");
+    start(() => renderRoutes(r.routes));
+  });
+}
