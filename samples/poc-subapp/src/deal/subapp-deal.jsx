abd1b182c13fb54052ecb3714f92927dd9908aa2
Joel Chen <xchen@walmartlabs.com>
2019-12-19 09:48:09 -0800

    first inline SubApp PoC

--- samples/poc-subapp/src/deal/subapp-deal.jsx
+++ samples/poc-subapp/src/deal/subapp-deal.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { loadSubApp } from "subapp-web";
 
-const Deal = () => {
-  return <div>SPECIAL DEAL - SPECIAL DEAL</div>;
+const Deal = props => {
+  return <div>SPECIAL DEAL - SPECIAL DEAL - {props.deal}</div>;
 };
 
 export default loadSubApp({ name: "Deal", Component: Deal });
