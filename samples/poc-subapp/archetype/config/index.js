735df6a65d20ea0c6ee9e173a95988b5455825c1
Joel Chen <xchen@walmartlabs.com>
2020-01-28 13:29:13 -0800

    update dep flags

--- samples/poc-subapp/archetype/config/index.js
+++ samples/poc-subapp/archetype/config/index.js
@@ -1,15 +1,5 @@
 module.exports = {
   options: {
-    mocha: false,
-    postcss: false,
-    pwa: false,
-    sass: false,
-    sinon: false,
-    stylus: false,
-    typescript: false,
-    karma: false,
-    subapp: true,
-    phantomjs: false,
-    criticalCSS: false
+    subapp: true
   }
 };
