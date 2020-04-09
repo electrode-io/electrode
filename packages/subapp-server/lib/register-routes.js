2865ec49ec43575b803abd705e20e5d0c39b066a
Christian Lent <christian@lent.us>
2020-04-09 00:22:35 -0400

    Add "selectTemplate" to picked opts: subapp -> electrode-react-webapp (#1594)

--- packages/subapp-server/lib/register-routes.js
+++ packages/subapp-server/lib/register-routes.js
@@ -18,7 +18,7 @@ module.exports = function registerRoutes({ routes, topOpts, server }) {
     const routeOptions = Object.assign(
       {},
       topOpts,
-      _.pick(route, ["pageTitle", "bundleChunkSelector", "templateFile"])
+      _.pick(route, ["pageTitle", "bundleChunkSelector", "templateFile", "selectTemplate"])
     );
 
     assert(routeOptions.templateFile, `subapp-server: route ${r.name} must define templateFile`);
