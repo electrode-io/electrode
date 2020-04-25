f75cbd36dc44c62053f2aef46a36873281cd7f0f
Joel Chen <xchen@walmartlabs.com>
2020-04-24 21:30:17 -0700

    fix: update sample for latest

--- samples/demo-tree-shaking/config/default.js
+++ samples/demo-tree-shaking/config/default.js
@@ -3,7 +3,7 @@
 const defaultListenPort = 3000;
 
 const portFromEnv = () => {
-  const x = parseInt(process.env.PORT, 10);
+  const x = parseInt(process.env.APP_SERVER_PORT || process.env.PORT, 10);
   /* istanbul ignore next */
   return x !== null && !isNaN(x) ? x : defaultListenPort;
 };
@@ -16,12 +16,12 @@ module.exports = {
         reporters: {
           myConsoleReporter: [
             {
-              module: "good-console"
+              module: "good-console",
             },
-            "stdout"
-          ]
-        }
-      }
+            "stdout",
+          ],
+        },
+      },
     },
     "webpack-dev": {
       module: "@xarc/app-dev/lib/webpack-dev-hapi",
@@ -30,26 +30,26 @@ module.exports = {
         // webpack dev middleware options
         dev: {
           // user reporter that's called by the archetype's reporter
-          reporter: reporterOptions => {
+          reporter: (reporterOptions) => {
             // For example, you can start app server with `clap devbrk` and attach
             // to it with chrome://inspect, and then enable this debugger statement
             // so chrome stop here every time webpack middleware finish compiling
             // debugger
-          }
-        }
-      }
+          },
+        },
+      },
     },
     inert: {
-      enable: true
+      enable: true,
     },
     electrodeStaticPaths: {
       enable: true,
       options: {
-        pathPrefix: "dist"
-      }
+        pathPrefix: "dist",
+      },
     }, //
     "server/plugins/pwa": {
-      module: "./{{env.APP_SRC_DIR}}/server/plugins/pwa"
+      module: "./{{env.APP_SRC_DIR}}/server/plugins/pwa",
     }, //
     "electrode-auto-ssr": {}, //
     webapp: {
@@ -60,13 +60,13 @@ module.exports = {
         paths: {
           "/{args*}": {
             content: {
-              module: "./{{env.APP_SRC_DIR}}/server/views/index-view"
-            }
-          }
-        }
-      }
+              module: "./{{env.APP_SRC_DIR}}/server/views/index-view",
+            },
+          },
+        },
+      },
     }, //
-    "electrode-cookies/hapi-plugin": {}
+    "electrode-cookies/hapi-plugin": {},
     //
   },
   connections: {
@@ -75,11 +75,11 @@ module.exports = {
       address: process.env.HOST_IP || "0.0.0.0",
       port: portFromEnv(),
       routes: {
-        cors: false
+        cors: false,
       },
       state: {
-        ignoreErrors: true
-      }
-    }
-  }
+        ignoreErrors: true,
+      },
+    },
+  },
 };
