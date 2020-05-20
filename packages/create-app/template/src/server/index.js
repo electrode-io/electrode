18e36091d5647e6999a8eda825ba23cec955652b
Joel Chen <jchip@users.noreply.github.com>
2020-05-20 12:32:30 -0700

    update: create-app templates and dep (#1655)
    - update template's deps
    - fix passing lodash to template's code during generation
    - switch to fastify

--- packages/create-app/template/src/server/index.js
+++ packages/create-app/template/src/server/index.js
@@ -1,10 +1,11 @@
 "use strict";
 
 const support = require("@xarc/app/support");
-const electrodeServer = require("electrode-server");
+const electrodeServer = require("@xarc/fastify-server");
 
 const config = {
   connection: {
+    host: process.env.HOST || "localhost",
     port: parseInt(process.env.APP_SERVER_PORT || process.env.PORT || "3000")
   },
   plugins: {
@@ -12,7 +13,13 @@ const config = {
       priority: -1,
       enable: process.env.WEBPACK_DEV === "true"
     },
-    "subapp-server": {}
+    "subapp-server": {
+      options: {
+        cdn: {
+          enable: process.env.NODE_ENV === "production"
+        }
+      }
+    }
   }
 };
 
