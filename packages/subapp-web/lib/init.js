ecdabd7ff719cfd9fb02fbb4a381546ea98281d3
Yisheng Jiang <yishengjiang@yahoo.com>
2020-05-22 12:12:45 -0700

    Update init.js

--- packages/subapp-web/lib/init.js
+++ packages/subapp-web/lib/init.js
@@ -11,10 +11,7 @@ module.exports = function setup(setupContext) {
   const distDir = process.env.NODE_ENV === "production" ? "../dist/min" : "../dist/dev";
   const clientJs = Fs.readFileSync(Path.join(__dirname, distDir, "subapp-web.js")).toString();
   const cdnJs = Fs.readFileSync(Path.join(__dirname, distDir, "cdn-map.js")).toString();
-  const loadJs = Fs.readFileSync(
-    require.resolve("loadjs/dist/loadjs.min.js"),
-    "utf8"
-  );
+  const loadJs = Fs.readFileSync(require.resolve("loadjs/dist/loadjs.min.js"), "utf8");
 
   //
   // TODO: in webpack dev mode, we need to reload stats after there's a change
@@ -34,12 +31,18 @@ module.exports = function setup(setupContext) {
     basePath: ""
   };
 
+  const inlineRuntime =
+    cdnJsBundles &&
+    cdnJsBundles.runtime &&
+    Fs.readFileSync(Path.join(distDir, cdnJsBundles.runtime));
+
   const webSubAppJs = `<script id="bundleAssets" type="application/json">
-  ${ cdnJsBundles && cdnJsBundles.runtime && Fs.readFileSync('dist'+cdnJsBundles.runtime) }
+  ${cdnJsBundles && cdnJsBundles.runtime && Fs.readFileSync("dist" + cdnJsBundles.runtime)}
 
 ${JSON.stringify(bundleAssets)}
 </script>
 <script>/*LJ*/${loadJs}/*LJ*/
+${inlineRuntime}
 ${clientJs}
 ${cdnJs}
 </script>
@@ -54,7 +57,9 @@ ${cdnJs}
     process: context => {
       context.user.assets = assets;
       context.user.includedBundles = {};
-
+      if (inlineRuntime) {
+        context.user.includedBundles[inlineRuntime] = true;
+      }
       // invoke the initialize method of subapp's server code
       if (subAppServers.length > 0) {
         for (const server of subAppServers) {
