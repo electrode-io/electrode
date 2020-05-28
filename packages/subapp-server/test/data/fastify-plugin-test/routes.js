291d59f10985467154943abd8dc020c5ca171f35
Yisheng Jiang <yishengjiang@yahoo.com>
2020-05-28 00:39:27 -0700

    subapp-server fastify plugin unit tests (#1658)
    * some unit test wip
    
    * proper unit tests
    
    proper unit tests
    
    * no listen on actual port
    
    * node module
    
    * fixed plugin... passes clap -n -x build check
    
    * use the right stats.json for unit test
    
    Co-authored-by: Yisheng Jiang <yisheng.jiang@gmail.com>

---
+++ packages/subapp-server/test/data/fastify-plugin-test/routes.js
@@ -0,0 +1,9 @@
+"use strict";
+module.exports = {
+  routes: {
+    "/": {
+      pageTitle: "Welcome to Electrode",
+      subApps: ["./Home", "./Demo1", "./Demo2"]
+    }
+  }
+};
---
+++ packages/subapp-server/test/spec/fastify-plugin.spec.js
@@ -0,0 +1,72 @@
+"use strict";
+
+const { fastifyPlugin } = require("../../lib/fastify-plugin");
+const Path = require("path");
+const { runFinally, asyncVerify } = require("run-verify");
+const http = require("http");
+
+describe("fastify-plugin", function () {
+  it("loads server from file system", async () => {
+    const server = await require("@xarc/fastify-server")({
+      deferStart: true,
+      connection: { port: 3002, host: "localhost" }
+    });
+
+    const opt = {
+      srcDir: Path.join(__dirname, "../data/fastify-plugin-test"),
+      loadRoutesFrom: "routes.js",
+      stats: Path.join(__dirname, "../data/fastify-plugin-test/stats.json")
+    };
+    return asyncVerify(
+      () => fastifyPlugin(server, opt),
+      () => server.start(),
+      () => {},
+      () => {
+        http.get("http://localhost:3002/", res => {
+          expect(res.statusCode).to.equal(200);
+          let data = "";
+          res.on("data", chunk => (data += chunk));
+          res.on("done", () => {
+            expect(data.to.contain("hello"));
+          });
+        });
+      },
+      runFinally(() => server.close())
+    );
+  });
+
+  it("preserves original end points", async function () {
+    const server = await require("@xarc/fastify-server")({
+      deferStart: true,
+      connection: { port: 3002, host: "localhost" }
+    });
+    server.route({
+      method: "GET",
+      path: "/",
+      handler: async () => "Hello World"
+    });
+    server.route({
+      method: "GET",
+      path: "/500",
+      handler: () => {
+        throw new Error("db error");
+      }
+    });
+
+    asyncVerify(
+      () => fastifyPlugin(server, {}),
+      () => server.start(),
+      () => {
+        http.get("http://localhost:3002/", res => {
+          expect(res.statusCode).to.equal(200);
+          let data = "";
+          res.on("data", chunk => (data += chunk));
+          res.on("done", () => {
+            expect(data.to.contain("Hello World"));
+          });
+        });
+      },
+      runFinally(() => server.close())
+    );
+  });
+});
