554876210898267a92c7036d403afc06731f7f33
Yisheng Jiang <yishengjiang@yahoo.com>
2020-05-28 17:10:27 -0700

    more concise code

--- packages/subapp-web/lib/init.js
+++ packages/subapp-web/lib/init.js
@@ -12,7 +12,6 @@ module.exports = function setup(setupContext) {
   const clientJs = Fs.readFileSync(Path.join(__dirname, distDir, "subapp-web.js")).toString();
   const cdnJs = Fs.readFileSync(Path.join(__dirname, distDir, "cdn-map.js")).toString();
   const loadJs = Fs.readFileSync(require.resolve("loadjs/dist/loadjs.min.js"), "utf8");
-
   //
   // TODO: in webpack dev mode, we need to reload stats after there's a change
   //
@@ -30,19 +29,17 @@ module.exports = function setup(setupContext) {
     basePath: ""
   };
 
-  let inlineRuntimeJS = "";
-  let rtJsCdnEntryPoint;
+  var inlineRuntimeJS = "";
+  let runtimeEntryPoints = [];
   if (process.env.NODE_ENV === "production") {
-    rtJsCdnEntryPoint = Object.keys(cdnJsBundles).filter(ep =>
+    runtimeEntryPoints = Object.keys(cdnJsBundles).filter(ep =>
       cdnJsBundles[ep].includes("runtime.bundle")
-    )[0];
-    const runtimeFullPath =
-      rtJsCdnEntryPoint &&
-      cdnJsBundles[rtJsCdnEntryPoint] &&
-      Path.join(Path.resolve("dist"), "js", Path.basename(cdnJsBundles[rtJsCdnEntryPoint]));
-    if (Fs.existsSync(runtimeFullPath)) {
-      inlineRuntimeJS = Fs.readFileSync(runtimeFullPath).toString();
-    }
+    );
+    inlineRuntimeJS = runtimeEntryPoints
+      .map(ep => Path.resolve("dist", "js", Path.basename(cdnJsBundles[ep])))
+      .filter(fullPath => Fs.existsSync(fullPath))
+      .map(fullPath => Fs.readFileSync(fullPath))
+      .join(" ");
   }
 
   const webSubAppJs = `<script id="bundleAssets" type="application/json">
@@ -52,12 +49,8 @@ ${JSON.stringify(bundleAssets)}
 /*rt*/${inlineRuntimeJS} /*rt*/
 ${clientJs}
 ${cdnJs}
-${
-  inlineRuntimeJS && rtJsCdnEntryPoint && `window.xarcV1.markBundlesLoaded([${rtJsCdnEntryPoint}]);`
-}
-
-</script>
-`;
+window.xarcV1.markBundlesLoaded([${runtimeEntryPoints.join(",")}]);
+</script>`;
 
   // check if any subapp has server side code with initialize method and load them
   const subAppServers = Object.keys(subappUtil.getAllSubAppManifest())
@@ -68,9 +61,9 @@ ${
     process: context => {
       context.user.assets = assets;
       context.user.includedBundles = {};
-      if (rtJsCdnEntryPoint && inlineRuntimeJS) {
-        context.user.includedBundles[rtJsCdnEntryPoint] = true;
-      }
+      runtimeEntryPoints.forEach(ep => {
+        context.user.includedBundles[ep] = true;
+      });
       // invoke the initialize method of subapp's server code
       if (subAppServers.length > 0) {
         for (const server of subAppServers) {
