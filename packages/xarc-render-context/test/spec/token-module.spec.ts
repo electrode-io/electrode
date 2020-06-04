8b845728c4e3db083c37d30f8d5fda444e116dae
Yisheng Jiang <yishengjiang@yahoo.com>
2020-06-04 00:33:24 -0700

    save prog on unit tests
    save prog
    
    save prog
    
    save prog
    
    save prog
    
    100% lines

--- packages/xarc-render-context/test/spec/token-module.spec.ts
+++ packages/xarc-render-context/test/spec/token-module.spec.ts
@@ -2,7 +2,6 @@ import { TokenModule, loadTokenModuleHandler } from "../../src";
 import { expect } from "chai";
 import * as Path from "path";
 import { TEMPLATE_DIR, TOKEN_HANDLER } from "../../src/symbols";
-import { isMainThread } from "worker_threads";
 const templateDir = Path.join(__dirname, "../fixtures");
 
 describe("TokenModule ", function () {
@@ -19,6 +18,61 @@ describe("TokenModule ", function () {
     tk.load({});
     expect(tk[TOKEN_HANDLER]).to.be.null;
     //    expect(Object.keys(tk[TOKEN_HANDLER]).length).to.equal(0);
+  });
+  it("should initialize null props to empty object", function () {
+    const tk = new TokenModule("test", 0, null, templateDir);
+    expect(tk.props).to.deep.equal({});
+  });
+
+  it("should set template dir to props[TEMPLATE_DIR] when exists ", function () {
+    const props = {};
+    props[TEMPLATE_DIR] = templateDir;
+    const tk = new TokenModule("test", 0, props, null);
+
+    expect(tk[TEMPLATE_DIR]).to.equal(templateDir); //deep.equal({});
+  });
+
+  it("should use process.cwd() as default templatedir ", function () {
+    const tk = new TokenModule("test", 0, null, null);
+
+    expect(tk[TEMPLATE_DIR]).to.equal(process.cwd()); //deep.equal({});
+  });
+});
+
+describe("require from modPath", function () {
+  const tk = new TokenModule("require(./token-module-01.ts)", 0, {}, templateDir);
+  it("should load as modPath if token.id contains string 'require'", function () {
+    tk.load({});
+    expect(tk[TOKEN_HANDLER]()).to.equal("hello from token 01");
+  });
+});
+
+describe("_call in options ", function () {
+  it("should load custom __call ggg function", function () {
+    const tk = new TokenModule("#./custom-call.ts", 0, { _call: "setup" }, templateDir);
+    expect(tk.props).to.deep.equal({ _call: "setup" });
+
+    tk.load({});
+    expect(tk[TOKEN_HANDLER]()).to.equal("hello from custom setup");
+    //  tk.custom = null;
+    tk.modPath = null;
+    tk.load({ a: 11 });
+    expect(tk[TOKEN_HANDLER]()).to.equal("hello from custom setup");
+    expect(tk[TOKEN_HANDLER]()).to.equal("hello from custom setup");
+    expect(tk[TOKEN_HANDLER]()).to.equal("hello from custom setup");
+  });
+
+  it("should load custom params", function () {
+    const tk = new TokenModule(
+      "#./custom-call-with-params.ts",
+      0,
+      { _call: "prepare" },
+      templateDir
+    );
+    expect(tk.props).to.deep.equal({ _call: "prepare" });
+
+    tk.load("css");
+    expect(tk[TOKEN_HANDLER]({ folder: "dist" })).to.equal("load css from dist folder");
   });
   it("should initialize null props to empty object", function () {
     const tk = new TokenModule("test", 0, null, templateDir);
