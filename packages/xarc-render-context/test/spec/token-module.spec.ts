55de460b9612c03301ed126b2307735040e95b1a
vn08447 <yisheng.jiang@walmart.com>
2020-06-09 09:54:56 -0700

    save prog

--- packages/xarc-render-context/test/spec/token-module.spec.ts
+++ packages/xarc-render-context/test/spec/token-module.spec.ts
@@ -1,11 +1,11 @@
-import { TokenModule } from "../../src";
+import { TokenModule, loadTokenModuleHandler } from "../../src";
 import { expect } from "chai";
 import * as Path from "path";
 import { TEMPLATE_DIR, TOKEN_HANDLER } from "../../src/symbols";
+import { isMainThread } from "worker_threads";
+const templateDir = Path.join(__dirname, "../fixtures");
 
 describe("TokenModule ", function () {
-  const templateDir = Path.join(__dirname, "../fixtures");
-
   it("should store id, pos, templateDir as props", function () {
     const tk = new TokenModule("test", 0, {}, templateDir);
     expect(tk.id).to.equal("test");
@@ -14,39 +14,12 @@ describe("TokenModule ", function () {
     expect(tk[TOKEN_HANDLER]).to.be.null;
   });
 
-  it("should load if token is a module", function () {
-    const tk = new TokenModule("#./token-module-01.ts", 0, {}, templateDir);
-
+  it("not a module and custom is defined", function () {
+    const tk = new TokenModule("custom", 0, { _call: "setup" }, templateDir);
     tk.load({});
-    expect(tk[TOKEN_HANDLER]()).to.equal("hello from token 01");
-    it("should not have to load module twice", function () {
-      tk.modPath = null;
-      tk.load({});
-      expect(tk[TOKEN_HANDLER]()).to.equal("hello from token 01");
-    });
+    expect(tk[TOKEN_HANDLER]).to.be.null;
+    //    expect(Object.keys(tk[TOKEN_HANDLER]).length).to.equal(0);
   });
-
-  it("should not have to load module twice", function () {
-    const tk = new TokenModule("#./token-module-01.ts", 0, {}, templateDir);
-    tk.load({});
-    expect(tk[TOKEN_HANDLER]()).to.equal("hello from token 01");
-  });
-
-  it("should load if token is a id contains string 'require'", function () {
-    const tk = new TokenModule("require(./token-module-01.ts)", 0, {}, templateDir);
-
-    tk.load({});
-    expect(tk[TOKEN_HANDLER]()).to.equal("hello from token 01");
-  });
-
-  it("should load custom __call function", function () {
-    const tk = new TokenModule("#./custom-call.ts", 0, { _call: "setup" }, templateDir);
-    expect(tk.props).to.deep.equal({ _call: "setup" });
-
-    tk.load({});
-    expect(tk[TOKEN_HANDLER]()).to.equal("hello from custom setup");
-  });
-
   it("should initialize null props to empty object", function () {
     const tk = new TokenModule("test", 0, null, templateDir);
     expect(tk.props).to.deep.equal({});
@@ -65,8 +38,32 @@ describe("TokenModule ", function () {
 
     expect(tk[TEMPLATE_DIR]).to.equal(process.cwd()); //deep.equal({});
   });
+});
 
-  it("should load custom __call function", function () {
+describe("require from modPath", function () {
+  const tk = new TokenModule("require(./token-module-01.ts)", 0, {}, templateDir);
+  it("should load as modPath if token.id contains string 'require'", function () {
+    tk.load({});
+    expect(tk[TOKEN_HANDLER]()).to.equal("hello from token 01");
+  });
+});
+
+describe("_call in options ", function () {
+  it("should load custom __call ggg function", function () {
+    const tk = new TokenModule("#./custom-call.ts", 0, { _call: "setup" }, templateDir);
+    expect(tk.props).to.deep.equal({ _call: "setup" });
+
+    tk.load({});
+    expect(tk[TOKEN_HANDLER]()).to.equal("hello from custom setup");
+    //  tk.custom = null;
+    tk.modPath = null;
+    tk.load({ a: 11 });
+    expect(tk[TOKEN_HANDLER]()).to.equal("hello from custom setup");
+    expect(tk[TOKEN_HANDLER]()).to.equal("hello from custom setup");
+    expect(tk[TOKEN_HANDLER]()).to.equal("hello from custom setup");
+  });
+
+  it("should load custom params", function () {
     const tk = new TokenModule(
       "#./custom-call-with-params.ts",
       0,
