8136c4de799024d0e01cb7dfa916a2450bdc4b44
vn08447 <yisheng.jiang@walmart.com>
2020-06-15 15:16:49 -0700

    save progress

--- packages/electrode-ignite/lib/menu-items/electrode-oss-doc.js
+++ packages/electrode-ignite/lib/menu-items/electrode-oss-doc.js
@@ -5,18 +5,14 @@ const Promise = require("bluebird");
 const opn = require("opn");
 const chalk = require("chalk");
 
-module.exports = function(link, docType, cliCmd) {
+module.exports = function (link, docType, cliCmd) {
   link = link || "https://docs.electrode.io";
   docType = docType || "Open Source";
   cliCmd = cliCmd || "docs";
 
   function execute() {
     return Promise.try(() => opn(link, { wait: false })).then(() => {
-      logger.log(
-        chalk.green(
-          `Electrode ${docType} docs opened in your browser, please check.`
-        )
-      );
+      logger.log(chalk.green(`Electrode ${docType} docs opened in your browser, please check.`));
     });
   }
 
