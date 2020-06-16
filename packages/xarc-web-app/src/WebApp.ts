4b23c8490624d6eedb2261aa51f1ea6d30363ae2
vn08447 <yisheng.jiang@walmart.com>
2020-06-16 16:32:44 -0700

    simplierfied make webapp handler

--- packages/xarc-web-app/src/WebApp.ts
+++ packages/xarc-web-app/src/WebApp.ts
@@ -1,16 +1,13 @@
 import { JsxRenderer } from "@xarc/jsx-renderer";
-import { RenderContext, TokenModule } from "@xarc/render-context";
-import { getTokenHandlers, TokeHandler, getReactTokenHandlers } from "@xarc/token_handler";
+import { RenderContext,TokenModule} from "@xarc/render-context";
+import { TokeHandler, getReactTokenHandlers } from "@xarc/handler_templates";
 import * as Path from "path";
 
 export class WebApp {
   _routeOptions: any;
-  _templateCache: {};
-  defaultSelection: any;
   templateFullPath: string;
-  templateFile: any;
   renderer: JsxRenderer;
-  template: any;
+  template: Array<TokenModule>;
 
   tokenHandlers: Array<TokeHandler>;
   renderContext: RenderContext;
@@ -20,46 +17,26 @@ export class WebApp {
     this.templateFullPath =
       (routeOptions.templateFile && Path.resolve(routeOptions.templateFile)) ||
       Path.join(__dirname, "../template/index");
+
     this.template = require(this.templateFullPath);
+
     this.tokenHandlers = [].concat(routeOptions.tokenHandler, routeOptions.tokenHandlers);
-    var gg = this.tokenHandlers;
-
-    //00000 originally
-    const tmplFile = templateFile || htmlFile;
-    cacheKey = cacheKey || (cacheId && `${tmplFile}#${cacheId}`) || tmplFile;
-
-    let asyncTemplate = routeOptions._templateCache[cacheKey];
-    if (asyncTemplate) {
-      return asyncTemplate;
+    if(!routeOptions.replaceTokenHandlers){
+      this.tokenHandlers.concat(getReactTokenHandlers());
     }
-
-    if (options) {
-      routeOptions = Object.assign({}, routeOptions, options);
-    }
-
-    const userTokenHandlers = []
-      .concat(tokenHandlers, routeOptions.tokenHandler, routeOptions.tokenHandlers)
-      .filter(x => x);
-
-    let finalTokenHandlers = userTokenHandlers;
-  }
-  initializeTemplate() {
+    
     this.renderer = new JsxRenderer({
       templateFullPath: Path.dirname(this.templateFullPath),
       template: _.get(this.template, "default", this.template),
-      tokenHandlers: finalTokenHandlers.filter(x => x),
+      tokenHandlers: this.tokenHandlers.filter(x => x),
       insertTokenIds: routeOptions.insertTokenIds,
       routeOptions
     });
   }
 
-  routeHandler(): (option: any) => {};
-
-  render = (options, temp: ateSelection) => {};
-
-  routeHandler(routeOptions);
-
-  static makeRouteHander(routeOptions) {
-    return new WebApp(routeOptions).routeHandler;
+  static makeRouteHander(routeOptions){
+    return new WebApp(routeOptions).renderer
   }
+
 }
+
