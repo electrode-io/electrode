503e0f08644d06b89ab9ca9e5d4b4b6c39ccb014
yisheng.jiang <yisheng.jiang@walmart.com>
2020-07-13 14:23:44 -0700

    new day new freshly forked branch

--- xclap.js
+++ xclap.js
@@ -138,7 +138,8 @@ const testGenerator = (testDir, name, clean, runTest, prompts) => {
 xclap.load({
   ".lerna.coverage":
     "~$lerna run --ignore ignite-core --ignore electrode-ignite --ignore generator-electrode --stream coverage",
-  bootstrap: "~$fynpo --ignore ignite-core electrode-ignite generator-electrode",
+  bootstrap:
+    "~$fynpo --ignore ignite-core electrode-ignite generator-electrode electrode-ui-config",
   test: ["bootstrap", ".lerna.coverage", "build-test"],
   "test-generator": [".test-generator --all"],
   "test-create-app": [".test-create-app"],
