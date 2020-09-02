302c33addec184def39cd3f1b78be25c8888fd64
Yisheng Jiang <yisheng.jiang@gmail.com>
2020-09-02 10:12:01 -0700

    fix typo in fake-res http respond

--- packages/xarc-app-dev/src/lib/fake-res.ts
+++ packages/xarc-app-dev/src/lib/fake-res.ts
@@ -89,7 +89,7 @@ class FakeRes extends EventEmitter {
   }
 
   httpRespond(res) {
-    res.writeHeader(res.this._statusCode, this._headers);
+    res.writeHeader(res, this._statusCode, this._headers);
     res.statusCode = this._statusCode;
     res.send(this._content);
     return res;
