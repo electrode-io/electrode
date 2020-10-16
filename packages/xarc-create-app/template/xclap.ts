bd45494a413046805261878da67a9884725eaeae
Yisheng Jiang <yishengjiang@yahoo.com>
2020-10-16 09:07:22 -0700

    feat: create-app default typescript (#1747)
    Co-authored-by: Yisheng Jiang <yisheng.jiang@gmail.com>

---
+++ packages/xarc-create-app/template/xclap.ts
@@ -0,0 +1,27 @@
+import { loadXarcDevTasks } from "@xarc/app-dev/lib/dev-tasks";
+import xclap from "xclap";
+
+xclap.updateEnv(
+  {
+    /*
+     * Configure local development with http://localhost:3000
+     */
+    HOST: "localhost",
+    PORT: 3000,
+    /*
+     * Set app's node server to listen at port 3100 so the proxy can listen at 3000
+     * and forward request to the app.
+     */
+    APP_SERVER_PORT: 3100,
+    /*
+     * Enable Electrode's built-in webpack dev server and reverse proxy for development
+     */
+    WEBPACK_DEV_MIDDLEWARE: true
+  },
+  {
+    // do not override any env flag already set in process.env
+    override: false
+  }
+);
+
+loadXarcDevTasks(xclap, {});
