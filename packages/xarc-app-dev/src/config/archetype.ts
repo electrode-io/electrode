06164d4c4d57b72e60fdb1a66fc9a0c495628354
Durrab Khan <Durrab.Khan@walmartlabs.com>
2020-11-17 12:48:42 -0800

    [PATCH] Bug fix for reading xarcOptions while populating legacy

--- packages/xarc-app-dev/src/config/archetype.ts
+++ packages/xarc-app-dev/src/config/archetype.ts
@@ -4,7 +4,7 @@ import { XarcOptions } from "./opt2/xarc-options";
 import { getDevArchetypeLegacy } from "./options";
 const _ = require("lodash");
 const getEnvProxy = require("./env-proxy");
-import { saveXarcOptions } from "../lib/utils";
+import { saveXarcOptions, loadXarcOptions } from "../lib/utils";
 
 let cachedArchetype = null;
 
@@ -23,7 +23,8 @@ module.exports = function getDevOptions(user: XarcOptions = {}) {
 
   // first get legacy configs
   const legacy = getDevArchetypeLegacy();
-
+  // try to read xarc-options.json if it exist and merge it into legacy
+  const xarcOptions = loadXarcOptions();
   const proxy = getEnvProxy();
 
   // proxy config was not set in legacy, so add to top level here
@@ -40,8 +41,32 @@ module.exports = function getDevOptions(user: XarcOptions = {}) {
     ...user,
     webpackOptions: undefined,
     babelOptions: undefined,
-    addOnFeatures: undefined
+    addOnFeatures: undefined,
   });
+  //if xarcOptions are available then merge it
+  if(!_.isNil(xarcOptions)){
+     // merge user.webpackOptions into legacy.webpack
+  _.merge(legacy.webpack, xarcOptions.webpack);
+  // merge user.babelOptions into legacy.babel
+  _.merge(legacy.babel, xarcOptions.babel);
+  // merge user.addOnFeatures into legacy.options
+  _.merge(legacy.options, xarcOptions.options);
+  // merge the rest into top level
+  _.merge(legacy, {
+    ...xarcOptions,
+    webpackOptions: undefined,
+    babelOptions: undefined,
+    addOnFeatures: undefined,
+  });
+  
+  }
+
+  //Added XARC CMD option
+  if(!_.isNil(user) && !_.isNil(user.XARC_CWD)){
+    legacy.options.XARC_CWD = user.XARC_CWD;
+  }
+  
+  console.log(`legacy.options.xarc_cwd ${JSON.stringify(legacy.options.XARC_CWD)}`);
 
   saveXarcOptions(legacy);
 
