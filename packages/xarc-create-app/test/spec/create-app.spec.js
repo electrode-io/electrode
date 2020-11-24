b8c33fd9710a5115feed05873b9dc2eba4bb7a4c
Durrab Khan <Durrab.Khan@walmartlabs.com>
2020-11-23 17:33:39 -0800

    [MINOR][FEATURE] added new line at the end

--- packages/xarc-create-app/test/spec/create-app.spec.js
+++ packages/xarc-create-app/test/spec/create-app.spec.js
@@ -2,20 +2,20 @@ const shcmd = require("shcmd");
 const { resolve } = require("path");
 const { execSync } = require("child_process");
 describe("create app", () => {
-  beforeEach(() => {
-    process.chdir(resolve(__dirname, "../../"));
+	beforeEach(() => {
+		process.chdir(resolve(__dirname, "../../"));
 
-    shcmd.mkdir("test/data/create-app-unit-test");
-    process.chdir("test/data/create-app-unit-test");
-  });
+		shcmd.mkdir("test/data/create-app-unit-test");
+		process.chdir("test/data/create-app-unit-test");
+	});
 
-  afterEach(() => {
-    process.chdir(resolve(__dirname, "../../"));
+	afterEach(() => {
+		process.chdir(resolve(__dirname, "../../"));
 
-    execSync("rm -rf test/data/create-app-unit-test");
-  });
+		execSync("rm -rf test/data/create-app-unit-test");
+	});
 
-  it("precondition: directory is created, and create app in cwd", () => {
-    shcmd.exec("echo 'y' | node ../../../src/create-app");
-  });
+	it("precondition: directory is created, and create app in cwd", () => {
+		shcmd.exec("echo 'y' | node ../../../src/create-app");
+	});
 });
