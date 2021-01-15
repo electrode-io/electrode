3babede7718ba57b28ff8058740653cd108226f7
Durrab Khan <Durrab.Khan@walmartlabs.com>
2021-01-14 16:37:48 -0800

    [MINIOR] merged electrode/master

--- packages/xarc-react-router/src/browser/react-router-browser.tsx
+++ packages/xarc-react-router/src/browser/react-router-browser.tsx
@@ -2,12 +2,14 @@ import { SubAppDef, SubAppFeatureFactory } from "@xarc/subapp";
 import { Router, BrowserRouter } from "react-router-dom";
 import { ReactRouterFeatureOptions, _id, _subId } from "../common";
 import { createBrowserHistory } from "history";
+
 let staticHistory;
+
 /**
  * Implement the component wrapping support for using react router on a subapp
  */
+
 export function reactRouterFeature(options: ReactRouterFeatureOptions): SubAppFeatureFactory {
-  console.log(`options.history ${options.history}`)
   const { createElement } = options.React; // eslint-disable-line
 
   const id = _id;
@@ -21,15 +23,19 @@ export function reactRouterFeature(options: ReactRouterFeatureOptions): SubAppFe
       let TheRouter: any;
 
       if (!options.hasOwnProperty("history") || options.history === true) {
+        // if user didn't specify history or set it to true, then create a shared
+        // history and use Router
         history = staticHistory || (staticHistory = createBrowserHistory());
         TheRouter = Router;
-      } else if (!options.history) {
-        TheRouter = BrowserRouter;
-      }
-      else {
+      } else if (options.history) {
+        // use Router with the history user provided
         history = options.history;
         TheRouter = Router;
+      } else {
+        // user specify a falsy history, fallback to BrowserRouter
+        TheRouter = BrowserRouter;
       }
+
       subapp._features.reactRouter = {
         id,
         subId,
