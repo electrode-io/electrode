3cb42b637d4483eb58e0dcf4d1dd53ee0627f79e
zenz34 <zz87022n@pace.edu>
2021-01-26 10:20:27 -0800

    [chore]refactored react query unit test, add more test case for browser and node side

--- packages/xarc-react-query/test/spec/browser-index.spec.tsx
+++ packages/xarc-react-query/test/spec/browser-index.spec.tsx
@@ -5,12 +5,12 @@ import { expect } from "chai";
 import { renderToString } from "react-dom/server";
 import { SubAppDef, SubAppContainer, envHooks } from "@xarc/subapp";
 import { useQuery } from "react-query";
-import { reactQueryFeature } from "../../src/node/index";
+import { reactQueryFeature } from "../../src/browser/index";
 import { testFetch } from "../prefetch";
 
 const { createElement } = React; // eslint-disable-line
 
-describe("reactQueryFeature node.js", function () {
+describe("reactQueryFeature browser", function () {
   it("should return a feature factory", async () => {
     const factory = reactQueryFeature({ React });
     expect(factory.id).equal("state-provider");
@@ -32,19 +32,14 @@ describe("reactQueryFeature node.js", function () {
     container.declare("test", def);
 
     factory.add(def);
+
     expect(def._features.reactQuery).to.be.an("object");
   });
 
   it("should render subapp with react-query", async () => {
     const container = new SubAppContainer({});
-
     envHooks.getContainer = () => container;
-
-    const factory = reactQueryFeature({
-      React,
-      serverModule: require.resolve("../prefetch")
-    });
-
+    const factory = reactQueryFeature({ React });
     const def = {
       name: "test",
       getModule() {
@@ -52,58 +47,26 @@ describe("reactQueryFeature node.js", function () {
       },
       _features: {}
     } as SubAppDef;
-
     container.declare("test", def);
 
-    factory.add(def);
-    const result = await def._features.reactQuery.execute({
+    const subapp = factory.add(def);
+
+    const res = await def._features.reactQuery.execute({
       input: {
         Component: () => {
           const { data } = useQuery("test", testFetch);
           return <div>test {JSON.stringify(data)}</div>;
         }
-      }
-    });
-
-    const str = renderToString(<result.Component />);
-
-    expect(str).equals(
-      `<div>test <!-- -->{&quot;msg&quot;:&quot;foo&quot;,&quot;queryKey&quot;:[&quot;test&quot;]}</div>`
-    );
-  });
-
-  it("should render subapp with react-query", async () => {
-    const container = new SubAppContainer({});
-
-    envHooks.getContainer = () => container;
-
-    const factory = reactQueryFeature({
-      React,
-      serverModule: undefined
-    });
-
-    const def = {
-      name: "test",
-      getModule() {
-        return Promise.resolve({});
       },
-      _features: {}
-    } as SubAppDef;
-
-    container.declare("test", def);
-
-    factory.add(def);
-    const result = await def._features.reactQuery.execute({
-      input: {
-        Component: () => {
-          const { data } = useQuery("test", testFetch);
-          return <div>test {JSON.stringify(data)}</div>;
-        }
+      csrData: {
+        name: "test",
+        getInitialState: () => "test"
       }
     });
 
-    const str = renderToString(<result.Component />);
+    expect(res).to.be.an("object");
 
-    expect(str).equals(`<div>test </div>`);
+    const htmlMarkup = renderToString(<res.Component />);
+    expect(htmlMarkup).equal(`<div>test </div>`);
   });
 });
