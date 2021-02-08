374c0a778b66969947d716d5f2c1a911f0088751
zenz34 <zz87022n@pace.edu>
2021-02-08 14:17:36 -0800

    add eslint config, add chai, mocha dependencies

--- samples/universal-react-node/.eslintrc.js
+++ samples/universal-react-node/.eslintrc.js
@@ -1,4 +1,18 @@
 const { eslintRcNodeTypeScript } = require("@xarc/module-dev");
+
 module.exports = {
+  env: {
+    mocha: true,
+    browser: true,
+    node: true
+  },
+  globals: {
+    document: false
+  },
+  rules: {
+    // note you must disable the base rule as it can report incorrect errors
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"]
+  },
   extends: eslintRcNodeTypeScript
 };
