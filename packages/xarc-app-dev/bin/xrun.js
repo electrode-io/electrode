2dc22555d423d73ac4dec16492aaa46c5765898f
Joel Chen <xchen@walmartlabs.com>
2021-04-08 11:34:10 -0700

    expose xrun command from xarc-app-dev so apps don't have to dep on xarc-run directly

---
+++ packages/xarc-app-dev/bin/xrun.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const requireAt = require("require-at")(process.cwd());
+
+function invoke() {
+  let xrunFull;
+
+  try {
+    xrunFull = requireAt.resolve("@xarc/run/bin/xrun");
+  } catch {
+    //
+  }
+
+  if (xrunFull && xrunFull.startsWith(process.cwd())) {
+    require(xrunFull);
+  } else {
+    require("@xarc/run/bin/xrun");
+  }
+}
+
+invoke();
