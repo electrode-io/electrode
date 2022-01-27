4b1a387964fdd31f5f58c234fdb5974a0bd0bb4c
Wesley Ren <wren@walmartlabs.com>
2022-01-27 15:43:45 -0800

    remove unneeded remote subapp settings

--- samples/poc-subapp-redux/xclap.js
+++ samples/poc-subapp-redux/xclap.js
@@ -14,35 +14,6 @@ const deps = require("./package.json").dependencies;
 
 loadDevTasks(xrun, {
   webpackOptions: {
-    minify: true,
-    v1RemoteSubApps: {
-      name: "poc-subapp",
-      subAppsToExpose: ["Deal", "Extras"],
-      shared: {
-        react: {
-          requiredVersion: deps.react,
-          import: "react",
-          shareKey: "react",
-          shareScope: "default",
-          singleton: true
-        },
-        "react-dom": {
-          requiredVersion: deps["react-dom"],
-          singleton: true
-        },
-        history: {
-          requiredVersion: deps["history"],
-          singleton: true
-        },
-        "subapp-web": {
-          requiredVersion: deps["subapp-web"],
-          singleton: true
-        },
-        "@babel/runtime": {
-          requiredVersion: deps["@babel/runtime"],
-          singleton: true
-        }
-      }
-    }
+    minify: true
   }
 });
