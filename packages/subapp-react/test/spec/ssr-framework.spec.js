c2d0ddd435b89ba2e02fabbe72e7158cd1117211
Mateja Ristic <matejaristic@MatejaR-USMAC22.lan>
2022-05-09 14:24:34 -0400

    remove leftover test function

--- packages/subapp-react/test/spec/ssr-framework.spec.js
+++ packages/subapp-react/test/spec/ssr-framework.spec.js
@@ -6,23 +6,6 @@ const { Routes, Route } = require("react-router-dom");
 const Redux = require("redux");
 const { connect } = require("react-redux");
 
-// safely handles circular references
-JSON.safeStringify = (obj, indent = 2) => {
-  let cache = [];
-  const retVal = JSON.stringify(
-    obj,
-    (key, value) =>
-      typeof value === "object" && value !== null
-        ? cache.includes(value)
-          ? undefined // Duplicate reference found, discard key
-          : cache.push(value) && value // Store value in our collection
-        : value,
-    indent
-  );
-  cache = null;
-  return retVal;
-};
-
 describe("SSR React framework", function () {
   it("should setup React framework", () => {
     expect(lib.React).to.be.ok;
