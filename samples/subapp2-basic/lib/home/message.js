c19a39ac4d4c42cbbf061add32e11f6ab7bc8ae0
mristic505 <mateja.ristic@gmail.com>
2022-05-19 11:10:37 -0400

    remove test subapp

--- samples/subapp2-basic/lib/home/message.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-exports.__esModule = true;
-exports.message = void 0;
-const message = "Welcome to xarc React Application";
-exports.message = message;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6WyJtZXNzYWdlIl0sInNvdXJjZXMiOlsiLi4vLi4vc3JjL2hvbWUvbWVzc2FnZS50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgbWVzc2FnZSA9IFwiV2VsY29tZSB0byB4YXJjIFJlYWN0IEFwcGxpY2F0aW9uXCI7XG4iXSwibWFwcGluZ3MiOiI7Ozs7QUFBTyxNQUFNQSxPQUFPLEdBQUcsbUNBQWhCIn0=
