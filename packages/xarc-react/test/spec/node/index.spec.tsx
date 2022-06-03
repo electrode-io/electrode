97346b6dd561130bad5c2ed1b0dc8a971f7285d2
Srinath Muthyala <Srinath.reddy.muthyala@walmart.com>
2022-06-03 12:16:57 -0700

    removed data-reactroot from tests

--- packages/xarc-react/test/spec/node/index.spec.tsx
+++ packages/xarc-react/test/spec/node/index.spec.tsx
@@ -19,14 +19,16 @@ describe("createDynamicComponent", function () {
     expect(container.getNames()).contains("test");
     expect(Component).to.be.a("function");
 
+    // data-reactroot isn't getting created due to Context.Provider
+    // see https://github.com/facebook/react/issues/15012
     const html = renderToString(<Component />);
     expect(html).to.equal(
-      `<div data-reactroot="">subapp <!-- -->test<!-- --> component loading... </div>`
+      `<div>subapp <!-- -->test<!-- --> component loading... </div>`
     );
 
     await container.get("test")._getModule();
 
     const html2 = renderToString(<Component />);
-    expect(html2).to.equal(`<div data-reactroot="">hello</div>`);
+    expect(html2).to.equal(`<div>hello</div>`);
   });
 });
