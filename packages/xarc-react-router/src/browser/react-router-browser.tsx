9444e72b7f1a090c6a824a5a50dd1d51ab132299
Srinath Muthyala <Srinath.reddy.muthyala@walmart.com>
2022-06-23 11:46:04 -0700

    fix: fix test cases and lint issues

--- packages/xarc-react-router/src/browser/react-router-browser.tsx
+++ packages/xarc-react-router/src/browser/react-router-browser.tsx
@@ -22,7 +22,7 @@ export function reactRouterFeature(options: ReactRouterFeatureOptions): SubAppFe
     subId,
     add(subapp: SubAppDef) {
       let history: any;
-      let TheRouter: any;      
+      let TheRouter: any;
 
       subapp._features.reactRouter = {
         id,
@@ -31,17 +31,17 @@ export function reactRouterFeature(options: ReactRouterFeatureOptions): SubAppFe
           const Component = input.Component || subapp._getExport()?.Component;
           return {
             Component: (props: any) => {
-              /** 
+              /**
                * unstable_HistoryRouter as HistoryRouter is used here instead of <Router>
-               * because react_router v6 removed history prop support for <Router>. 
-               * This API is currently prefixed as unstable_ because you may unintentionally 
-               * add two versions of the history library, the one you have added 
+               * because react_router v6 removed history prop support for <Router>.
+               * This API is currently prefixed as unstable_ because you may unintentionally
+               * add two versions of the history library, the one you have added
                * to the package.json and whatever version React Router uses internally.
-               * Once react-router has a mechanism to detect mis-matched versions, 
+               * Once react-router has a mechanism to detect mis-matched versions,
                * this API will remove its unstable_ prefix.
                * https://reactrouter.com/docs/en/v6/routers/history-router
                * For now, use the unstable_HistoryRouter version here.
-              */
+               */
               if (!options.hasOwnProperty("history") || options.history === true) {
                 // if user didn't specify history or set it to true, then create a shared
                 // history and use HistoryRouter
@@ -54,9 +54,9 @@ export function reactRouterFeature(options: ReactRouterFeatureOptions): SubAppFe
               } else {
                 // user specify a falsy history, fallback to BrowserRouter
                 TheRouter = BrowserRouter;
-              }      
+              }
               // conditional history props. BrowserRouter does not support history props in v6.
-              const historyProps = history ? { history } : {}
+              const historyProps = history ? { history } : {};
               return (
                 <TheRouter {...historyProps} >
                   <Component {...props} />
