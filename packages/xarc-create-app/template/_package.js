e60e43882fb70540e0dca35809b2335e125f65d2
Srinath Muthyala <59977748+smuthya@users.noreply.github.com>
2022-08-11 13:32:42 -0700

    [major] upgrade xarc-create-app package dependencies to refer react 18 (#1903)
    * major: upgrade xarc-create-app package dependencies to refer react 18
    
    * minor: update create-app-demo sample app
    
    Co-authored-by: Srinath Muthyala <Srinath.reddy.muthyala@walmart.com>

--- packages/xarc-create-app/template/_package.js
+++ packages/xarc-create-app/template/_package.js
@@ -36,17 +36,17 @@ module.exports = (base, merge) => {
     },
     dependencies: {
       "@babel/runtime": "^7.12.5",
-      "@xarc/app": "^9.0.3", // version will come from ../package.json
+      "@xarc/app": "^11.0.0", // version will come from ../package.json
       "@xarc/fastify-server": "^3.2.3",
-      "@xarc/react": "^0.1.0", // version will come from ../package.json
-      "@xarc/react-query": "^0.1.3", // version will come from ../package.json
-      "@xarc/react-redux": "^0.1.6", // version will come from ../package.json
+      "@xarc/react": "^1.0.0", // version will come from ../package.json
+      "@xarc/react-query": "^1.0.0", // version will come from ../package.json
+      "@xarc/react-redux": "^1.0.0", // version will come from ../package.json
     },
     devDependencies: {
       "@types/node": "^14.14.6",
-      "@xarc/app-dev": "^9.0.3", // version will come from ../package.json
-      "@xarc/opt-postcss": "^1.0.0",
-      "@xarc/opt-stylus": "^1.0.0",
+      "@xarc/app-dev": "^11.0.0", // version will come from ../package.json
+      "@xarc/opt-postcss": "^2.0.0",
+      "@xarc/opt-stylus": "^2.0.0",
       prettier: "^2.2.1",
       "ts-node": "^9.1.1",
       typescript: "^4.1.3",
