0ce0be3f8d240ce9df58d1ebe675fb94bd6ad118
smuthya <srinath.reddy.muthyala@walmart.com>
2022-10-17 14:20:01 -0700

    update eslintrc.js file

--- samples/stylus-sample/.eslintrc.js
+++ samples/stylus-sample/.eslintrc.js
@@ -1,32 +1,6 @@
-//.eslintrc.js
+
+const { eslintRcReact } = require("@xarc/app-dev");
 module.exports = {
-  env: {
-    node: true,
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@next/next/recommended",
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {
-    "react/no-unknown-property": ["error", { ignore: ["jsx"] }],
-  },
-  globals: {
-    React: "writable",
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+  extends: eslintRcReact,
+  "react/no-unknown-property": ["error", { ignore: ["jsx"] }]
 };
