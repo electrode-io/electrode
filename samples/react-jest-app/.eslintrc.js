17df4303ab4620be22d7f84ce65dbeded17802fd
smuthya <srinath.reddy.muthyala@walmart.com>
2022-10-18 12:32:23 -0700

    fix lint issues in react-jest-app

--- samples/react-jest-app/.eslintrc.js
+++ samples/react-jest-app/.eslintrc.js
@@ -1,7 +1,4 @@
 const { eslintRcReact } = require("@xarc/app-dev");
 module.exports = {
-  extends: eslintRcReact,
-  rules: {
-    "react/no-unknown-property": ["error", { ignore: ["jsx"] }]
-  }
+  extends: eslintRcReact
 };
