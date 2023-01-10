479211573cb73a5d30602461159dc37f15cec75d
arunvishnun <1584121+arunvishnun@users.noreply.github.com>
2023-01-09 21:22:00 -0800

    docs: documentation updates (#1922)
    * docs: update readme and contribution guidelines
    
    * docs: documentation and contribution guidelines updated
    
    * docs: update release guidelines
    
    * docs: update release guidelines

---
+++ docs/assets/js/9edccaa3.04d6f351.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkelectrode_docs=self.webpackChunkelectrode_docs||[]).push([[728],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
