e3e199a3d380df68965a71a7943e2cba87b34842
arunvishnun <1584121+arunvishnun@users.noreply.github.com>
2023-05-05 13:16:41 -0700

    chore: fix sample application with csp header

--- samples/poc-subappv1-csp/src/server/templates/template.jsx
+++ samples/poc-subappv1-csp/src/server/templates/template.jsx
@@ -24,7 +24,8 @@ import {
           <Require _id="subapp-web/lib/init" />
           <Token _id="CUSTOM_TOKEN_HANDLER" />
           <Token _id="CRITICAL_CSS" />
-          <Token _id="INLINE_CSS" />
+          {/* Enable this to see inline style blocked with CSP */}
+          {/* <Token _id="INLINE_CSS" /> */}
         </head>
         <Token _id="HEAD_CLOSED" />
         <body>
