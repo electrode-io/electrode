cfa61fc2e00ecac814652382d57cb15e935e34f8
arunvishnun <1584121+arunvishnun@users.noreply.github.com>
2023-11-29 23:49:43 -0800

    feat: fixing stats error on initial load

--- packages/subapp-server/lib/utils.js
+++ packages/subapp-server/lib/utils.js
@@ -206,6 +206,16 @@ function getCSPHeader({ styleNonce = "", scriptNonce = "" }) {
   return `${scriptSrc}${styleSrc}`;
 }
 
+function until(conditionFunction) {
+
+  const poll = resolve => {
+    if(conditionFunction()) resolve();
+    else setTimeout(_ => poll(resolve), 400);
+  }
+
+  return new Promise(poll);
+}
+
 module.exports = {
   getSrcDir,
   getDefaultRouteOptions,
@@ -215,5 +225,6 @@ module.exports = {
   checkSSRMetricsReporting,
   invokeTemplateProcessor,
   setCSPNonce,
-  getCSPHeader
+  getCSPHeader,
+  until
 };
