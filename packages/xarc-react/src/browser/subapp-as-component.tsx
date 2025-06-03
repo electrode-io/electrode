c7e1373088f0df4d6a690e05e2bfa20359a285ea
Rahul Kumar <rahulkumar271@gmail.com>
2025-06-03 15:52:52 +0530

    fix: update SubAppStartComponent as funcational component

--- packages/xarc-react/src/browser/subapp-as-component.tsx
+++ packages/xarc-react/src/browser/subapp-as-component.tsx
@@ -15,8 +15,8 @@ import { SubAppInlineOptions } from "../common";
  * - If `ssr` is true, then `prepareOnly` for an inlined subapp is always `true` because
  *   it will be rendered as part of its parent subapp instead.
  *
- * @param subapp
- * @param options
+ * @param subapp The subapp definition to inline
+ * @param options Options for inlining the subapp
  * @returns A react component to be inlined in another subapp
  */
 export function subAppInlineComponent(
@@ -26,51 +26,15 @@ export function subAppInlineComponent(
   if (options.ssr) {
     subapp._ssr = true;
   }
-
-  //
-  // A subapp within another subapp has one big quirk with SSR:
-  //
-  // - It's not possible for us to find out what they are until the parent subapp
-  //   is being rendered.
-  //
-  // Solutions:
-  //
-  // 1. With suspense, we can suspend rendering and prepare a nested subapp, but
-  //    React still doesn't support suspense for SSR.
-  // 2. Without suspense, we need user to pre-specify all the subapps that will need
-  //    preparing.
-  //
-  // Even though this is client side only and suspense is available, we are relying on
-  // number 2 for now. So by the time we reach here, we expect all subapps should've
-  // been prepared.
-  //
-  // The key is, how do we get the pipeline that was used to prepare the subapp, and
-  // connect it to SubAppStartComponent here?
-  //
-
-  //
-  // subapp.id -> unique definition declare time id
-  // subapp.asCompId -> incrementing index of as component instance
-  // subapp.asCompId resets to zero when subapp is re-declared for HMR
-  //
-  // the prepare step needs hint from user on how many pipeline is needed for a subapp
-  // to be used as nesting comp
-  // when SubAppStartComponent needs a pipeline to use, it just look up as-comp pipeline
-  // with count index same as asCompId.
-  //
-
   const id = `subapp-${subapp.name}-inline`;
-
-  const Comp = () => (
-    <SubAppInlineComponent subapp={subapp} inlineId={id} __props={{}} />
+  const Comp = (props: any) => (
+    <SubAppInlineComponent subapp={subapp} inlineId={id} __props={props} />
   );
-
   Comp.loadOptions = {
     name: subapp.name,
     ssr: options.ssr,
     prepareOnly: true,
     inlineId: id,
   };
-
   return Comp;
 }
--- packages/xarc-react/src/browser/subapp-inline-component.tsx
+++ packages/xarc-react/src/browser/subapp-inline-component.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-console, @typescript-eslint/ban-ts-comment */
-/* global window */
 
-import { createElement, Component } from "react"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import React from "react";
 import {
   SubAppStartComponent,
   SubAppStartComponentPropsType,
@@ -13,45 +12,24 @@ type SubAppInlineComponentPropsType = SubAppStartComponentPropsType & {
 };
 
 /**
- * wrapping component for starting a subapp, either for hot reload or nesting within another
- * subapp as a component.
+ * Functional component for starting a subapp inline, for hot reload or nesting within another subapp.
+ * @param props Props for the inline subapp component
+ * @returns React.JSX.Element
  */
-export class SubAppInlineComponent extends SubAppStartComponent {
-  // props: SubAppInlineComponentPropsType;
-  inlineId: string;
 
-  constructor(props: SubAppInlineComponentPropsType, context: any) {
-    super(props, context, "inline");
-    this.inlineId = props.inlineId;
-    xarcV2.debug(
-      "SubAppInlineComponent subapp",
-      props.subapp.name,
-      this.inlineId
-    );
-  }
-
-  shouldComponentUpdate() {
-    // Note: without this, every time parent subapp has changes, the whole
-    // nested subapp will be re-rendered, which is a perf killer.
-    // TODO: define conditions an inline subapp should update
-    return false;
-  }
-
-  render() {
-    xarcV2.debug(
-      "SubAppInlineComponent rendering subapp",
-      this.props.subapp.name
-    );
-    const subapp = this.getSubApp();
-    if (!subapp._module) {
-      subapp._getModule().then(() => this.reload());
-      return <div>loading subapp for inlining: {this.subapp.name}</div>;
-    }
-    const prepResult = this.getPipeline().getPrepResult();
-    return (
-      <div>
-        <prepResult.Component {...this.props.__props} />
-      </div>
-    );
-  }
+export function SubAppInlineComponent(
+  props: SubAppInlineComponentPropsType
+): React.JSX.Element {
+  const { subapp, inlineId, pipeline, __props } = props;
+  React.useEffect(() => {
+    xarcV2.debug("SubAppInlineComponent subapp", subapp.name, inlineId);
+  }, [subapp, inlineId]);
+  // Pass all props to SubAppStartComponent
+  return (
+    <SubAppStartComponent
+      subapp={subapp}
+      pipeline={pipeline}
+      __props={__props}
+    />
+  );
 }
