7b283990940e6a43f07eae93fa533973472f7773
Shubham Sharma - s0s0p6u <Shubham.Sharma3@walmart.com>
2025-08-21 20:27:40 +0530

    fix: lint errors in publish flow

--- packages/xarc-app/.eslintrc.js
+++ packages/xarc-app/.eslintrc.js
@@ -1,5 +1,4 @@
 const { eslintRcNodeTypeScript } = require("@xarc/module-dev");
 module.exports = {
-  extends: eslintRcNodeTypeScript,
-  parser: "@typescript-eslint/parser"
+  extends: eslintRcNodeTypeScript
 };
